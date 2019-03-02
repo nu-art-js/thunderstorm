fb03f754599877845fe1d16b1d69d77ad01fe943
TacB0sS <adam.vdk@nu-art-software.com>
2019-03-03 00:03:58 +0200

    working with sources and submodules

---
+++ setup.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+projectModule=project
+modules=(nu-art-core)
+modulePackageName=()
+moduleVersion=()
+
+for module in "${modules[@]}"; do
+    cd ${module}
+        npm link
+        packageName=`cat package.json | grep '"name":' | sed -E "s/.*\"name\".*\"(.*)\",?/\1/"`
+        version=`cat package.json | grep '"version":' | sed -E "s/.*\"version\".*\"(.*)\",?/\1/"`
+        modulePackageName+=(${packageName})
+        moduleVersion+=(${version})
+    cd ..
+done
+
+cd ${projectModule}
+    for (( arg=0; arg<${#modules[@]}; arg+=1 )); do
+        npm link ${modules[${arg}]} ${modulePackageName[${arg}]}
+    done
+
+    cp package.json _package.json
+    for moduleName in "${modulePackageName[@]}"; do
+        sed -i '' "/${moduleName/\//\\/}/d" package.json
+    done
+
+    npm install
+    rm package.json
+    mv _package.json package.json
+cd ..
