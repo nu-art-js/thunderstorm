cd723b7d8341673de13e3bc83b053e006567f5f9
TacB0sS <adam.vdk@nu-art-software.com>
2019-03-05 00:31:52 +0200

    test submodules

--- .pull-fork-origin.sh
+++ .pull-fork-origin.sh
@@ -2,6 +2,7 @@
 
 git stash
 git remote add public git@github.com:nu-art-js/typescript-boilerplate.git
-git pull
+git fetch public
 git merge public/master
 git stash pop
+git submodule update dev-tools
