dcbba2818a531ea9ca9d6c46e4d6fd0e31bd57d6
TacB0sS <adam.vdk@nu-art-software.com>
2019-10-11 17:33:33 +0300

    server api errors & slack integration in storm

---
+++ .scripts/modules.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+boilerplateRepo="git@github.com:nu-art-js/thunderstorm-boilerplate.git"
+
+frontendModule=app-frontend
+backendModule=app-backend
+allowedBranchesForPromotion=(master staging dev)
+
+projectModules=(app-backend app-frontend)
+
+otherModules=(app-shared)
+
+nuArtModules=(ts-common testelot thunder storm live-docs)
+nuArtModules=(ts-common testelot thunder storm)
+modules=()
