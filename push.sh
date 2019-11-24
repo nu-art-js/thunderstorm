ec050d6199379fc952dcc8442818d819a694ebe9
TacB0sS <adam.vdk@nu-art-software.com>
2019-11-24 14:25:21 +0200

    api names for http request erroe and success & localstorage patch

--- push.sh
+++ push.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
+source ./dev-tools/scripts/git/_core.sh
+source ./dev-tools/scripts/_core-tools/_source.sh
+
+if [[ "${1}" == "" ]]; then
+    throwError "Missing commit message" 2
+fi
+
 bash ./dev-tools/scripts/git/git-pull.sh -a -f
-bai --lint
+throwError "Error pulling Thunderstorm" $?
+
+bash ./build-and-install.sh --lint
+throwError "Error linting ans building Thunderstorm" $?
+
 bash ./dev-tools/scripts/git/git-push.sh --this -m="${1}"
+throwError "Error pushing Thunderstorm" $?
+
