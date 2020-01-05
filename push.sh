6da97be7ee72a8ef0fc5976dbddfa41edcb4784a
TacB0sS <adam.vdk@nu-art-software.com>
2020-01-05 18:55:42 +0200

    ts-common:\n update node-forge\n\nthunder:\n made http response handler to be async\n\nstorm:\n arrow function instead of method custom transactions firestore transaction wip\n\n

--- push.sh
+++ push.sh
@@ -3,16 +3,30 @@
 source ./dev-tools/scripts/git/_core.sh
 source ./dev-tools/scripts/_core-tools/_source.sh
 
-if [[ "${1}" == "" ]]; then
-    throwError "Missing commit message" 2
+version=`getVersionName version-nu-art.json`
+if [[ ! "${version}" ]]; then
+    throwError "Could not resolve version!" 2
 fi
 
 bash ./dev-tools/scripts/git/git-pull.sh -a -f
 throwError "Error pulling Thunderstorm" $?
 
 bash ./build-and-install.sh --lint
-throwError "Error linting ans building Thunderstorm" $?
+throwError "Error linting and building Thunderstorm" $?
 
-bash ./dev-tools/scripts/git/git-push.sh --this -m="${1}"
+
+modules=(ts-common testelot thunder storm)
+log=""
+for module in ${modules[@]}; do
+    cd ${module}
+        moduleLog=`git log --pretty=oneline --decorate=no --invert-grep --grep="lint" --grep="shit" --no-merges v${version}... | sed -E "s/[0-9a-f]*( .*)/\1/g"`
+        gitAssertRepoClean
+        if [[ "${moduleLog}" ]]; then
+            log="${log}${module}:\n${moduleLog}\n\n"
+        fi
+    cd ..
+done
+
+bash ./dev-tools/scripts/git/git-push.sh --this -m="${log}"
 throwError "Error pushing Thunderstorm" $?
 
