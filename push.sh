9d4950ed189b1508991e2fdb61952a66248aa956
TacB0sS <adam.vdk@nu-art-software.com>
2020-01-12 13:44:56 +0200

    ts-common:\n README.md app-backend app-frontend app-shared build-and-install.sh dev-tools firebase.json live-docs push.sh storm testelot thunder thunderstorm-boilerplate.iml ts-common version-app.json version-nu-art.json array and type utilsn\n\n

--- push.sh
+++ push.sh
@@ -8,26 +8,25 @@ if [[ ! "${version}" ]]; then
     throwError "Could not resolve version!" 2
 fi
 
-bash ./dev-tools/scripts/git/git-pull.sh -a -f
-throwError "Error pulling Thunderstorm" $?
+bash ./dev-tools/scripts/git/gist-pull.sh -a -f
 
 bash ./build-and-install.sh --lint
 throwError "Error linting and building Thunderstorm" $?
 
 
 modules=(ts-common testelot thunder storm)
-log=""
+submodulesLog=""
 for module in ${modules[@]}; do
     cd ${module}
         moduleLog=`git log --pretty=oneline --decorate=no --invert-grep --grep="lint" --grep="shit" --no-merges v${version}... | sed -E "s/[0-9a-f]*( .*)/  * \1\n/g"`
         gitAssertRepoClean
         if [[ "${moduleLog}" ]]; then
-            log="${log}${module}:\n${moduleLog}\n\n"
+            submodulesLog="${submodulesLog}${module}:\n${moduleLog}\n\n"
         fi
     cd ..
 done
 
-echo -e "${log}"
-bash ./dev-tools/scripts/git/git-push.sh --this -m="${log}"
+echo -e "${submodulesLog}"
+bash ./dev-tools/scripts/git/git-push.sh --this -m="${submodulesLog}"
 throwError "Error pushing Thunderstorm" $?
 
