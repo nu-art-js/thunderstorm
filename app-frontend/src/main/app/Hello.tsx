32a2914f4cd332340c9d01bf31467844a8bbfb94
TacB0sS <adam.vdk@nu-art-software.com>
2020-01-25 18:41:50 +0200

    added example for livedocs.. phase 1

--- app-frontend/src/main/app/Hello.tsx
+++ app-frontend/src/main/app/Hello.tsx
@@ -26,6 +26,7 @@ import {
 	BaseComponent,
 	OnRequestListener
 } from "@nu-art/thunder";
+import {LiveDoc} from "@nu-art/live-docs/frontend";
 
 export class Hello
 	extends BaseComponent<{}, { label: string }>
@@ -40,8 +41,8 @@ export class Hello
 
 	render() {
 		return <>
-			<h1 onClick={ExampleModule.getMessageFromServer1} >{this.state.label}</h1>
-			<h1 onDoubleClick={ExampleModule.getMessageFromServer2}>Double click me</h1>
+			<div className="ll_h_c"><h1 onClick={ExampleModule.getMessageFromServer1}>{this.state.label}</h1><LiveDoc docKey="one-mouse-click"/></div>
+			<div className="ll_h_c"><h1 onDoubleClick={ExampleModule.getMessageFromServer2}>Double click me</h1><LiveDoc docKey="double-mouse-click"/></div>
 		</>;
 	}
 
