dce94652bbd34e6bf0e5b1179d2d04d8753bb9a4
TacB0sS <adam.vdk@nu-art-software.com>
2020-01-25 17:30:48 +0200

    saml module moved to user account packagegs

--- push.sh
+++ push.sh
@@ -4,35 +4,38 @@ source ./dev-tools/scripts/git/_core.sh
 source ./dev-tools/scripts/_core-tools/_source.sh
 
 version=`getVersionName version-nu-art.json`
+dryRun=
 
-if [[ "${1}" ]]; then
-    version=${1}
-fi
+[[ "${1}" == "-dr" ]] && dryRun=true && shift
+[[ "${1}" ]] && version=${1}
+[[ ! "${version}" ]] && throwError "Could not resolve version!" 2
 
-if [[ ! "${version}" ]]; then
-    throwError "Could not resolve version!" 2
-fi
+[[ ! ${dryRun} ]] && bash ./dev-tools/scripts/git/gist-pull.sh -a -f
 
-bash ./dev-tools/scripts/git/gist-pull.sh -a -f
-
-bash ./build-and-install.sh --lint
+[[ ! ${dryRun} ]] && bash ./build-and-install.sh --lint
 throwError "Error linting and building Thunderstorm" $?
 
 
-modules=(ts-common testelot thunder storm)
+modules=(ts-common testelot thunder storm live-docs user-account)
+function appendLog() {
+    local module=${1}
+    gitAssertRepoClean
+
+    [[ ! $(gitAssertTagExists v${version})  ]] && logWarning "Could not find version tag v${version} in package: ${module}" && return 0
+    moduleLog=`git log --pretty=oneline --decorate=no --invert-grep --grep="lint" --grep="version bumped" --grep="shit" --no-merges v${version}... | sed -E "s/[0-9a-f]*( .*)/  * \1\n/g"`
+
+    [[ ! "${moduleLog}" ]] && logInfo "No changes found in package: ${module}" && return 0
+    submodulesLog="${submodulesLog}${module}:\n ${moduleLog}\n\n"
+}
+
 submodulesLog=""
 for module in ${modules[@]}; do
-    cd ${module}
-        gitAssertRepoClean
-
-        [[ ! $(gitAssertTagExists v${version})  ]] && logWarning "Could not find version tag v${version} in package: ${module}" && continue
-        moduleLog=`git log --pretty=oneline --decorate=no --invert-grep --grep="lint" --grep="version bumped" --grep="shit" --no-merges v${version}... | sed -E "s/[0-9a-f]*( .*)/  * \1\n/g"`
-
-        [[ ! "${moduleLog}" ]] && logInfo "No changes found in package: ${module}" && continue
-        submodulesLog="${submodulesLog}${module}:\n${moduleLog}\n\n"
-    cd ..
+    pushd ${module} > /dev/null
+        appendLog ${module}
+    popd > /dev/null
 done
 
-bash ./dev-tools/scripts/git/git-push.sh --this -m="${submodulesLog}"
+[[ ! ${dryRun} ]] && bash ./dev-tools/scripts/git/git-push.sh --this -m="${submodulesLog}"
 throwError "Error pushing Thunderstorm" $?
 
+[[ ${dryRun} ]] && logDebug "Found log message: ${submodulesLog}"
