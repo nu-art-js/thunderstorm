8065c73022ac2e06de9fdcccdb7699b906a7ebcb
Adam van der Kruk <Adam.Zehavi@nu-art-software.com>
2020-02-13 12:44:18 +0200

    firebase module was not structured as a ts lib (#3)

--- app-backend/src/main/modules/ValueChangedModule.ts
+++ app-backend/src/main/modules/ValueChangedModule.ts
@@ -15,7 +15,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-import {FirebaseFunctionModule,} from "@nu-art/firebase/functions";
+import {FirebaseFunctionModule,} from "@nu-art/firebase/backend-functions";
 
 export class NodeChangedModule_Class
 	extends FirebaseFunctionModule<any> {
