a20650c0d2790c5672fdfca474b74360f6d6c62e
adambunim-ir <adambunim@intuitionrobotics.com>
2020-03-25 12:58:34 +0200

    StopLongRecordings_Class

--- app-backend/src/main/modules/StopLongRecordings.ts
+++ app-backend/src/main/modules/StopLongRecordings.ts
@@ -1,11 +1,10 @@
-import {FirebaseScheduledFunction} from "@nu-art/firebase/app-backend/functions/firebase-function";
-
+import {FirebaseScheduledFunction} from "@nu-art/firebase/backend-functions";
 
 export class StopLongRecordings_Class
 	extends FirebaseScheduledFunction<any> {
 
 	constructor() {
-		super();
+		super("stopRecordings");
 		console.log("StopLongRecordings ctor")
 	}
 
