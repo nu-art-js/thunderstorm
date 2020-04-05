afb53f65d601ce0ceccab355f4fa44b351cca0d2
TacB0sS <adam.vdk@nu-art-software.com>
2020-04-05 13:03:56 +0300

    project backup schduler, copyright.. yes it should have been two commits...

--- app-backend/src/main/api/v1/firebase/list-collections.ts
+++ app-backend/src/main/api/v1/firebase/list-collections.ts
@@ -1,3 +1,21 @@
+/*
+ * A backend boilerplate with example apis
+ *
+ * Copyright (C) 2020 Adam van der Kruk aka TacB0sS
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 import {
 	ServerApi,
 	ApiResponse
@@ -20,7 +38,7 @@ class ServerApi_RegisterExternalProject
 	}
 
 	protected async process(request: express.Request, response: ApiResponse, queryParams: {}, body: void) {
-		return {list: Object.values(FirebaseModule.listCollectionsInModules())};
+		return {list: FirebaseModule.listCollectionsInModules()};
 	}
 }
 
--- app-backend/src/main/modules/SchedulerExample.ts
+++ app-backend/src/main/modules/SchedulerExample.ts
@@ -1,10 +1,29 @@
+/*
+ * A backend boilerplate with example apis
+ *
+ * Copyright (C) 2020 Adam van der Kruk aka TacB0sS
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 import {FirebaseScheduledFunction} from "@nu-art/firebase/backend-functions";
 
 export class SchedulerExample_Class
 	extends FirebaseScheduledFunction {
 
 	constructor() {
-		super('every 2 minutes', "schedulerExample");
+		super("schedulerExample");
+		this.setSchedule('every 2 minutes');
 	}
 
 	onScheduledEvent = async (): Promise<any> => {
