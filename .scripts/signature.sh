2942987ebfad7e21a53f65afae4074ee5890f2ee
TacB0sS <adam.vdk@nu-art-software.com>
2020-04-12 12:45:14 +0300

    build refactoring in progress

--- .scripts/signature.sh
+++ .scripts/signature.sh
@@ -2,11 +2,11 @@
 
 function signature() {
   clear
-  logVerbose "${Gray} -------     _____ _                     _                    _                            ------- ${NoColor}"
-  logVerbose "${Gray} -------    |_   _| |__  _   _ _ __   __| | ___ _ __      ___| |_ ___  _ __ _ __ ___    ${Gray}   ------- ${NoColor}"
-  logVerbose "${Gray} -------      | | | '_ \| | | | '_ \ / _\` |/ _ \ '__|____/ __| __/ _ \| '__| '_ \` _ \   ${Gray}   ------- ${NoColor}"
-  logVerbose "${Gray} -------      | | | | | | |_| | | | | (_| |  __/ | |_____\__ \ || (_) | |  | | | | | |  ${Gray}   ------- ${NoColor}"
-  logVerbose "${Gray} -------      |_| |_| |_|\__,_|_| |_|\__,_|\___|_|       |___/\__\___/|_|  |_| |_| |_|  ${Gray}   ------- ${NoColor}"
-  logVerbose "${Gray} -------                                                                                ${Gray}   ------- ${NoColor}"
+  logVerbose "${Gray} -------     _____ _                     _              _                            ------- ${NoColor}"
+  logVerbose "${Gray} -------    |_   _| |__  _   _ _ __   __| | ___ _ _____| |_ ___  _ __ _ __ ___    ${Gray}   ------- ${NoColor}"
+  logVerbose "${Gray} -------      | | | '_ \| | | | '_ \ / _\` |/ _ \ '__/__| __/ _ \| '__| '_ \` _ \   ${Gray}   ------- ${NoColor}"
+  logVerbose "${Gray} -------      | | | | | | |_| | | | | (_| |  __/ | \__ \ || (_) | |  | | | | | |  ${Gray}   ------- ${NoColor}"
+  logVerbose "${Gray} -------      |_| |_| |_|\__,_|_| |_|\__,_|\___|_| |___/\__\___/|_|  |_| |_| |_|  ${Gray}   ------- ${NoColor}"
+  logVerbose "${Gray} -------                                                                          ${Gray}   ------- ${NoColor}"
   logVerbose
 }
