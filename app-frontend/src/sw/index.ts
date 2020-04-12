b6ad16ab680b749e48da3bc2ed39f31802f4eb84
Alan Ben Seralvo <alan@intuitionrobotics.com>
2020-04-12 17:01:32 +0300

    other config? tsconfig inside?

--- app-frontend/src/sw/index.ts
+++ app-frontend/src/sw/index.ts
@@ -15,11 +15,23 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
+const config = {
+	PushPubSubModule: {
+		config: {
+			apiKey: "AIzaSyCoQjoQibuydMi1ejlpobfgHOI7WMf11P8",
+			authDomain: "nu-art-thunderstorm.firebaseapp.com",
+			projectId: "nu-art-thunderstorm",
+			messagingSenderId: "992823653177",
+			appId: "1:992823653177:web:e289e37f159c1b56de6ee8"
+		}
+	}
+}
 import {ServiceWorker} from '@nu-art/thunderstorm/sw';
 import {PushPubSubModule} from "@nu-art/push-pub-sub/sw";
 
 new ServiceWorker()
-	.setConfig(require("../main/config").config)
+	.setConfig(config)
 	.addModules(PushPubSubModule)
 	.build();
+
+export default null;
