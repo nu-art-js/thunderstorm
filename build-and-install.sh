59f23d4670f59620e872b86dff862348d76f6cbd
TacB0sS <adam.vdk@nu-art-software.com>
2020-05-19 13:41:37 +0300

    bai oos, live docs undo - redo as transaction

--- build-and-install.sh
+++ build-and-install.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-bash dev-tools/scripts/dev/typescript/build-and-install.sh "$@"
+echo "ZEVEL ${*}"
+if [[ "${2}" == "new" ]]; then
+  bash dev-tools/scripts/dev/typescript-oo/build-and-install.sh "${1}" "${@:3}"
+else
+  bash dev-tools/scripts/dev/typescript/build-and-install.sh "${@}"
+fi
