40290152e1d036fe5c4be2bf105b50462f7dea21
TacB0sS <adam.vdk@nu-art-software.com>
2020-05-19 18:13:48 +0300

    new bai script

--- build-and-install.sh
+++ build-and-install.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-echo "ZEVEL ${*}"
-if [[ "${2}" == "new" ]]; then
-  bash dev-tools/scripts/dev/typescript-oo/build-and-install.sh "${1}" "${@:3}"
+if [[ "${2}" == "old" ]]; then
+  echo Running old script
+  sleep 2s
+  bash dev-tools/scripts/dev/typescript/build-and-install.sh "${1}" "${@:3}"
 else
-  bash dev-tools/scripts/dev/typescript/build-and-install.sh "${@}"
+  bash dev-tools/scripts/dev/typescript-oo/build-and-install.sh "${@}"
 fi
