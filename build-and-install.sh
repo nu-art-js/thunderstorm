6ba9f6bd14fcb4513ed7adb0066a9638b6dbac7f
TacB0sS <adam.vdk@nu-art-software.com>
2020-05-19 18:28:51 +0300

    bad will still use old bai

--- build-and-install.sh
+++ build-and-install.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-if [[ "${2}" == "old" ]]; then
+if [[ "${1}" == "old" ]]; then
   echo Running old script
   sleep 2s
-  bash dev-tools/scripts/dev/typescript/build-and-install.sh "${1}" "${@:3}"
+  bash dev-tools/scripts/dev/typescript/build-and-install.sh "${@:2}"
 else
   bash dev-tools/scripts/dev/typescript-oo/build-and-install.sh "${@}"
 fi
