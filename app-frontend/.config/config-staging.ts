2b3c0f464d8850e5026ff52d105c9171e4864fad
Alan Ben Seralvo <alan@intuitionrobotics.com>
2020-06-09 18:42:02 +0300

    staging with configs

--- app-frontend/.config/config-staging.ts
+++ app-frontend/.config/config-staging.ts
@@ -28,8 +28,20 @@ export const config = {
 	ExampleModule: {
 		remoteUrl: "/v1/sample/endpoint-example"
 	},
+	FirebaseModule: {
+		local: {
+			apiKey: "AIzaSyD2xhGl4-gq3L_rknxoYF0KZLvedrFWQbg",
+			authDomain: "thunderstorm-staging.firebaseapp.com",
+			databaseURL: "https://thunderstorm-staging.firebaseio.com",
+			projectId: "thunderstorm-staging",
+			storageBucket: "thunderstorm-staging.appspot.com",
+			messagingSenderId: "387990980732",
+			appId: "1:387990980732:web:62ce3fe05f0fc852faa1f9",
+			measurementId: "G-PSCS2QH5YV"
+		}
+	},
 	PushPubSubModule: {
-		publicKeyBase64: 'BH_Awj-nfzwZBMnzRgyfQN2mIZPbxV9JqLWljlQcJbHtz-2IxKPvon78Wt_NLF0yn4QkMIErSlqkAm8T75ySM84'
+		publicKeyBase64: 'BBsKBw0R-mITlCSAOtCiHCLvKl-EetCmt5JKMg8L8ev1GqBEpDryum8ve3htIlbN3cjV1MLDFQnk0a8Wfks7cFk'
 	},
 	ForceUpgrade: {
 		assertVersionUrl: "/v1/version/assert"
