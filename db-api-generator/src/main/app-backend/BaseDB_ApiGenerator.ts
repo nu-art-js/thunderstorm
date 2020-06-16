41cc40b9f1bd32b74971ef2c43fee7a21df42191
Ilias Bimpas <ilias@intuitionrobotics.com>
2020-06-16 18:15:24 +0300

    Add documentation for BaseDB_ApiGenerator

--- db-api-generator/src/main/app-backend/BaseDB_ApiGenerator.ts
+++ db-api-generator/src/main/app-backend/BaseDB_ApiGenerator.ts
@@ -78,7 +78,11 @@ export type Config<Type extends object> = {
 	externalFilterKeys: FilterKeys<Type>
 }
 
-
+/**
+ * An abstract base class used for implementing CRUD operations on a specific collection.
+ *
+ * By default, it exposes API endpoints for creating, deleting, updating, querying and querying for unique document.
+ */
 export abstract class BaseDB_ApiGenerator<DBType extends DB_Object, ConfigType extends Config<DBType> = Config<DBType>, UType extends PartialProperties<DBType, "_id"> = PartialProperties<DBType, "_id">>
 	extends Module<ConfigType> {
 
@@ -98,6 +102,18 @@ export abstract class BaseDB_ApiGenerator<DBType extends DB_Object, ConfigType e
 
 	// this.setExternalUniqueKeys(["accessLevelIds"]);
 
+	/**
+	 * Sets the external unique keys. External keys are the attributes of a document that must be unique inside the
+	 * collection. Default is `_id`.
+	 *
+	 * @remarks
+	 * You can only update the external unique keys before the module is initialized, preferably from its constructor.
+	 *
+	 * @param keys - The external unique keys.
+	 *
+	 * @returns
+	 * The external filter keys.
+	 */
 	protected setExternalUniqueKeys(keys: FilterKeys<DBType>) {
 		if (this.initiated)
 			throw new BadImplementationException("You can only update the 'externalUniqueKeys' before the module was initialized.. preferably from its constructor");
@@ -105,6 +121,17 @@ export abstract class BaseDB_ApiGenerator<DBType extends DB_Object, ConfigType e
 		return this.config.externalFilterKeys = keys;
 	}
 
+	/**
+	 * Sets the patch keys. Patch keys are the attributes that are allowed to be changed in a document.
+	 *
+	 * @remarks
+	 * You can only update the patch keys before the module is initialized, preferably from its constructor.
+	 *
+	 * @param keys - The patch keys.
+	 *
+	 * @returns
+	 * The patch keys.
+	 */
 	protected setPatchKeys(keys: (keyof DBType)[]) {
 		if (this.initiated)
 			throw new BadImplementationException("You can only update the 'patchKeys' before the module was initialized.. preferably from its constructor");
@@ -120,6 +147,10 @@ export abstract class BaseDB_ApiGenerator<DBType extends DB_Object, ConfigType e
 		return this.config.itemName;
 	}
 
+	/**
+	 * Executed during the initialization of the module.
+	 * The collection reference is set in this method.
+	 */
 	init() {
 		const firestore = FirebaseModule.createAdminSession(this.config?.projectId).getFirestore();
 		// @ts-ignore
@@ -136,6 +167,14 @@ export abstract class BaseDB_ApiGenerator<DBType extends DB_Object, ConfigType e
 		return dbInstance;
 	}
 
+	/**
+	 * Asserts the uniqueness of an instance in two steps:
+	 * - Executes `this.assertCustomUniqueness`.
+	 * - Asserts uniqueness based on the internal filters.
+	 *
+	 * @param transaction - The transaction object.
+	 * @param instance - The document for which the uniqueness assertion will occur.
+	 */
 	public async assertUniqueness(transaction: FirestoreTransaction, instance: DBType) {
 		await this.assertCustomUniqueness(transaction, instance);
 
@@ -156,6 +195,13 @@ export abstract class BaseDB_ApiGenerator<DBType extends DB_Object, ConfigType e
 		}
 	}
 
+	/**
+	 * Runs the module's validator for the instance.
+	 *
+	 * @param instance - The object to be validated.
+	 *
+	 * @throws `ApiException` for bad implementation or invalid input.
+	 */
 	public async validateImpl(instance: DBType) {
 		try {
 			await validate(instance, this.validator);
@@ -173,16 +219,44 @@ export abstract class BaseDB_ApiGenerator<DBType extends DB_Object, ConfigType e
 		}
 	}
 
+	/**
+	 * Override this method to return a list of "where" queries that dictate uniqueness inside the collection.
+	 * Example return value: [{attribute1: item.attribute1, attribute2: item.attribute2}].
+	 *
+	 * @param item - The DB entry that will be used.
+	 */
 	protected internalFilter(item: DBType): Clause_Where<DBType>[] {
 		return [];
 	}
 
+	/**
+	 * Override this method to customize the assertions that should be done before the insertion of the document to the DB.
+	 *
+	 * @param transaction - The transaction object.
+	 * @param dbInstance - The DB entry for which the uniqueness is being asserted.
+	 */
 	protected async assertCustomUniqueness(transaction: FirestoreTransaction, dbInstance: DBType) {
 	}
 
+	/**
+	 * Override this method to provide actions or assertions to be executed before the deletion happens.
+	 *
+	 * Currently executed only before `deleteUnique()`.
+	 *
+	 * @param transaction - The transaction object
+	 * @param dbInstance - The DB entry that is going to be deleted.
+	 */
 	protected async assertDeletion(transaction: FirestoreTransaction, dbInstance: DBType) {
 	}
 
+	/**
+	 * A wrapper of the collections's `runInTransaction`.
+	 *
+	 * @param processor - The transaction's processor.
+	 *
+	 * @returns
+	 * A promise of the result of the `processor`.
+	 */
 	async runInTransaction<ReturnType>(processor: (transaction: FirestoreTransaction) => Promise<ReturnType>): Promise<ReturnType> {
 		return this.collection.runInTransaction(processor);
 	}
@@ -192,10 +266,27 @@ export abstract class BaseDB_ApiGenerator<DBType extends DB_Object, ConfigType e
 		await this.collection.deleteAll();
 	}
 
+	/**
+	 * Inserts the `instance` using the `transaction` object.
+	 *
+	 * @param transaction - The transaction object.
+	 * @param instance - The object to be inserted.
+	 *
+	 * @returns
+	 * A promise of the document that was inserted.
+	 */
 	async insertImpl(transaction: FirestoreTransaction, instance: UType) {
 		return transaction.insert(this.collection, {...instance, _id: generateHex(idLength)} as unknown as DBType);
 	}
 
+	/**
+	 * Upserts the `instance` using a transaction, after validating it and asserting uniqueness.
+	 *
+	 * @param instance - The object to be upserted.
+	 *
+	 * @returns
+	 * A promise of the document that was upserted.
+	 */
 	async upsert(instance: UType) {
 		return this.collection.runInTransaction(async (transaction) => {
 			const dbInstance: DBType = {...instance, _id: instance._id || generateHex(idLength)} as unknown as DBType;
@@ -205,6 +296,14 @@ export abstract class BaseDB_ApiGenerator<DBType extends DB_Object, ConfigType e
 		});
 	}
 
+	/**
+	 * Upserts a set of objects. Batching is used to circumvent firestore limitations on the number of objects.
+	 *
+	 * @param instances - The objects to be upserted.
+	 *
+	 * @returns
+	 * A promise of an array of documents that were upserted.
+	 */
 	async upsertAll(instances: UType[]): Promise<DBType[]> {
 		const writes: DBType[] = [];
 		await batchAction(instances, 500, async chunked => {
@@ -213,7 +312,7 @@ export abstract class BaseDB_ApiGenerator<DBType extends DB_Object, ConfigType e
 		return writes;
 	}
 
-	async upsertAllBatched(instances: UType[]): Promise<DBType[]> {
+	private async upsertAllBatched(instances: UType[]): Promise<DBType[]> {
 		return this.collection.runInTransaction(async (transaction) => {
 			const dbInstances: DBType[] = instances.map(instance => ({...instance, _id: instance._id || generateHex(idLength)} as unknown as DBType));
 			await Promise.all(dbInstances.map(async dbInstance => this.validateImpl(dbInstance)));
@@ -223,14 +322,44 @@ export abstract class BaseDB_ApiGenerator<DBType extends DB_Object, ConfigType e
 		});
 	}
 
+	/**
+	 * Upserts the `dbInstance` using the `transaction` transaction object.
+	 *
+	 * @param transaction - The transaction object.
+	 * @param dbInstance - The object to be upserted.
+	 *
+	 * @returns
+	 * A promise of the document that was upserted.
+	 */
 	protected async upsertImpl(transaction: FirestoreTransaction, dbInstance: DBType): Promise<DBType> {
 		return transaction.upsert(this.collection, dbInstance);
 	}
 
+	/**
+	 * Upserts the `dbInstances` using the `transaction` object.
+	 *
+	 * @param transaction - The transaction object.
+	 * @param dbInstance - The set of objects to be upserted.
+	 *
+	 * @throws `BadImplementationException` when the instances are more than 500.
+	 *
+	 * @returns
+	 * A promise of the array of documents that were upserted.
+	 */
 	protected async upsertAllImpl(transaction: FirestoreTransaction, dbInstances: DBType[]): Promise<DBType[]> {
 		return transaction.upsertAll(this.collection, dbInstances);
 	}
 
+	/**
+	 * Deletes a unique document based on its `_id`. Uses a transaction, after deletion assertions occur.
+	 *
+	 * @param _id - The _if of th object to be deleted.
+	 *
+	 * @throws `ApiException` when the document doesn't exist in the collection.
+	 *
+	 * @returns
+	 * A promise of the document that was deleted.
+	 */
 	async deleteUnique(_id: string) {
 		return this.collection.runInTransaction(async (transaction: FirestoreTransaction) => {
 			const ourQuery = {where: {_id} as Clause_Where<DBType>};
@@ -244,14 +373,38 @@ export abstract class BaseDB_ApiGenerator<DBType extends DB_Object, ConfigType e
 		});
 	}
 
+	/**
+	 * Uses the `transaction` to delete a unique document, querying with the `ourQuery`.
+	 *
+	 * @param transaction
+	 * @param ourQuery
+	 *
+	 * @returns
+	 * A promise of the document that was deleted.
+	 */
 	private async deleteImpl(transaction: FirestoreTransaction, ourQuery: { where: Clause_Where<DBType> }) {
-		await transaction.deleteUnique(this.collection, ourQuery);
+		return transaction.deleteUnique(this.collection, ourQuery);
 	}
 
+	/**
+	 * Calls the `delete` method of the module's collection.
+	 *
+	 * @param query - The query to be executed for the deletion.
+	 */
 	async delete(query: FirestoreQuery<DBType>) {
 		return this.collection.delete(query);
 	}
 
+	/**
+	 * Queries the database for a specific document in the module's collection.
+	 *
+	 * @param where - The where clause to be used for querying.
+	 *
+	 * @throws `ApiException` if the document is not found.
+	 *
+	 * @returns
+	 * The DB document that was found.
+	 */
 	async queryUnique(where: Clause_Where<DBType>) {
 		const dbItem = await this.collection.queryUnique({where});
 		if (!dbItem)
@@ -260,10 +413,28 @@ export abstract class BaseDB_ApiGenerator<DBType extends DB_Object, ConfigType e
 		return dbItem;
 	}
 
+	/**
+	 * Executes the specified query on the module's collection.
+	 *
+	 * @param query - The query to be executed.
+	 *
+	 * @returns
+	 * A promise of an array of documents.
+	 */
 	async query(query: FirestoreQuery<DBType>) {
 		return await this.collection.query(query);
 	}
 
+	/**
+	 * Executes `this.upsertImpl` using an instance that is formed by the `_props` and the defined patch keys.
+	 * Uses a transaction to execute the patch.
+	 *
+	 * @param instance - The initial instance to be upserted.
+	 * @param _props - The attributes to be changed.
+	 *
+	 * @returns
+	 * A promise of the document that was patched.
+	 */
 	async patch(instance: DBType, _props?: (keyof DBType)[]): Promise<DBType> {
 		return this.collection.runInTransaction(async (transaction) => {
 			const dbInstance: DBType = await this.assertExternalQueryUnique(instance, transaction);
@@ -284,6 +455,14 @@ export abstract class BaseDB_ApiGenerator<DBType extends DB_Object, ConfigType e
 		});
 	}
 
+	/**
+	 * Override this method, to control which server api endpoints are created automatically.
+	 *
+	 * @param pathPart - The path part.
+	 *
+	 * @returns
+	 * An array of api endpoints.
+	 */
 	apis(pathPart?: string): ServerApi<any>[] {
 		return [
 			new ServerApi_Create(this, pathPart),
