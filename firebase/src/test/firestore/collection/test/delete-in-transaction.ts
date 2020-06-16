80c39fd3ccdba4e764de4580587962f81f4bd851
Alan Ben Seralvo <alan@intuitionrobotics.com>
2020-06-16 11:05:48 +0300

    test should fail

--- firebase/src/test/firestore/collection/test/delete-in-transaction.ts
+++ firebase/src/test/firestore/collection/test/delete-in-transaction.ts
@@ -20,7 +20,7 @@
 import {simpleTypeCollection} from "../_core/consts";
 import {__scenario} from "@nu-art/testelot";
 import {
-	assert,
+	BadImplementationException,
 	generateHex
 } from "@nu-art/ts-common";
 
@@ -28,9 +28,9 @@ export const scenarioCollectionDeleteInTransaction = __scenario("Delete In Trans
 const deleteId = generateHex(8);
 const n = 600;
 
-scenarioCollectionDeleteInTransaction.add(simpleTypeCollection.processClean("Add Elements", async (collection) => {
+scenarioCollectionDeleteInTransaction.add(simpleTypeCollection.processClean(`Addding ${n} Elements`, async (collection) => {
 
-	console.log(`Inserting ${n} elements....`);
+	// console.log(`Inserting ${n} elements....`);
 	const instances = new Array(n).fill(0).map(e => ({
 		label: generateHex(16),
 		deleteId
@@ -40,15 +40,15 @@ scenarioCollectionDeleteInTransaction.add(simpleTypeCollection.processClean("Add
 	// await collection.runInTransaction(transaction => {
 	// 	return transaction.insertAll(collection,instances)
 	// })
-	console.log(`Inserted ${n} elements....`);
-
-	console.log('Deleting a ton of documents');
-	await collection.runInTransaction(transaction => {
-
-		return transaction.delete(collection, {where: {deleteId: deleteId}});
-	});
-	console.log('Deleted a ton of documents');
-
-	assert("Expected db to be empty", (await collection.getAll()).length, 0);
+	// console.log(`Inserted ${n} elements....`);
 }));
 
+scenarioCollectionDeleteInTransaction.add(simpleTypeCollection.processDirty("Trying to Delete Elements", async (collection) => {
+	// console.log('Deleting a ton of documents');
+	await collection.runInTransaction(transaction => {
+		return transaction.delete(collection, {where: {deleteId: deleteId}});
+	});
+	// console.log('Deleted a ton of documents');
+
+	// assert("Expected db to be empty", (await collection.getAll()).length, 0);
+}).expectToFail(BadImplementationException,(e: Error) => e.message.toLowerCase().startsWith("trying to delete")));
