b4f0cc805f2ad84b53d908b01818b5834f1a5ff0
Alan Ben Seralvo <alan@intuitionrobotics.com>
2020-06-17 15:06:22 +0300

    compile

--- app-frontend/src/main/app/pages/Page_Playground.tsx
+++ app-frontend/src/main/app/pages/Page_Playground.tsx
@@ -23,17 +23,16 @@ import {
 } from "@nu-art/thunderstorm/frontend";
 import {Page_ApiGen} from "./Page_ApiGen";
 import {Hello} from "../Hello";
-import {Example_ApiCustomError} from '../playground/Example_ApiCustomError';
-import {Example_Tabs2} from "../playground/tabs/Example_Tabs2";
-import {Example_GenericSelect} from "../playground/_to-be-removed/Example_GenericSelect";
-import {Example_Tabs} from "../playground/tabs/Example_Tabs";
-import {Example_KeyboardOnTree} from '../playground/keyboard-listener/Example_KeyboardOnTree';
-import {Example_Dialogs} from '../playground/Example_Dialogs';
-import {Example_Toaster} from '../playground/Example_Toaster';
-import {Example_Menu} from '../playground/Example_Menu';
-import {Example_DropDown} from "../playground/Example_DropDown";
-import {selectStyles} from "../ui/SelectStyle";
+import {Example_ApiCustomError} from '../playground/examples/Example_ApiCustomError';
+import {Example_Tabs2} from "../playground/examples/tabs/Example_Tabs2";
+import {Example_GenericSelect} from "../playground/examples/_to-be-removed/Example_GenericSelect";
+import {Example_Tabs} from "../playground/examples/tabs/Example_Tabs";
+import {Example_Dialogs} from '../playground/examples/Example_Dialogs';
+import {Example_Toaster} from '../playground/examples/Example_Toaster';
+import {Example_Menu} from '../playground/examples/Example_Menu';
+import {Example_DropDown} from "../playground/examples/Example_DropDown";
 import {Example_ErrorBoundary} from "../playground/Example_ErrorBoundary";
+import {selectStyles} from "../playground/Page_Playground";
 
 const icon__arrowClose = require('@res/images/icon__arrowClose.svg');
 const icon__arrowOpen = require('@res/images/icon__arrowOpen.svg');
@@ -60,55 +59,55 @@ export class Page_Playground
 		return [
 			{
 				name: "Hello",
-				getNode: () => <Hello/>
+				renderer: Hello
 			},
 			{
 				name: "Dialog Examples",
-				getNode: () => <Example_Dialogs/>
+				renderer: Example_Dialogs
 			},
 			{
 				name: "Toaster Examples",
-				getNode: () => <Example_Toaster/>
+				renderer: Example_Toaster
 			},
 			{
 				name: "Api Generator",
-				getNode: () => <Page_ApiGen/>
+				renderer: Page_ApiGen
 			},
 			{
 				name: "Live docs",
-				getNode: () => <Hello/>
+				renderer: Hello
 			},
 			{
 				name: "Custom error",
-				getNode: () => <Example_ApiCustomError/>
+				renderer: Example_ApiCustomError
 			},
 			{
 				name: "Tabs",
-				getNode: () => <Example_Tabs2/>
+				renderer: Example_Tabs2
 			},
 			{
 				name: "GenericTabs",
-				getNode: () => <Example_Tabs/>
+				renderer: Example_Tabs
 			},
 			{
 				name: "GenericSelect",
-				getNode: () => <Example_GenericSelect/>
+				renderer: Example_GenericSelect
 			},
 			{
 				name: "DropDown Examples",
-				getNode: () => <Example_DropDown/>
+				renderer: Example_DropDown
 			},
 			{
 				name: "Menu",
-				getNode: () => <Example_Menu/>
+				renderer: Example_Menu
 			},
-			{
-				name: "Special keyboard listener",
-				getNode: () => <Example_KeyboardOnTree/>
-			},
+			// {
+			// 	name: "Special keyboard listener",
+			// 	renderer: Example_KeyboardOnTree
+			// },
 			{
 				name: "Error boundary",
-				getNode: () => <Example_ErrorBoundary/>
+				renderer: Example_ErrorBoundary
 			}
 		];
 	}
