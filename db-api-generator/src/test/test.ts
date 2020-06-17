cd51574114ff93f3359777bf25ca064f58b6100a
Ilias Bimpas <ilias@intuitionrobotics.com>
2020-06-17 17:15:07 +0300

    Add tests to db-ap-generator

---
+++ db-api-generator/src/test/test.ts
@@ -0,0 +1,36 @@
+/*
+ * Permissions management system, define access level for each of
+ * your server apis, and restrict users by giving them access levels
+ *
+ * Copyright (C) 2020 Adam van der Kruk aka TacB0sS
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import {StormTester} from "@nu-art/thunderstorm/backend-test";
+import {__scenario} from "@nu-art/testelot";
+import { FirebaseModule } from "@nu-art/firebase/backend";
+import {
+	ExampleModule,
+	patchTest
+} from "./tests/db-api-generator";
+
+export const mainScenario = __scenario("Tests for base-db-api-generator.");
+
+mainScenario.add(patchTest());
+
+module.exports = new StormTester()
+	.addModules(FirebaseModule)
+	.addModules(ExampleModule)
+	.setScenario(mainScenario)
+	.build();
