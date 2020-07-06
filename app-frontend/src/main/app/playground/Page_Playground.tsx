b0e221e6cc913d90e16e0e6e79a454cf29b28ba9
TacB0sS <adam.vdk@nu-art-software.com>
2020-07-06 22:22:55 +0300

    adapter multi nested type with objects

--- app-frontend/src/main/app/playground/Page_Playground.tsx
+++ app-frontend/src/main/app/playground/Page_Playground.tsx
@@ -43,7 +43,10 @@ import {Example_MultiRendererDropDown} from "./examples/dropdown/Example_MultiRe
 import {Example_List_SingleType} from "./examples/list/Example_List_SingleType";
 import {Example_List_MultiType} from "./examples/list/Example_List_MultiType";
 import {Example_NestedList_SingleType} from './examples/list/Example_NestedList_SingleType';
-import {Example_NestedList_MultiType} from './examples/list/Example_NestedList_MultiType';
+import {
+	Example_NestedList_MultiType,
+	Example_NestedList_MultiType_Object
+} from './examples/list/Example_NestedList_MultiType';
 import {Example_List_All} from "./examples/list/Example_List_All";
 import {Example_Tree_SingleType} from './examples/tree/Example_Tree_SingleType';
 
@@ -101,11 +104,12 @@ export class Page_Playground
 
 	getScreens(): PlaygroundScreen[] {
 		return [
-			{renderer: Example_Tree_SingleType, name: "Example_Tree_SingleType",},
+			{renderer: Example_Tree_SingleType, name: "Tree_SingleType",},
 			{renderer: Example_List_SingleType, name: "List_SingleType_Menu",},
 			{renderer: Example_NestedList_SingleType, name: "List_SingleType_NestedMenu",},
 			{renderer: Example_List_MultiType, name: "List_MultiType_Menu",},
-			{renderer: Example_NestedList_MultiType, name: "List_MultiType_NestedMenu",},
+			{renderer: Example_NestedList_MultiType, name: "NestedList_MultiType",},
+			{renderer: Example_NestedList_MultiType_Object, name: "NestedList_MultiType_Object",},
 			{renderer: Example_List_All, name: "Example_List_All",},
 
 			{renderer: Example_Form, name: "Form - Register",},
