47bd53e0cf294c926ee0b089e8793188d910ff43
Alan Ben Seralvo <alan@intuitionrobotics.com>
2020-07-23 18:07:25 +0300

    keyevent handled in ts_input when focused

--- thunderstorm/src/main/app-frontend/components/TS_Input.tsx
+++ thunderstorm/src/main/app-frontend/components/TS_Input.tsx
@@ -51,15 +51,13 @@ export class TS_Input<Key extends string>
 	};
 
 	private handleKeyEvent = (ev: KeyboardEvent) => {
-		if (this.props.onAccept && ev.which === 13) {
+		if (this.props.onAccept && ev.which === 13)
 			this.props.onAccept();
-			ev.stopPropagation();
-		}
 
-		if (this.props.onCancel && ev.which === 27) {
+		if (this.props.onCancel && ev.which === 27)
 			this.props.onCancel();
-			ev.stopPropagation();
-		}
+
+		ev.stopPropagation();
 	};
 
 	private onDoubleClick(ev: React.MouseEvent) {
