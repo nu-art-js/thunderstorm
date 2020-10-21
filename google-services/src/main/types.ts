f58ad106cd8eeb87b226f653ba22348e9cc0a750
Ilias Bimpas <ilias@intuitionrobotics.com>
2020-10-21 16:04:38 +0300

    Export intent type from google api

---
+++ google-services/src/main/types.ts
@@ -0,0 +1,23 @@
+/*
+ * Permissions management system, define access level for each of
+ * your server apis, and restrict users by giving them access levels
+ *
+ * Copyright (C) 2020 Adam van der Kruk aka TacB0sS
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import {dialogflow_v2} from "googleapis";
+import Schema$GoogleCloudDialogflowV2Intent = dialogflow_v2.Schema$GoogleCloudDialogflowV2Intent;
+
+export type TS_Intent = Schema$GoogleCloudDialogflowV2Intent;
