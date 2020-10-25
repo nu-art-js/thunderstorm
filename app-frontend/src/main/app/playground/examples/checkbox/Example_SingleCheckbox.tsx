01bb1fb245f3b6e8fe08b607262314e4d747968f
Tzuri IR <tzuri@intuitionrobotics.com>
2020-10-25 16:05:05 +0200

    wonderful new checkbox fieldgs

--- app-frontend/src/main/app/playground/examples/checkbox/Example_SingleCheckbox.tsx
+++ app-frontend/src/main/app/playground/examples/checkbox/Example_SingleCheckbox.tsx
@@ -1,3 +1,22 @@
+/*
+ * Permissions management system, define access level for each of
+ * your server apis, and restrict users by giving them access levels
+ *
+ * Copyright (C) 2020 Adam van der Kruk aka TacB0sS
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 import * as React from 'react';
 import {TS_Checkbox} from '@nu-art/thunderstorm/frontend';
 import {css} from 'emotion';
@@ -10,7 +29,8 @@ type State = {
     f4: boolean
 }
 
-export class Example_SingleCheckbox extends React.Component<{}, State> {
+export class Example_SingleCheckbox
+    extends React.Component<{}, State> {
     state = {f1: false, f2: true, f3: true, f4: false};
 
     render() {
