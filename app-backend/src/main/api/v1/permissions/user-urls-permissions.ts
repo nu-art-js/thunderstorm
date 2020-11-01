989dde8839d3827c0561f17ee498c82338916520
Alan Ben Seralvo <alan@intuitionrobotics.com>
2020-11-01 17:04:40 +0200

    loading component in permissions

---
+++ app-backend/src/main/api/v1/permissions/user-urls-permissions.ts
@@ -0,0 +1,43 @@
+/*
+ * A backend boilerplate with example apis
+ *
+ * Copyright (C) 2020 Adam van der Kruk aka TacB0sS
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import {
+	ApiResponse,
+	ExpressRequest,
+	ServerApi
+} from "@nu-art/thunderstorm/backend";
+import {HttpMethod} from "@nu-art/thunderstorm";
+import {
+	PermissionsApi_UserUrlsPermissions,
+	Request_UserUrlsPermissions
+} from "@nu-art/permissions";
+
+class ServerApi_UserUrlsPermissions
+	extends ServerApi<PermissionsApi_UserUrlsPermissions> {
+
+	constructor() {
+		super(HttpMethod.POST, "user-urls-permissions");
+		this.dontPrintResponse();
+	}
+
+	protected async process(request: ExpressRequest, response: ApiResponse, queryParams: {}, body: Request_UserUrlsPermissions) {
+		return {'/v1/test/api': true};
+	}
+}
+
+module.exports = new ServerApi_UserUrlsPermissions();
+
