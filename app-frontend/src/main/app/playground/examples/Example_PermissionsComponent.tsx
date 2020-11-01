614d3d120b2eebdd91b3367ee74ada4cb32dae6e
adambunim-ir <adambunim@intuitionrobotics.com>
2020-11-01 14:00:52 +0200

    PermissionComponent playground

--- app-frontend/src/main/app/playground/examples/Example_PermissionsComponent.tsx
+++ app-frontend/src/main/app/playground/examples/Example_PermissionsComponent.tsx
@@ -1,9 +1,13 @@
 import * as React from 'react';
+import {PermissionsComponent} from "@nu-art/permissions/app-frontend/modules/PermissionsComponent";
 
 export class Example_PermissionsComponent extends React.Component {
 
 	render() {
-		return "Example_PermissionsComponent"
+		return <PermissionsComponent url={"v1/blat/query"}
+		                      fallback={() => <div>not allowed</div>}>
+			<div>Example_PermissionsComponent</div>
+		</PermissionsComponent>
 	}
 
 }
