d56b90ade225f421384413d67b7b70e1bb134061
adambunim-ir <adambunim@intuitionrobotics.com>
2020-11-15 18:59:31 +0200

    Analytics playground

--- app-frontend/src/main/app/playground/examples/Example_Analytics.tsx
+++ app-frontend/src/main/app/playground/examples/Example_Analytics.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import {AnalyticsModule} from "@nu-art/analytics/app-frontend/modules/AnalyticsModule";
 
 export class Example_Analytics extends React.Component {
 
@@ -9,7 +10,8 @@ export class Example_Analytics extends React.Component {
 	}
 
 	addEvent = () => {
-		console.log("adding event...")
+		AnalyticsModule.logEvent("test")
+		console.log("event added")
 	}
 
 }
