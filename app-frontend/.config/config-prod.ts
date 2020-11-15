76f72d67ac80c09ade925ac7cd11449eba2da941
Alan Ben Seralvo <alan@intuitionrobotics.com>
2020-11-15 11:36:10 +0200

    prod config

--- app-frontend/.config/config-prod.ts
+++ app-frontend/.config/config-prod.ts
@@ -31,6 +31,18 @@ export const config = {
 	ForceUpgrade: {
 		assertVersionUrl: "/v1/version/assert"
 	},
+	FirebaseModule: {
+		local: {
+			apiKey: "AIzaSyCoQjoQibuydMi1ejlpobfgHOI7WMf11P8",
+			authDomain: "nu-art-thunderstorm.firebaseapp.com",
+			databaseURL: "https://nu-art-thunderstorm.firebaseio.com",
+			projectId: "nu-art-thunderstorm",
+			storageBucket: "nu-art-thunderstorm.appspot.com",
+			messagingSenderId: "992823653177",
+			appId: "1:992823653177:web:e289e37f159c1b56de6ee8",
+			measurementId: "G-CBR3QM4STY"
+		}
+	},
 	PushPubSubModule: {
 		publicKeyBase64: 'BF0GqqEoe1UmqcU-dg3Dse_2ctkaq5uFpFuR6il1U9A3HkvYcL83I8yC_rX-G8mM8M0hnH5TqcSIsHScd4LTS28'
 	},
