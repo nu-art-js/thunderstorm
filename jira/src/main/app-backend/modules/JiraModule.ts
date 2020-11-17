df647b8a90ba908a5eeb1de3a696736d7f64febe
TacB0sS <adam.vdk@nu-art-software.com>
2020-11-17 12:55:51 +0200

    updated jira api

--- jira/src/main/app-backend/modules/JiraModule.ts
+++ jira/src/main/app-backend/modules/JiraModule.ts
@@ -19,7 +19,9 @@
 import {
 	ImplementationMissingException,
 	Module,
-	StringMap
+	StringMap,
+	TypedMap,
+	_keys
 } from "@nu-art/ts-common";
 import {
 	ApiException,
@@ -68,22 +70,14 @@ type JiraFields = {
 
 export type IssueType = {
 	id: string
-} | {
 	name: string
 }
 
 export type JiraProjectInfo = {
 	id: string
-} | {
 	name: string
-} | {
 	key: string
 }
-
-export type ResponseGetIssue = BaseIssue & {
-	expand: string
-	fields: StringMap
-};
 
 export type BaseIssue = {
 	id: string
@@ -91,10 +85,28 @@ export type BaseIssue = {
 	self: string
 };
 
+export type JiraIssue = BaseIssue & {
+	expand: string
+	fields: JiraFields & StringMap
+};
+
 export type FixVersionType = {
 	fixVersions: { name: string }[]
 };
 
+export type JiraQuery = TypedMap<string | string[]> & {
+	status?: string | string[]
+	project?: string | string[]
+	fixVersion?: string | string[]
+};
+
+export type JiraQueryResponse = {
+	expand: string,
+	startAt: number,
+	maxResults: number,
+	total: number,
+	issues: JiraIssue[]
+}
 export type ResponsePostIssue = BaseIssue;
 
 const createFormData = (filename: string, buffer: Buffer) => ({file: {value: buffer, options: {filename}}});
@@ -103,14 +115,13 @@ export class JiraModule_Class
 	extends Module<Config> {
 	private headersJson!: Headers;
 	private headersForm!: Headers;
-	private baseUrl = "https://introb.atlassian.net/rest/api/3";
 
 	protected init(): void {
-		if (this.config?.baseUrl)
-			this.baseUrl = this.config?.baseUrl;
+		if (!this.config.baseUrl)
+			throw new ImplementationMissingException("Missing Jira subdomain")
 
-		if (!this.config?.auth?.email || !this.config.auth.apiKey)
-			throw new ImplementationMissingException('Missing right config variables for JiraModule');
+		if (!this.config.auth || !this.config.auth.apiKey || !this.config.auth.email)
+			throw new ImplementationMissingException('Missing auth config variables for JiraModule');
 
 		this.headersJson = this.buildHeaders(this.config.auth, true);
 		this.headersForm = this.buildHeaders(this.config.auth, false);
@@ -176,25 +187,30 @@ export class JiraModule_Class
 		return this.executePutRequest(`/issue/${issueKey}`, this.createVersionBody(data));
 	};
 
-	private buildSearch = (params: StringMap) => {
-		const search = Object.keys(params).reduce((carry, key) => {
-			return `${carry}${carry.length !== 0 ? '%20and%20' : ''}${key}${key === 'project' ? '=' : '~'}${encodeURIComponent(params[key])}`
-		}, '');
-		return 'jql=' + (search);
+	private buildQuery = (query: JiraQuery) => {
+		const params = _keys(query).map((key) => {
+			let queryValue;
+			if (Array.isArray(query[key])) {
+				queryValue = (query[key] as string[]).map(value => `"${value}"`).join(",");
+				queryValue = `(${queryValue})`
+			} else
+				queryValue = `"${query[key]}"`
+
+			return `${key}=${queryValue}`;
+		});
+		return params.join(" and ")
 	};
 
-	getIssueByCustomField = async (project: string, query: StringMap) => {
-		// return this.executeGetRequest('/search?jql=summary~'+summary+'&project='+project)
-		const search = this.buildSearch({project, ...query});
-		return this.executeGetRequest(`/search?${search}`)
+	query = async (query: JiraQuery): Promise<JiraIssue[]> => {
+		const jql = this.buildQuery(query);
+		return (await this.executeGetRequest<JiraQueryResponse>(`/search`, {jql})).issues;
 	};
-
 
 	postIssueRequest = async (project: JiraProjectInfo, issueType: IssueType, summary: string, description: string): Promise<ResponsePostIssue> => {
-		return this.executePostRequest('/issue', this.createBody(project, issueType, summary, description));
+		return this.executePostRequest<ResponsePostIssue>('/issue', this.createBody(project, issueType, summary, description));
 	};
 
-	getIssueRequest = async (issue: string): Promise<ResponseGetIssue> => {
+	getIssueRequest = async (issue: string): Promise<JiraIssue> => {
 		return this.executeGetRequest(`/issue/${issue}`)
 	};
 
@@ -211,44 +227,31 @@ export class JiraModule_Class
 		return this.executePostRequest(`/issue/${issue}/comment`, obj)
 	};
 
-
-	// editDescriptionRequest = (issue: string, description: string) => {
-	// 	// No idea
-	// 	const request: UriOptions & CoreOptions = {
-	// 		headers: this.headersJson,
-	// 		uri: '/issue/' + issue,
-	// 		method: 'PUT',
-	// 		body: description,
-	// 		json: true
-	// 	};
-	// 	return this.executeRequest(request)
-	// };
-
 	private executeFormRequest = async (url: string, buffer: Buffer) => {
 		const request: UriOptions & CoreOptions = {
 			headers: this.headersForm,
-			uri: `${this.baseUrl}${url}`,
+			uri: `${this.config.baseUrl}${url}`,
 			formData: createFormData('logs.zip', buffer),
 			method: HttpMethod.POST,
 		};
 		return this.executeRequest(request);
 	};
 
-	private async executePostRequest(url: string, body: any) {
+	private async executePostRequest<T>(url: string, body: any) {
 		const request: UriOptions & CoreOptions = {
 			headers: this.headersJson,
-			uri: `${this.baseUrl}${url}`,
+			uri: `${this.config.baseUrl}${url}`,
 			body,
 			method: HttpMethod.POST,
 			json: true
 		};
-		return this.executeRequest(request);
+		return this.executeRequest<T>(request);
 	}
 
 	private async executePutRequest(url: string, body: any) {
 		const request: UriOptions & CoreOptions = {
 			headers: this.headersJson,
-			uri: `${this.baseUrl}${url}`,
+			uri: `${this.config.baseUrl}${url}`,
 			body,
 			method: HttpMethod.PUT,
 			json: true
@@ -257,7 +260,7 @@ export class JiraModule_Class
 	}
 
 
-	private async executeGetRequest(url: string, _params?: { [k: string]: string }) {
+	private async executeGetRequest<T>(url: string, _params?: { [k: string]: string }) {
 		const params = _params && Object.keys(_params).map((key) => {
 			return `${key}=${_params[key]}`;
 		});
@@ -268,23 +271,25 @@ export class JiraModule_Class
 
 		const request: UriOptions & CoreOptions = {
 			headers: this.headersJson,
-			uri: `${this.baseUrl}${url}${urlParams}`,
+			uri: `${this.config.baseUrl}${url}${urlParams}`,
 			method: HttpMethod.GET,
 			json: true
 		};
-		return this.executeRequest(request);
+
+		console.log(`request: `, request)
+		return this.executeRequest<T>(request);
 	}
 
-	private handleResponse(response: Response) {
+	private handleResponse<T>(response: Response) {
 		if (`${response.statusCode}`[0] !== '2')
 			throw new ApiException(response.statusCode, response.body)
 
-		return response.toJSON().body;
+		return response.toJSON().body as T;
 	}
 
-	private async executeRequest(body: UriOptions & CoreOptions) {
+	private async executeRequest<T>(body: UriOptions & CoreOptions) {
 		const response = await promisifyRequest(body, false);
-		return this.handleResponse(response);
+		return this.handleResponse<T>(response);
 	}
 }
 
