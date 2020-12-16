f6400f3be0d553819f4e9ec731af17a3aac627d5
TacB0sS <adam.vdk@nu-art-software.com>
2020-12-16 22:37:42 +0200

    thunderstorm moves to pipeline

---
+++ .jenkins/build-deploy-publish.groovy
@@ -0,0 +1,26 @@
+@Library('dev-tools')
+
+import com.nu.art.pipeline.modules.SlackModule
+import com.nu.art.pipeline.thunderstorm.Pipeline_ThunderstormMain
+import com.nu.art.pipeline.workflow.Workflow
+
+class Pipeline_Build
+	extends Pipeline_ThunderstormMain<Pipeline_Build> {
+
+		Pipeline_Build() {
+			super("Thunderstorm", "thunderstorm", SlackModule.class)
+		}
+
+		@Override
+		protected void init() {
+			declareEnv("dev", "thunderstorm-dev")
+			declareEnv("staging", "thunderstorm-staging")
+			declareEnv("prod", "nu-art-thunderstorm")
+			setGitRepoId("nu-art-js/thunderstorm")
+			super.init()
+		}
+}
+
+node("ci") {
+	Workflow.createWorkflow(Pipeline_Build.class, this)
+}
