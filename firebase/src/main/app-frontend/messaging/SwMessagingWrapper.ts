aa24d9dc977e3d6f9ed7163cf4731d761e725f94
noabaker <noabkr@intuitionrobotics.com>
2020-12-23 11:00:46 +0200

    push notification warning fixed

--- firebase/src/main/app-frontend/messaging/SwMessagingWrapper.ts
+++ firebase/src/main/app-frontend/messaging/SwMessagingWrapper.ts
@@ -26,6 +26,8 @@ import {FirebaseType_Messaging} from "./types";
 import 'firebase/messaging';
 
 
+
+
 export class SwMessagingWrapper
 	extends Logger {
 
@@ -36,14 +38,14 @@ export class SwMessagingWrapper
 		this.messaging = messaging;
 	}
 
-	onBackgroundMessage(callback: (payload: any) => void){
+	onBackgroundMessage(callback: (payload: any) => void) {
 		// This means that the bundle is being evaluated in the main thread to register the service worker so there is no need to run the rest
 		// Also because it would fail since firebase would initialize the messaging controller as the main thread one instead of the sw one...
-		if(!self || !('ServiceWorkerGlobalScope' in self))
+		if (!self || !('ServiceWorkerGlobalScope' in self))
 			return this.logWarning('Not a service worker context');
 
 		this.logInfo('This is a service worker context');
-		this.messaging.onBackgroundMessage(callback)
+		this.messaging.onBackgroundMessage(callback);
 	}
 
 }
