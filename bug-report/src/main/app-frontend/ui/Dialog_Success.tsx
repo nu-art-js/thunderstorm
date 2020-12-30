1ec615cb5ba3c8a664b7cf0465e47cbf7267869d
noabaker <noabkr@intuitionrobotics.com>
2020-12-30 11:44:55 +0200

    updates to bug report

--- bug-report/src/main/app-frontend/ui/Dialog_Success.tsx
+++ bug-report/src/main/app-frontend/ui/Dialog_Success.tsx
@@ -31,21 +31,24 @@ export class Dialog_Success
 			<div className={'ll_v_s'} style={{flex: 1, position: 'relative'}}>
 				<div style={{textAlign: 'center', padding: '10px', fontSize: '16px', color: 'darkslategray'}}>Success</div>
 				<div style={{textAlign: 'center', padding: '5px', fontSize: '14px', color: 'darkslategray'}}>Bug report submitted successfully</div>
-				<div
-					className={'clickable ll_h_c'}
-					style={{
-						background: 'DeepSkyBlue',
-						height: 32,
-						paddingRight: 20,
-						paddingLeft: 20,
-						borderRadius: 16,
-						textAlign: "center",
-						width: '125px',
-						fontSize: '14px',
-						color: 'darkslategray'
-					}}
-					onClick={() => this.redirectToJira()}>
-					Take me to jira
+				<div style={{position: 'relative', height: '40px'}}>
+					<div
+						className={'clickable ll_h_c'}
+						style={{
+							background: 'DeepSkyBlue',
+							height: 32,
+							paddingRight: 10,
+							paddingLeft: 10,
+							borderRadius: 16,
+							width: '93px',
+							fontSize: '14px',
+							color: 'darkslategray',
+							position: "absolute",
+							right: '35%'
+						}}
+						onClick={() => this.redirectToJira()}>
+						Take me to jira
+					</div>
 				</div>
 			</div>
 		</div>;
