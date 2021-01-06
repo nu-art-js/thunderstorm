7980a592e8cf90372acaf241ab030cd72545339a
TacB0sS <adam.vdk@nu-art-software.com>
2021-01-06 16:28:35 +0200

    only ts-common

--- .scripts/modules.sh
+++ .scripts/modules.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 projectModules=(
-#  app-backend
-#  app-frontend
+  #  app-backend
+  #  app-frontend
 )
-
+thunderstormLibraries=(ts-common)
 projectLibs=(
   ts-common
-#  ${thunderstormLibraries[@]}
-#  app-shared
+  #  ${thunderstormLibraries[@]}
+  #  app-shared
 )
 
 testServiceAccount=../.trash/test-account.json
