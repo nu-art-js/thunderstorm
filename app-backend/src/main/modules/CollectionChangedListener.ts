5336c7cb60860651626f7f8a358a2b46e8f1b887
Alan Ben Seralvo <alan@intuitionrobotics.com>
2021-01-18 11:21:00 +0200

    be

--- app-backend/src/main/modules/CollectionChangedListener.ts
+++ app-backend/src/main/modules/CollectionChangedListener.ts
@@ -19,7 +19,7 @@ import {PushPubSubModule} from "@nu-art/push-pub-sub/backend";
 import {FirestoreFunctionModule} from "@nu-art/firebase/backend-functions";
 
 export class CollectionChangedListener_Class
-	extends FirestoreFunctionModule{
+	extends FirestoreFunctionModule<object>{
 
 	constructor() {
 		super(`test`);
@@ -27,7 +27,7 @@ export class CollectionChangedListener_Class
 		this.onFunctionReady = this.onFunctionReady.bind(this);
 	}
 
-	processChanges = async (params: { [p: string]: any }, previousValue?: any, newValue?: any, ): Promise<any> => {
+	processChanges = async (params: { [p: string]: any }, previousValue?: object, newValue?: object ): Promise<any> => {
 		this.logInfo(`Doing nothing...${previousValue}, ${newValue}`);
 		await PushPubSubModule.pushToKey('collection_test', {id: 'collection_test1'}, {a: 'b', b: 1});
 	};
