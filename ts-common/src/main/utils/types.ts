fc44d8efe315a3349a0b8bc7a74c3d0cbb6963f8
TacB0sS <adam.vdk@nu-art-software.com>
2021-02-03 00:54:56 +0200

    deprecte create and update in favor of upsert and patch

--- ts-common/src/main/utils/types.ts
+++ ts-common/src/main/utils/types.ts
@@ -46,6 +46,8 @@ export type ObjectTS = { [s: string]: any };
 
 export type TypedMap<ValueType> = { [s: string]: ValueType };
 
+export type TypedMapValue<T extends ObjectTS, ValueType> = { [P in keyof T]: ValueType };
+
 export type DB_Object = { _id: string }
 
 export type Auditable = {
