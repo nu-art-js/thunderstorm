c881dda11fcc7338ed6c747a6eef245e8d7b7475
Alan Ben Seralvo <alan@intuitionrobotics.com>
2021-03-14 18:59:24 +0200

    permissions optimized

--- permissions/src/main/app-frontend/modules/PermissionsModuleFE.ts
+++ permissions/src/main/app-frontend/modules/PermissionsModuleFE.ts
@@ -49,7 +49,6 @@ export class PermissionsModuleFE_Class
 				return;
 
 			this.loadingUrls.add(url);
-			this.userUrlsPermissions[url] = false;
 		});
 
 		this.setPermissions();
@@ -64,7 +63,6 @@ export class PermissionsModuleFE_Class
 			return permitted;
 
 		this.loadingUrls.add(url);
-		this.userUrlsPermissions[url] = false;
 		this.setPermissions();
 		return undefined;
 	}
@@ -74,6 +72,8 @@ export class PermissionsModuleFE_Class
 			throw new ImplementationMissingException("need to set up a project id config");
 
 		this.debounce(() => {
+			const urls: string[] = [];
+			this.loadingUrls.forEach(url => urls.push(url))
 			XhrHttpModule
 				.createRequest<PermissionsApi_UserUrlsPermissions>(HttpMethod.POST, 'user-urls-permissions')
 				.setRelativeUrl(`/v1/permissions/user-urls-permissions`)
@@ -81,7 +81,7 @@ export class PermissionsModuleFE_Class
 				.setLabel(`Getting user urls permissions`)
 				.setJsonBody({
 					             projectId: this.config.projectId,
-					             urls: this.userUrlsPermissions,
+					             urls: urls,
 					             requestCustomField: this.requestCustomField
 				             })
 				.setOnError(() => {
