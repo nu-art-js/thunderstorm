bbb3ff2203e5aebd215ffd7f20748c34fd27c792
TacB0sS <adam.vdk@nu-art-software.com>
2021-04-10 12:01:28 +0300

    dev env override staging till have dev project

--- app-frontend/.config/config-local.ts
+++ app-frontend/.config/config-local.ts
@@ -19,14 +19,14 @@
 
 export const config = {
 	XhrHttpModule: {
-		origin: "https://us-central1-thunderstorm-staging.cloudfunctions.net/api",
-		timeout: 10000
+		// origin: "http://192.168.1.5:3000",
+		origin: "http://localhost:5000/thunderstorm-staging/us-central1/api",
+		timeout: 30000,
+		compress: false
 	},
 	frontend: {
-		origin: "https://thunderstorm-staging.firebaseapp.com",
-	},
-	ExampleModule: {
-		remoteUrl: "/v1/sample/endpoint-example"
+		// origin: "http://192.168.1.5:3010",
+		origin: "http://localhost:5001",
 	},
 	FirebaseModule: {
 		local: {
@@ -42,6 +42,9 @@ export const config = {
 	},
 	PushPubSubModule: {
 		publicKeyBase64: 'BBsKBw0R-mITlCSAOtCiHCLvKl-EetCmt5JKMg8L8ev1GqBEpDryum8ve3htIlbN3cjV1MLDFQnk0a8Wfks7cFk'
+	},
+	ExampleModule: {
+		remoteUrl: "/v1/sample/endpoint-example"
 	},
 	ForceUpgrade: {
 		assertVersionUrl: "/v1/version/assert"
