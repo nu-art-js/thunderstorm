5bc8bd4dbac35f4f07bd7046e4db6ae2eb33a14c
yairbcm <yairbcm@intuitionrobotics.com>
2021-05-31 11:00:05 +0300

    add new account api & logic

--- user-account/src/main/shared/api.ts
+++ user-account/src/main/shared/api.ts
@@ -48,7 +48,7 @@ export type Request_CreateAccount = {
 	// customProps?: StringMap
 }
 
-export type Request_CreateAccountWithoutLogin = {
+export type Request_AddNewAccount = {
 	email: string
 	password?: string
 	password_check?: string
@@ -84,8 +84,8 @@ export type PostAssertBody = {
 	RelayState: string
 };
 
+export type AccountApi_AddNewAccount = ApiWithBody<'/v1/account/add-new-account', Request_AddNewAccount, UI_Account>
 export type AccountApi_Create = ApiWithBody<'/v1/account/create', Request_CreateAccount, Response_Auth>
-export type AccountApi_CreateAccountWithoutLogin = ApiWithBody<'/v1/account/create-account-without-login', Request_CreateAccountWithoutLogin, UI_Account>
 export type AccountApi_Login = ApiWithBody<'/v1/account/login', Request_LoginAccount, Response_Auth>
 export type AccountApi_LoginSAML = ApiWithQuery<"/v1/account/login-saml", Response_LoginSAML, RequestParams_LoginSAML>
 export type AccountApi_ValidateSession = ApiWithQuery<'/v1/account/validate', Response_Validation>
