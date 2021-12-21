f64589bd0060c2b9fcbbe7ce49531648febc46da
TacB0sS <adam.vdk@nu-art-software.com>
2021-12-21 15:24:10 +0200

    removed test stage

--- .jenkins/proxy.groovy
+++ .jenkins/proxy.groovy
@@ -12,63 +12,19 @@ import com.nu.art.pipeline.workflow.variables.Var_Env
 abstract class Pipeline_Router<T extends Pipeline_Router>
 	extends BasePipeline<T> {
 
-	private static Class<? extends WorkflowModule>[] defaultModules = [SlackModule.class]
 	public Var_Env Env_Branch = new Var_Env("BRANCH_NAME")
 	def envJobs = [:]
 
 	Pipeline_Router() {
-		super("proxy", defaultModules)
+		super("proxy", ([SlackModule.class] as Class<? extends WorkflowModule>[]))
 	}
 
 	Pipeline_Router(Class<? extends WorkflowModule>... modules) {
-		super("proxy", defaultModules + modules)
+		super("proxy", (([SlackModule.class] as Class<? extends WorkflowModule>[]) + modules))
 	}
 
-	Pipeline_Router(String name, Class<? extends WorkflowModule>... modules = []) {
-		super(name, defaultModules + modules)
-	}
-
-	void declareJob(String branch, String jobName) {
-		envJobs.put(branch, jobName)
-	}
-
-	void setDisplayName() {
-		def branch = Env_Branch.get()
-		getModule(BuildModule.class).setDisplayName("#${VarConsts.Var_BuildNumber.get()}: ${getName()}-${branch}")
-	}
-
-	@Override
-	void pipeline() {
-		addStage("running", {
-			def branch = Env_Branch.get()
-			def jobName = (String) envJobs[branch]
-			JobTrigger trigger = new JobTrigger(workflow, jobName)
-			def result = trigger.run()
-			getModule(BuildModule.class).setResult(result.result)
-		})
-	}
-}
-
-
-
-class Pipeline_ThunderstormRouter
-	extends BasePipeline<Pipeline_ThunderstormRouter> {
-
-	public Var_Env Env_Branch = new Var_Env("BRANCH_NAME")
-	def envJobs = [:]
-
-	Pipeline_ThunderstormRouter() {
-		super("proxy", [SlackModule.class] as Class<? extends WorkflowModule>[])
-	}
-
-	@Override
-	protected void init() {
-		declareJob("dev", "thunderstorm--DEV")
-		declareJob("staging", "thunderstorm--STAGING")
-		declareJob("master", "thunderstorm--PROD")
-
-		getModule(SlackModule.class).setDefaultChannel("thunderstorm")
-		super.init()
+	Pipeline_Router(String name, Class<? extends WorkflowModule>... modules) {
+		super(name, (([SlackModule.class] as Class<? extends WorkflowModule>[]) + modules))
 	}
 
 	void declareJob(String branch, String jobName) {
@@ -88,7 +44,26 @@ class Pipeline_ThunderstormRouter
 			new JobTrigger(workflow, jobName).setWait(false).run()
 		})
 	}
+}
 
+
+class Pipeline_ThunderstormRouter
+	extends Pipeline_Router<Pipeline_ThunderstormRouter> {
+
+	Pipeline_ThunderstormRouter() {
+		super("proxy", [SlackModule.class] as Class<? extends WorkflowModule>[])
+	}
+
+	@Override
+	protected void init() {
+		getModule(SlackModule.class).setDefaultChannel("thunderstorm")
+
+		declareJob("dev", "thunderstorm--DEV")
+		declareJob("staging", "thunderstorm--STAGING")
+		declareJob("master", "thunderstorm--PROD")
+
+		super.init()
+	}
 }
 
 
