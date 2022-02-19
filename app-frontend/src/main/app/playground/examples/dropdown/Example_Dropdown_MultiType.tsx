4e9b54ed83e0fb8f5f757cb411dc75c03d1ccf79
TacB0sS <adam.vdk@nu-art-software.com>
2022-02-19 14:20:14 +0200

    more clean up

--- app-frontend/src/main/app/playground/examples/dropdown/Example_Dropdown_MultiType.tsx
+++ app-frontend/src/main/app/playground/examples/dropdown/Example_Dropdown_MultiType.tsx
@@ -17,14 +17,13 @@
  * limitations under the License.
  */
 
-import {AdapterBuilder, TreeItem} from "@nu-art/thunderstorm/frontend";
-import {DropDown, DropDown_headerStyle, DropDown_inputStyle, DropDown_listStyle,} from "@nu-art/thunderstorm/app-frontend/components/DropDown";
-import * as React from "react";
-import {ICONS} from "@res/icons";
-import {css} from "emotion";
-import {flatPlaguesWithTitles, Plague, RendererMap_Plague} from "./consts";
-import {PlaygroundExample_BodyStyle, PlaygroundExample_HeaderStyle} from "../consts";
-import {PG_Example} from "../_core/PG_Example";
+import {AdapterBuilder, DropDown_headerStyle, DropDown_inputStyle, DropDown_listStyle, TreeItem, TS_DropDown} from '@nu-art/thunderstorm/frontend';
+import * as React from 'react';
+import {ICONS} from '@res/icons';
+import {css} from 'emotion';
+import {flatPlaguesWithTitles, Plague, RendererMap_Plague} from './consts';
+import {PlaygroundExample_BodyStyle, PlaygroundExample_HeaderStyle} from '../consts';
+import {PG_Example} from '../_core/PG_Example';
 
 
 export class Example_Dropdown_MultiType
@@ -48,69 +47,69 @@ export class Example_Dropdown_MultiType
 		const inputStylable = {
 			className: css(
 				{
-					backgroundColor: "lime",
+					backgroundColor: 'lime',
 					fontSize: 13,
-					"::placeholder": {
-						color: "red",
+					'::placeholder': {
+						color: 'red',
 					}
 				}),
-			style: {...DropDown_inputStyle, padding: "0 20px"},
+			style: {...DropDown_inputStyle, padding: '0 20px'},
 			placeholder: this.state?._selected?.label
-		}
+		};
 
 		const valueRenderer = (selected?: TreeItem<Plague>) => {
-			const style: React.CSSProperties = {backgroundColor: "lime", boxSizing: "border-box", height: "100%", width: "100%", padding: "4px 7px"};
+			const style: React.CSSProperties = {backgroundColor: 'lime', boxSizing: 'border-box', height: '100%', width: '100%', padding: '4px 7px'};
 			if (selected)
-				return <div style={{...style, color: "red"}}>{selected?.item.label}</div>;
-			return <div style={style}>CHOOSE</div>
+				return <div style={{...style, color: 'red'}}>{selected?.item.label}</div>;
+			return <div style={style}>CHOOSE</div>;
 		};
-		const headerStylable = {style: {...DropDown_headerStyle, border: "solid 2px red", borderRadius: "5px 5px 0px 0px"}};
+		const headerStylable = {style: {...DropDown_headerStyle, border: 'solid 2px red', borderRadius: '5px 5px 0px 0px'}};
 		const listStylable = {
 			style: {
 				...DropDown_listStyle,
-				border: "solid 2px red",
-				borderRadius: "0px 0px 5px 5px",
-				borderTop: "none",
+				border: 'solid 2px red',
+				borderRadius: '0px 0px 5px 5px',
+				borderTop: 'none',
 				top: 0,
 				maxHeight: 90
 			}
 		};
-		const caretItem = <div style={{backgroundColor: "lime", paddingRight: 8}}>
+		const caretItem = <div style={{backgroundColor: 'lime', paddingRight: 8}}>
 			<div style={{marginTop: 3}}>{ICONS.arrowOpen(undefined, 11)}</div>
 		</div>;
-		const caret = {open: caretItem, close: caretItem}
+		const caret = {open: caretItem, close: caretItem};
 		return <div {...PlaygroundExample_BodyStyle}>
 			<div {...PlaygroundExample_HeaderStyle}>Multiple renderers, flat list</div>
 			<h4>Filter, 1 caret, default value,</h4>
 			<h4>all renderers & custom inline style</h4>
 			<h4>multiple renderers, flat list</h4>
-			<DropDown
+			<TS_DropDown
 				adapter={adapter}
 				onSelected={this.onSelected}
 				selectedItemRenderer={valueRenderer}
 				inputStylable={inputStylable}
 				inputEventHandler={(_state, e) => {
-					if (e.key === "Enter") {
-						const newOption = _state.filteredOptions ? _state.filteredOptions[1] : _state.selected
+					if (e.key === 'Enter') {
+						const newOption = _state.filteredOptions ? _state.filteredOptions[1] : _state.selected;
 						_state.selected = newOption;
 						_state.open = false;
-						newOption && this.onSelected(newOption)
+						newOption && this.onSelected(newOption);
 					}
 					return _state;
 				}}
-				filter={(item) => [(item).item.label.toLowerCase()]}
+				filterMapper={(item) => [(item).item.label.toLowerCase()]}
 				selected={flatPlaguesWithTitles[2]}
 				caret={caret}
 				headerStylable={headerStylable}
 				listStylable={listStylable}
 			/>
-			<h4>{this.state?._selected ? `You chose ${this.state._selected.value}` : "You didn't choose yet"}</h4>
-		</div>
+			<h4>{this.state?._selected ? `You chose ${this.state._selected.value}` : 'You didn\'t choose yet'}</h4>
+		</div>;
 	}
 }
 
 
-const name = "Dropdown - Multi Type";
+const name = 'Dropdown - Multi Type';
 
 export function Playground_DropdownMultiType() {
 	return {
