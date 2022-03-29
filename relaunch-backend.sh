871dce2f9491c4ddc609ab44ad09b56ec0cd6760
TacB0sS <adam.vdk@nu-art-software.com>
2022-03-29 13:10:58 +0300

    tabs with scss build in, works

--- relaunch-backend.sh
+++ relaunch-backend.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
-sleep 5
-echo SHIT
-date +%Y-%m-%d--%H-%M-%S > ../app-backend/src/main/dummy
 
+copyScss() {
+  local absoluteSourcesFolder="${1}"
+  local absoluteOutputDir="${2}"
+  echo "${absoluteSourcesFolder} => ${absoluteOutputDir}"
+  cd "${absoluteSourcesFolder}"
+    find . -name '*.scss' | cpio -pdm "${absoluteOutputDir}" > /dev/null
+  cd -
+}
+
+copyScss "${1}" "${2}"
+
+date +%Y-%m-%d--%H-%M-%S > ../app-backend/src/main/dummy
