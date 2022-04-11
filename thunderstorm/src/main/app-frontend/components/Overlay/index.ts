99f61595acd496fba12e38ccfbdb442966540fd5
Matan Koby <matan.k@quai.md>
2022-04-11 14:57:01 +0300

    Updated Overlay to use css

--- thunderstorm/src/main/app-frontend/components/Overlay/index.ts
+++ thunderstorm/src/main/app-frontend/components/Overlay/index.ts
@@ -19,45 +19,4 @@
  * limitations under the License.
  */
 
-import * as React from 'react';
-import {CSSProperties} from 'react';
-
-type Props = {
-	zIndex: number
-	showOverlay: boolean
-	onClickOverlay: (event: React.MouseEvent<HTMLDivElement>) => void
-};
-
-const overlayStyle = (zIndex: number): CSSProperties => ({
-	zIndex,
-	position: 'fixed',
-	top: 0,
-	left: 0,
-	right: 0,
-	bottom: 0
-});
-
-const childrenStyle = (zIndex: number) => ({
-	zIndex,
-});
-
-export class Overlay
-	extends React.Component<Props> {
-
-	static defaultProps: Partial<Props> = {
-		zIndex: 2
-	};
-
-	render() {
-		if (!this.props.showOverlay)
-			return this.props.children;
-
-		return <>
-			<div style={childrenStyle(this.props.zIndex + 1)}>
-				{this.props.children}
-			</div>
-
-			<div onClick={event => this.props.onClickOverlay(event)} style={overlayStyle(this.props.zIndex)}/>
-		</>;
-	}
-}
+export * from './Overlay';
