b9a5eb9a3998f0a977be5e957dc295e604d28a84
TacB0sS <adam.vdk@nu-art-software.com>
2022-04-13 13:43:05 +0300

    more fixing

--- thunderstorm/src/main/app-frontend/modules/menu/MenuAndButton.tsx
+++ thunderstorm/src/main/app-frontend/modules/menu/MenuAndButton.tsx
@@ -1,3 +1,24 @@
+/*
+ * Thunderstorm is a full web app framework!
+ *
+ * Typescript & Express backend infrastructure that natively runs on firebase function
+ * Typescript & React frontend infrastructure
+ *
+ * Copyright (C) 2020 Adam van der Kruk aka TacB0sS
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 import * as React from 'react';
 import {CSSProperties, ReactNode} from 'react';
 import {Menu_Model, MenuBuilder, MenuListener, resolveRealPosition} from './MenuModule';
@@ -13,6 +34,7 @@ type Props = {
 	resolvePosition?: (button: HTMLImageElement) => MenuPosition
 	css?: CSSProperties
 }
+
 
 export class MenuAndButton
 	extends React.Component<Props, { isOpen: boolean, over: boolean }>
@@ -51,7 +73,6 @@ export class MenuAndButton
 			</div>
 		</div>;
 	}
-
 
 	open = () => {
 		if (!this.ref.current)
