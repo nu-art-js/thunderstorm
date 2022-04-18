8639bebd20408dcef1356d6317201bf11bf57ce5
TacB0sS <adam.vdk@nu-art-software.com>
2022-04-18 23:22:50 +0300

    TS Loader

---
+++ thunderstorm/src/main/app-frontend/components/TS_Loader/TS_Loader.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import './TS_Loader.scss';
+
+
+export class TS_Loader
+	extends React.Component {
+
+	render() {
+		return <div className="ts-loader">
+			<div className="ts-loader__content"/>
+		</div>;
+	}
+}
+
