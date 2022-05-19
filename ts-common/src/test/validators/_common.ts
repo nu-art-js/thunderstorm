d77431b2cf1f993d9ab6e5ec6b812cf56169d1ed
TacB0sS <adam.vdk@nu-art-software.com>
2022-05-19 15:25:09 +0300

    fixed ts-common tests

--- ts-common/src/test/validators/_common.ts
+++ ts-common/src/test/validators/_common.ts
@@ -16,14 +16,15 @@
  * limitations under the License.
  */
 
-import {tsValidate} from "../_main";
-import {ValidatorTestInput} from "./test";
+import {ObjectTS, tsValidate} from '../_main';
+import {ValidatorTestInput} from './test';
+
 
 export const validatorProcessor = async <T extends ObjectTS>(model: ValidatorTestInput<T>) => {
 	try {
 		await tsValidate(model.instance, model.validator);
-		return "pass";
-	} catch (e:any) {
-		return "fail";
+		return 'pass';
+	} catch (e: any) {
+		return 'fail';
 	}
 };
