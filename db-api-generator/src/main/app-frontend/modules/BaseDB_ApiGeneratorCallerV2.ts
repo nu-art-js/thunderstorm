14d6a4ac851e91213019bf6b361a7755f8836980
Cipher <itayleybovich@gmail.com>
2022-07-18 13:52:34 +0300

    indexedDB queryReduce first try

--- db-api-generator/src/main/app-frontend/modules/BaseDB_ApiGeneratorCallerV2.ts
+++ db-api-generator/src/main/app-frontend/modules/BaseDB_ApiGeneratorCallerV2.ts
@@ -249,6 +249,10 @@ export abstract class BaseDB_ApiGeneratorCallerV2<DBType extends DB_Object, Ks e
 		return this.db.queryFind(filter);
 	}
 
+	// public async queryReduce<ReturnType>(reducer: ReduceFunction<DBType, ReturnType>, initialValue: ReturnType, filter?: (item: DBType) => boolean): Promise<ReturnType> {
+	// 	return this.db.queryReduce(reducer, initialValue, filter);
+	// }
+
 	// public async queryMap<Type>(mapper: (item: DBType) => Type, filter?: (item: DBType) => boolean, query?: IndexDb_Query): Promise<Type[]> {
 	// 	return this.db.queryMap(mapper, filter, query);
 	// }
