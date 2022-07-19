ba83d50bcce14c1e75854736128349d3d2b0653b
Matan Koby <matan.k@quai.md>
2022-07-19 17:01:09 +0300

    rename

--- file-upload/src/main/app-frontend/modules/ModuleFE_AssetUploader.ts
+++ file-upload/src/main/app-frontend/modules/ModuleFE_AssetUploader.ts
@@ -18,9 +18,9 @@
  */
 import {ThunderDispatcher, XhrHttpModule, XhrHttpModule_Class} from '@nu-art/thunderstorm/frontend';
 import {BaseUploaderFile, FileStatus, FileUploadResult, OnFileStatusChanged, PushKey_FileUploaded, TempSecureUrl} from '../../shared';
-import {ModuleBE_PushPubSub} from '@nu-art/push-pub-sub/frontend';
 import {ModuleBase_AssetUploader} from '../../shared/modules/ModuleBase_AssetUploader';
 import {DB_Notifications} from '@nu-art/push-pub-sub';
+import {ModuleFE_PushPubSub} from '@nu-art/push-pub-sub/frontend/modules/ModuleFE_PushPubSub';
 
 
 export class ModuleFE_AssetUploader_Class
@@ -49,13 +49,13 @@ export class ModuleFE_AssetUploader_Class
 	}
 
 	protected async subscribeToPush(toSubscribe: TempSecureUrl[]): Promise<void> {
-		return ModuleBE_PushPubSub.subscribeMulti(toSubscribe.map(r => ({pushKey: PushKey_FileUploaded, props: {feId: r.asset.feId}})));
+		return ModuleFE_PushPubSub.subscribeMulti(toSubscribe.map(r => ({pushKey: PushKey_FileUploaded, props: {feId: r.asset.feId}})));
 	}
 
 	__onMessageReceived(notification: DB_Notifications<FileUploadResult>): void {
 		super.__onMessageReceived(notification);
 		if (notification.data?.status === FileStatus.Completed || notification.data?.status?.startsWith('Error'))
-			ModuleBE_PushPubSub.unsubscribe({pushKey: PushKey_FileUploaded, props: notification.props});
+			ModuleFE_PushPubSub.unsubscribe({pushKey: PushKey_FileUploaded, props: notification.props});
 	}
 }
 
