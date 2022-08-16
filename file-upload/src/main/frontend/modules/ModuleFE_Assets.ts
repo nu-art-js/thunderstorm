d9855ee0a2c70d370e6f8e80fa655b374255e2cd
TacB0sS <adam.vdk@nu-art-software.com>
2022-08-16 19:39:27 +0200

    moving to v2

--- file-upload/src/main/frontend/modules/ModuleFE_Assets.ts
+++ file-upload/src/main/frontend/modules/ModuleFE_Assets.ts
@@ -1,21 +1,20 @@
-import {ApiCallerEventType, BaseDB_ApiGeneratorCaller} from '@nu-art/db-api-generator/frontend';
+import {ApiCallerEventTypeV2, BaseDB_ApiGeneratorCallerV2} from '@nu-art/db-api-generator/frontend';
 import {ThunderDispatcher} from '@nu-art/thunderstorm/frontend';
 import {DB_Asset, DBDef_Assets} from '../../shared';
 
 
 export interface OnAssetsUpdated {
-	__onAssetsUpdated: (...params: ApiCallerEventType) => void;
+	__onAssetsUpdated: (...params: ApiCallerEventTypeV2<DB_Asset>) => void;
 }
 
 export const dispatch_onAssetsListChanged = new ThunderDispatcher<OnAssetsUpdated, '__onAssetsUpdated'>('__onAssetsUpdated');
 
 export class ModuleFE_Assets_Class
-	extends BaseDB_ApiGeneratorCaller<DB_Asset> {
+	extends BaseDB_ApiGeneratorCallerV2<DB_Asset> {
 
 
 	constructor() {
-		super(DBDef_Assets);
-		this.setDefaultDispatcher(dispatch_onAssetsListChanged);
+		super(DBDef_Assets,dispatch_onAssetsListChanged);
 	}
 }
 
