cef4892e25c2573c908983541dc5d2a5c9da5cdb
Cipher <itayleybovich@gmail.com>
2022-08-16 10:43:14 +0300

    collapsable container done

--- thunderstorm/src/main/frontend/components/TS_CollapsableContainer/TS_CollapsableContainer.tsx
+++ thunderstorm/src/main/frontend/components/TS_CollapsableContainer/TS_CollapsableContainer.tsx
@@ -1,10 +1,13 @@
+import {compare} from '@nu-art/ts-common';
 import * as React from 'react';
-import {ComponentSync, LL_V_L, _className} from '../..';
+import {_className, ComponentSync, LL_V_L} from '../..';
 import './TS_CollapsableContainer.scss';
 
 type Props = {
 	headerRenderer: React.ReactNode | (() => React.ReactNode);
 	containerRenderer: React.ReactNode | (() => React.ReactNode);
+	collapsed?: boolean;
+	onCollapseToggle?: (collapseState: boolean) => void;
 }
 
 type State = {
@@ -18,11 +21,29 @@ export class TS_CollapsableContainer extends ComponentSync<Props, State> {
 	}
 
 	private toggleCollapse = () => {
+		//If component is controlled, return
+		this.props.onCollapseToggle?.(this.isCollapsed());
+		if (this.props.collapsed !== undefined) {
+			return;
+		}
+
 		this.setState({collapsed: !this.state.collapsed});
 	};
 
+	shouldComponentUpdate(nextProps: Readonly<Props>, nextState: Readonly<State>, nextContext: any): boolean {
+		let res = super.shouldComponentUpdate(nextProps, nextState, nextContext);
+		if (!res)
+			res = !compare(this.props.collapsed, nextProps.collapsed);
+		return res;
+	}
+
+	private isCollapsed() {
+		//Return the collapse from props if controlled, otherwise state
+		return this.props.collapsed !== undefined ? this.props.collapsed : this.state.collapsed;
+	}
+
 	renderHeader() {
-		const className = _className('ts-collapsable-container__header', this.state?.collapsed ? 'collapsed' : undefined);
+		const className = _className('ts-collapsable-container__header', this.isCollapsed() ? 'collapsed' : undefined);
 		return <div className={className} onClick={this.toggleCollapse}>
 			<span className={'ts-collapsable-container__header__caret'}>{this.state.collapsed ? '+' : '-'}</span>
 			{/*<TS_Icons.arrow className={_className(this.state.collapsed ? 'collapsed' : undefined)}/>*/}
@@ -31,7 +52,7 @@ export class TS_CollapsableContainer extends ComponentSync<Props, State> {
 	}
 
 	renderContainer() {
-		const className = _className('ts-collapsable-container__container', this.state?.collapsed ? 'collapsed' : undefined);
+		const className = _className('ts-collapsable-container__container', this.isCollapsed() ? 'collapsed' : undefined);
 		return <div className={className}>
 			{(typeof this.props.containerRenderer === 'function' ? this.props.containerRenderer() : this.props.containerRenderer)}
 		</div>;
