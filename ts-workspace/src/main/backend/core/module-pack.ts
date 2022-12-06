8507b1e8fc1f4dca9decf2065037b298491aa80a
Cipher <itayleybovich@gmail.com>
2022-12-06 15:00:58 +0200

    ts-workspace lib

---
+++ ts-workspace/src/main/backend/core/module-pack.ts
@@ -0,0 +1,28 @@
+/*
+ * Live-Docs will allow you to add and edit tool-tips from within your app...
+ *
+ * Copyright (C) 2020 Adam van der Kruk aka TacB0sS
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import {Module} from '@nu-art/ts-common';
+import {ModuleBE_Workspace} from '../modules/ModuleBE_Workspace';
+
+
+export const ModulePack_Backend_TSWorkspace: Module[] = [
+	ModuleBE_Workspace,
+];
+
+export * from '../modules/ModuleBE_Workspace';
+
