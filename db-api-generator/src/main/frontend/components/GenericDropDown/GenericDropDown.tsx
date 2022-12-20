cdaac97e6d94c6dd68f06f1e7ecc37062d20cf5e
Cipher <itayleybovich@gmail.com>
2022-12-20 17:06:30 +0200

    Generic Dropdown sync insted of async

--- db-api-generator/src/main/frontend/components/GenericDropDown/GenericDropDown.tsx
+++ db-api-generator/src/main/frontend/components/GenericDropDown/GenericDropDown.tsx
@@ -1,7 +1,7 @@
-import {SimpleListAdapter, TS_DropDown} from '@nu-art/thunderstorm/frontend';
-import {DB_Object, Filter, sortArray} from '@nu-art/ts-common';
+import {ComponentSync, SimpleListAdapter, TS_DropDown} from '@nu-art/thunderstorm/frontend';
+import {cloneArr, DB_Object, Filter, sortArray} from '@nu-art/ts-common';
 import * as React from 'react';
-import {BaseDB_ApiCaller, Props_SmartComponent, SmartComponent, State_SmartComponent} from '../..';
+import {BaseDB_ApiCaller} from '../..';
 
 
 type OptionalProps_GenericDropDown<T> = {
@@ -21,7 +21,7 @@ export type PartialProps_GenericDropDown<T> = OptionalProps_GenericDropDown<T> &
 	onNoMatchingSelectionForString?: (filterText: string, matchingItems: T[], e: React.KeyboardEvent) => Promise<void> | void;
 	boundingParentSelector?: string;
 	inputValue?: string;
-	selected?: T | string | (() => Promise<T | undefined>);
+	selected?: T | string | (() => T | undefined);
 }
 
 export type MandatoryProps_GenericDropDown<T extends DB_Object, Ks extends keyof T = '_id'> = OptionalProps_GenericDropDown<T> & {
@@ -43,7 +43,7 @@ export type Props_GenericDropDown<T extends DB_Object, Ks extends keyof T = '_id
 	caret?: { open: React.ReactNode, close: React.ReactNode }
 	renderSearch?: (dropDown: TS_DropDown<T>) => React.ReactNode;
 
-	selected?: T | string | (() => Promise<T | undefined>);
+	selected?: T | string | (() => T | undefined);
 	inputValue?: string;
 	onSelected: (selected: T) => void;
 	onNoMatchingSelectionForString?: (filterText: string, matchingItems: T[], e: React.KeyboardEvent) => Promise<void> | void;
@@ -60,20 +60,21 @@ type State<T extends DB_Object> = {
 // const defaultQueryFilter = () => true;
 
 export class GenericDropDown<T extends DB_Object, Ks extends keyof T = '_id'>
-	extends SmartComponent<Props_GenericDropDown<T, Ks>, State<T>> {
+	extends ComponentSync<Props_GenericDropDown<T, Ks>, State<T>> {
 
 	constructor(props: Props_GenericDropDown<T, Ks>) {
 		super(props);
 	}
 
-	protected async deriveStateFromProps(nextProps: Props_SmartComponent & Props_GenericDropDown<T, Ks>, state: (State<T> & State_SmartComponent)) {
+	protected deriveStateFromProps(nextProps: Props_GenericDropDown<T, Ks>): State<T> {
+		const state = {} as State<T>;
 		//If filter exists, get filtered items
 		if (nextProps.queryFilter)
-			state.items = await nextProps.module.IDB.filter(nextProps.queryFilter);
+			state.items = nextProps.module.cache.filter(nextProps.queryFilter);
 
 		//If filter doesn't exist OR (filter gave 0 results AND ifNoneShowAll condition flagged true)
 		if (!nextProps.queryFilter || (state.items.length === 0 && nextProps.ifNoneShowAll === true))
-			state.items = await nextProps.module.IDB.query();
+			state.items = cloneArr(nextProps.module.cache.all() as T[]);
 
 		//Sort Items by sort function or object keys
 		if (typeof nextProps.sortBy === 'function')
@@ -94,11 +95,11 @@ export class GenericDropDown<T extends DB_Object, Ks extends keyof T = '_id'>
 				break;
 
 			case 'string':
-				state.selected = await nextProps.module.IDB.unique(selectedItem);
+				state.selected = nextProps.module.cache.unique(selectedItem);
 				break;
 
 			case 'function':
-				state.selected = await selectedItem();
+				state.selected = selectedItem();
 				break;
 
 			case 'undefined':
