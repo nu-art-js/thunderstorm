1f58b8c22b662623ad1b6e87b6a58fcc20e8980b
Matan Koby <matan.k@quai.md>
2023-01-18 14:05:38 +0200

    BackupModule to contain backup code

--- thunderstorm/src/main/backend/modules/backup/FirestoreBackupScheduler.ts
+++ thunderstorm/src/main/backend/modules/backup/FirestoreBackupScheduler.ts
@@ -19,21 +19,8 @@
  * limitations under the License.
  */
 
-import {
-	__stringify,
-	_logger_logException,
-	currentTimeMillis,
-	dispatch_onServerError,
-	Dispatcher,
-	Format_YYYYMMDD_HHmmss,
-	formatTimestamp,
-	generateHex,
-	ServerErrorSeverity
-} from '@nu-art/ts-common';
 import {FirebaseScheduledFunction} from '@nu-art/firebase/backend/functions/firebase-function';
-import {ModuleBE_Firebase} from '@nu-art/firebase/backend/ModuleBE_Firebase';
 import {ActDetailsDoc,} from '../CleanupScheduler';
-import {FirestoreQuery} from '@nu-art/firebase';
 import {FirestoreBackupDetails, ModuleBE_Backup} from './ModuleBE_Backup';
 
 
@@ -51,7 +38,6 @@ export interface OnModuleCleanup {
 	__onCleanupInvoked: () => Promise<void>;
 }
 
-const dispatch_onModuleCleanup = new Dispatcher<OnModuleCleanup, '__onCleanupInvoked'>('__onCleanupInvoked');
 
 export class FirestoreBackupScheduler_Class
 	extends FirebaseScheduledFunction {
@@ -62,73 +48,10 @@ export class FirestoreBackupScheduler_Class
 		this.setRuntimeOptions({timeoutSeconds: 300});
 	}
 
-	onScheduledEvent = async (force?: boolean): Promise<any> => {
+	onScheduledEvent = async (): Promise<any> => {
 		this.logInfoBold('Running function FirestoreBackupScheduler');
 
-		try {
-			this.logInfo('Cleaning modules...');
-			await dispatch_onModuleCleanup.dispatchModuleAsync();
-			this.logInfo('Cleaned modules!');
-		} catch (e: any) {
-			this.logWarning(`modules cleanup has failed with error`, e);
-			const errorMessage = `modules cleanup has failed with error\nError: ${_logger_logException(e)}`;
-
-			await dispatch_onServerError.dispatchModuleAsync(ServerErrorSeverity.Critical, this, errorMessage);
-		}
-
-		const backups: FirestoreBackupDetails<any>[] = ModuleBE_Backup.getBackupDetails();
-		const backupId = generateHex(32);
-		const nowMs = currentTimeMillis();
-
-		const bucket = await ModuleBE_Firebase.createAdminSession().getStorage().getMainBucket();
-		await Promise.all(backups.map(async (backupItem) => {
-			const query: FirestoreQuery<BackupDoc> = {
-				where: {moduleKey: backupItem.moduleKey},
-				orderBy: [{key: 'timestamp', order: 'asc'}],
-				limit: 1
-			};
-
-			const docs = await ModuleBE_Backup.query(query);
-			const latestDoc = docs[0];
-			if (!force && latestDoc && latestDoc.timestamp + backupItem.minTimeThreshold > nowMs)
-				return; // If the oldest doc is still in the keeping timeframe, don't delete any docs.
-
-			const timeFormat = formatTimestamp(Format_YYYYMMDD_HHmmss, nowMs);
-			const backupPath = `backup/firestore/${timeFormat}/${backupItem.moduleKey}.json`;
-			try {
-				const toBackupData = await backupItem.collection.query(backupItem.backupQuery);
-				await (await bucket.getFile(backupPath)).write(toBackupData);
-
-				this.logInfoBold('Upserting Backup for ' + backupItem.moduleKey);
-				await ModuleBE_Backup.upsert({timestamp: nowMs, moduleKey: backupItem.moduleKey, backupPath, backupId: backupId});
-
-				this.logInfoBold('Upserting BackupStatus for ' + backupItem.moduleKey); // happened 30 seconds later
-				const keepInterval = backupItem.keepInterval;
-				if (keepInterval) {
-					this.logInfoBold('Querying for items to delete for ' + backupItem.moduleKey);
-					const queryOld = {where: {moduleKey: backupItem.moduleKey, timestamp: {$lt: nowMs - keepInterval}}};
-					const oldDocs = await ModuleBE_Backup.query(queryOld);
-
-					this.logInfoBold('Received items to delete total: ' + oldDocs.length);
-					await Promise.all(oldDocs.map(async oldDoc => {
-						try {
-							await (await bucket.getFile(oldDoc.backupPath)).delete();
-							await ModuleBE_Backup.deleteItem(oldDoc);
-						} catch (e: any) {
-							this.logError('error deleting file: ', oldDoc, e);
-						}
-					}));
-
-					this.logInfoBold('Successfully deleted items for ' + backupItem.moduleKey);
-				}
-
-			} catch (e: any) {
-				this.logWarning(`backup of ${backupItem.moduleKey} has failed with error`, e);
-				const errorMessage = `Error backing up firestore collection config:\n ${__stringify(backupItem, true)}\nError: ${_logger_logException(e)}`;
-
-				await dispatch_onServerError.dispatchModuleAsync(ServerErrorSeverity.Critical, this, errorMessage);
-			}
-		}));
+		await ModuleBE_Backup.initiateBackup();
 	};
 }
 
--- thunderstorm/src/main/backend/modules/backup/ModuleBE_Backup.ts
+++ thunderstorm/src/main/backend/modules/backup/ModuleBE_Backup.ts
@@ -1,9 +1,22 @@
-import {Dispatcher, filterInstances, Module, TS_Object} from '@nu-art/ts-common';
+import {
+	__stringify,
+	_logger_logException,
+	currentTimeMillis,
+	dispatch_onServerError,
+	Dispatcher,
+	filterInstances,
+	Format_YYYYMMDD_HHmmss,
+	formatTimestamp,
+	generateHex,
+	Module,
+	ServerErrorSeverity,
+	TS_Object
+} from '@nu-art/ts-common';
 import {ApiDefServer} from '../../utils/api-caller-types';
 import {ApiDef_Backup, ApiStruct_Backup} from '../../../shared';
 import {createQueryServerApi} from '../../core/typed-api';
 import {FirestoreCollection, ModuleBE_Firebase} from '@nu-art/firebase/backend';
-import {BackupDoc, FirestoreBackupScheduler, OnFirestoreBackupSchedulerAct} from './FirestoreBackupScheduler';
+import {BackupDoc, OnFirestoreBackupSchedulerAct, OnModuleCleanup} from './FirestoreBackupScheduler';
 import {FilterKeys, FirestoreQuery} from '@nu-art/firebase';
 
 export type FirestoreBackupDetails<T extends TS_Object> = {
@@ -15,12 +28,13 @@ export type FirestoreBackupDetails<T extends TS_Object> = {
 }
 
 const dispatch_onFirestoreBackupSchedulerAct = new Dispatcher<OnFirestoreBackupSchedulerAct, '__onFirestoreBackupSchedulerAct'>('__onFirestoreBackupSchedulerAct');
+const dispatch_onModuleCleanup = new Dispatcher<OnModuleCleanup, '__onCleanupInvoked'>('__onCleanupInvoked');
 
 
-class ModuleBE_Backup_Class<DBType extends BackupDoc & TS_Object>
+class ModuleBE_Backup_Class
 	extends Module<{}> {
 	readonly vv1: ApiDefServer<ApiStruct_Backup>['vv1'];
-	public collection!: FirestoreCollection<DBType>;
+	public collection!: FirestoreCollection<BackupDoc>;
 
 	constructor() {
 		super();
@@ -33,9 +47,9 @@ class ModuleBE_Backup_Class<DBType extends BackupDoc & TS_Object>
 		this.collection = this.getBackupStatusCollection();
 	}
 
-	public getBackupStatusCollection = (): FirestoreCollection<DBType> => {
+	public getBackupStatusCollection = (): FirestoreCollection<BackupDoc> => {
 		return ModuleBE_Firebase.createAdminSession().getFirestore()
-			.getCollection<DBType>('firestore-backup-status', ['moduleKey', 'timestamp'] as FilterKeys<DBType>);
+			.getCollection<BackupDoc>('firestore-backup-status', ['moduleKey', 'timestamp'] as FilterKeys<BackupDoc>);
 	};
 
 	public getBackupDetails = (): FirestoreBackupDetails<any>[] => filterInstances(dispatch_onFirestoreBackupSchedulerAct.dispatchModule()).reduce<FirestoreBackupDetails<any>[]>((resultBackupArray, currentBackup) => {
@@ -45,19 +59,82 @@ class ModuleBE_Backup_Class<DBType extends BackupDoc & TS_Object>
 	}, []);
 
 	initiateBackup = async () => {
-		return await FirestoreBackupScheduler.onScheduledEvent(true);
+		try {
+			this.logInfo('Cleaning modules...');
+			await dispatch_onModuleCleanup.dispatchModuleAsync();
+			this.logInfo('Cleaned modules!');
+		} catch (e: any) {
+			this.logWarning(`modules cleanup has failed with error`, e);
+			const errorMessage = `modules cleanup has failed with error\nError: ${_logger_logException(e)}`;
+
+			await dispatch_onServerError.dispatchModuleAsync(ServerErrorSeverity.Critical, this, errorMessage);
+		}
+
+		const backups: FirestoreBackupDetails<any>[] = this.getBackupDetails();
+		const backupId = generateHex(32);
+		const nowMs = currentTimeMillis();
+
+		const bucket = await ModuleBE_Firebase.createAdminSession().getStorage().getMainBucket();
+		await Promise.all(backups.map(async (backupItem) => {
+			const query: FirestoreQuery<BackupDoc> = {
+				where: {moduleKey: backupItem.moduleKey},
+				orderBy: [{key: 'timestamp', order: 'asc'}],
+				limit: 1
+			};
+
+			const docs = await this.query(query);
+			const latestDoc = docs[0];
+			if (latestDoc && latestDoc.timestamp + backupItem.minTimeThreshold > nowMs)
+				return; // If the oldest doc is still in the keeping timeframe, don't delete any docs.
+
+			const timeFormat = formatTimestamp(Format_YYYYMMDD_HHmmss, nowMs);
+			const backupPath = `backup/firestore/${timeFormat}/${backupItem.moduleKey}.json`;
+			try {
+				const toBackupData = await backupItem.collection.query(backupItem.backupQuery);
+				await (await bucket.getFile(backupPath)).write(toBackupData);
+
+				this.logInfoBold('Upserting Backup for ' + backupItem.moduleKey);
+				await this.upsert({timestamp: nowMs, moduleKey: backupItem.moduleKey, backupPath, backupId: backupId});
+
+				this.logInfoBold('Upserting BackupStatus for ' + backupItem.moduleKey); // happened 30 seconds later
+				const keepInterval = backupItem.keepInterval;
+				if (keepInterval) {
+					this.logInfoBold('Querying for items to delete for ' + backupItem.moduleKey);
+					const queryOld = {where: {moduleKey: backupItem.moduleKey, timestamp: {$lt: nowMs - keepInterval}}};
+					const oldDocs = await this.query(queryOld);
+
+					this.logInfoBold('Received items to delete total: ' + oldDocs.length);
+					await Promise.all(oldDocs.map(async oldDoc => {
+						try {
+							await (await bucket.getFile(oldDoc.backupPath)).delete();
+							await this.deleteItem(oldDoc);
+						} catch (e: any) {
+							this.logError('error deleting file: ', oldDoc, e);
+						}
+					}));
+
+					this.logInfoBold('Successfully deleted items for ' + backupItem.moduleKey);
+				}
+
+			} catch (e: any) {
+				this.logWarning(`backup of ${backupItem.moduleKey} has failed with error`, e);
+				const errorMessage = `Error backing up firestore collection config:\n ${__stringify(backupItem, true)}\nError: ${_logger_logException(e)}`;
+
+				await dispatch_onServerError.dispatchModuleAsync(ServerErrorSeverity.Critical, this, errorMessage);
+			}
+		}));
 	};
 
-	query = async (ourQuery: FirestoreQuery<DBType>): Promise<DBType[]> => {
+	query = async (ourQuery: FirestoreQuery<BackupDoc>): Promise<BackupDoc[]> => {
 		return await this.collection.query(ourQuery);
 	};
 
-	upsert = async (instance: DBType): Promise<DBType> => {
+	upsert = async (instance: BackupDoc): Promise<BackupDoc> => {
 		this.logWarning(instance);
 		return await this.collection.upsert(instance);
 	};
 
-	deleteItem = async (instance: DBType): Promise<DBType | undefined> => {
+	deleteItem = async (instance: BackupDoc): Promise<BackupDoc | undefined> => {
 		return await this.collection.deleteItem(instance);
 	};
 }
