3762ff68000866af29ba9c27d215e14461525613
TacB0sS <adam.vdk@nu-art-software.com>
2023-01-21 22:39:47 +0200

    sort array fix, assets module is now v2

--- file-upload/src/main/frontend/modules/ModuleFE_Assets.ts
+++ file-upload/src/main/frontend/modules/ModuleFE_Assets.ts
@@ -1,21 +1,30 @@
-import {ApiCallerEventType, BaseDB_ApiGeneratorCaller} from '@nu-art/db-api-generator/frontend';
+import {ApiCallerEventTypeV2, BaseDB_ApiCaller} from '@nu-art/db-api-generator/frontend';
 import {ThunderDispatcher} from '@nu-art/thunderstorm/frontend';
+import {currentTimeMillis} from '@nu-art/ts-common';
 import {DB_Asset, DBDef_Assets} from '../../shared';
 
 
 export interface OnAssetsUpdated {
-	__onAssetsUpdated: (...params: ApiCallerEventType) => void;
+	__onAssetsUpdated: (...params: ApiCallerEventTypeV2<DB_Asset>) => void;
 }
 
 export const dispatch_onAssetsListChanged = new ThunderDispatcher<OnAssetsUpdated, '__onAssetsUpdated'>('__onAssetsUpdated');
 
 export class ModuleFE_Assets_Class
-	extends BaseDB_ApiGeneratorCaller<DB_Asset> {
-
+	extends BaseDB_ApiCaller<DB_Asset> {
 
 	constructor() {
-		super(DBDef_Assets);
-		this.setDefaultDispatcher(dispatch_onAssetsListChanged);
+		super(DBDef_Assets, dispatch_onAssetsListChanged);
+	}
+
+	async resolveValidSignedUrl(assetId: string) {
+		const asset = this.cache.unique(assetId);
+		const signedUrl = (asset?.signedUrl?.validUntil || 0) > currentTimeMillis() ? asset?.signedUrl : undefined;
+		if (signedUrl)
+			return signedUrl.url;
+
+
+		return (await ModuleFE_Assets.v1.queryUnique(assetId).executeSync()).signedUrl!.url;
 	}
 }
 
