92f54ee53548d87714e6854f106c1bda55d1e14c
TacB0sS <adam.vdk@nu-art-software.com>
2023-01-26 17:17:36 +0200

    tsValidateCustom

--- ts-common/src/main/validator/type-validators.ts
+++ ts-common/src/main/validator/type-validators.ts
@@ -1,162 +1,164 @@
-import {__stringify} from "../utils/tools";
+import {__stringify} from '../utils/tools';
 import {
-    InvalidResult,
-    InvalidResultArray, InvalidResultObject,
-    tsValidate,
-    tsValidateExists,
-    tsValidateResult,
-    Validator,
-    ValidatorTypeResolver
-} from "./validator-core";
-import {currentTimeMillis} from "../utils/date-time-tools";
-import {ArrayType, AuditBy, RangeTimestamp} from "../utils/types";
-import {filterInstances} from "../utils/array-tools";
-import {_keys} from "../utils/object-tools";
+	InvalidResult,
+	InvalidResultArray, InvalidResultObject,
+	tsValidate,
+	tsValidateExists,
+	tsValidateResult,
+	Validator, ValidatorImpl,
+	ValidatorTypeResolver
+} from './validator-core';
+import {currentTimeMillis} from '../utils/date-time-tools';
+import {ArrayType, AuditBy, RangeTimestamp} from '../utils/types';
+import {filterInstances} from '../utils/array-tools';
+import {_keys} from '../utils/object-tools';
+
 
 export const tsValidateDynamicObject = <T extends object>(valuesValidator: ValidatorTypeResolver<T[keyof T]>, keysValidator: ValidatorTypeResolver<string>, mandatory = true) => {
-    return [tsValidateExists(mandatory), (input?: T) => {
-        if (!input)
-            return
+	return [tsValidateExists(mandatory), (input?: T) => {
+		if (!input)
+			return;
 
-        const keys = _keys(input) as string[];
-        return keys.reduce<InvalidResultObject<T>>((res, key) => {
-            const _valRes = tsValidateResult(input[key as keyof T], valuesValidator);
-            const _keyRes = tsValidateResult(key, keysValidator);
+		const keys = _keys(input) as string[];
+		return keys.reduce<InvalidResultObject<T>>((res, key) => {
+			const _valRes = tsValidateResult(input[key as keyof T], valuesValidator);
+			const _keyRes = tsValidateResult(key, keysValidator);
 
-            if (_valRes && _keyRes)
-                res[key as keyof T] = `Key: ${_keyRes}}\nValue: ${_valRes}` as InvalidResult<T[keyof T]>;
-            else
-                _valRes ? res[key as keyof T] = 'Value: ' + _valRes as InvalidResult<T[keyof T]> : res[key as keyof T] = 'Key: ' + _keyRes as InvalidResult<T[keyof T]>;
+			if (_valRes && _keyRes)
+				res[key as keyof T] = `Key: ${_keyRes}}\nValue: ${_valRes}` as InvalidResult<T[keyof T]>;
+			else
+				_valRes ? res[key as keyof T] = 'Value: ' + _valRes as InvalidResult<T[keyof T]> : res[key as keyof T] = 'Key: ' + _keyRes as InvalidResult<T[keyof T]>;
 
-            return res
-        }, {});
-    }];
+			return res;
+		}, {});
+	}];
 };
 
 export const tsValidateUnion = <T extends any>(validators: ValidatorTypeResolver<T>[], mandatory = true) => {
-    return [tsValidateExists(mandatory), (input?: any) => {
-        const results: InvalidResultArray<T>[] = [];
-        for (const validator of validators) {
-            const _res = tsValidateResult(input, validator);
-            if (!_res)
-                return;
-            results.push(_res);
-        }
+	return [tsValidateExists(mandatory), (input?: any) => {
+		const results: InvalidResultArray<T>[] = [];
+		for (const validator of validators) {
+			const _res = tsValidateResult(input, validator);
+			if (!_res)
+				return;
+			results.push(_res);
+		}
 
-        return filterInstances(results).length !== 0 ? ['Input does not match any of the possible types', results] as InvalidResultArray<T>[] : undefined;
-    }];
+		return filterInstances(results).length !== 0 ? ['Input does not match any of the possible types', results] as InvalidResultArray<T>[] : undefined;
+	}];
 };
 
-export const tsValidateUnionV2 = <T extends any>(processor: (input?: T, parentInput?: any) => InvalidResult<T>, mandatory = true): Validator<T> => {
-    return [tsValidateExists(mandatory), processor];
+export const tsValidateCustom = <T extends any>(processor: (input?: T, parentInput?: any) => InvalidResult<T>, mandatory = true): Validator<T>[] => {
+	return [tsValidateExists(mandatory), processor];
 };
 
 const typeFunc = (type: any) => typeof type;
 type types = ReturnType<typeof typeFunc>;
 type validatorObject<T> = { [k in types]?: ValidatorTypeResolver<T> }
 export const tsValidateUnionV3 = <T extends any>(validatorObject: validatorObject<T>, mandatory = true) => {
-    return [tsValidateExists(mandatory), (input?: T) => {
-        const _type = typeof input;
-        return _keys(validatorObject).includes(_type) ? tsValidateResult(input, validatorObject[_type] as ValidatorTypeResolver<T>) : 'Input does not match any of the possible types';
-    }];
+	return [tsValidateExists(mandatory), (input?: T) => {
+		const _type = typeof input;
+		return _keys(validatorObject)
+			.includes(_type) ? tsValidateResult(input, validatorObject[_type] as ValidatorTypeResolver<T>) : 'Input does not match any of the possible types';
+	}];
 };
 
 export const tsValidateArray = <T extends any[], I extends ArrayType<T> = ArrayType<T>>(validator: ValidatorTypeResolver<I>, mandatory = true, minimumLength: number = 0): Validator<I[]> => {
-    return [tsValidateExists(mandatory), (input?: I[]) => {
-        const results: InvalidResultArray<I>[] = [];
-        const _input = input as unknown as I[];
-        if (_input.length < minimumLength)
-            return 'Array length smaller than minimum defined length';
-        for (let i = 0; i < _input.length; i++) {
-            results[i] = tsValidateResult(_input[i], validator);
-        }
+	return [tsValidateExists(mandatory), (input?: I[]) => {
+		const results: InvalidResultArray<I>[] = [];
+		const _input = input as unknown as I[];
+		if (_input.length < minimumLength)
+			return 'Array length smaller than minimum defined length';
+		for (let i = 0; i < _input.length; i++) {
+			results[i] = tsValidateResult(_input[i], validator);
+		}
 
-        return filterInstances(results).length !== 0 ? results : undefined;
-    }];
+		return filterInstances(results).length !== 0 ? results : undefined;
+	}];
 };
 export const tsValidateString = (length: number = -1, mandatory = true): Validator<string> => {
-    return [tsValidateExists(mandatory), (input?: string) => {
-        // noinspection SuspiciousTypeOfGuard
-        if (typeof input !== 'string')
-            return `input is not a string`;
+	return [tsValidateExists(mandatory), (input?: string) => {
+		// noinspection SuspiciousTypeOfGuard
+		if (typeof input !== 'string')
+			return `input is not a string`;
 
-        if (length === -1)
-            return;
+		if (length === -1)
+			return;
 
-        if (input.length <= length)
-            return;
+		if (input.length <= length)
+			return;
 
-        return `input is longer than ${length}`;
-    }];
+		return `input is longer than ${length}`;
+	}];
 };
 
 export const tsValidateNumber = (mandatory = true): Validator<number> => {
-    return [tsValidateExists(mandatory), (input?: number) => {
-        // noinspection SuspiciousTypeOfGuard
-        if (typeof input === 'number')
-            return;
+	return [tsValidateExists(mandatory), (input?: number) => {
+		// noinspection SuspiciousTypeOfGuard
+		if (typeof input === 'number')
+			return;
 
-        return `Input is not a number! \nvalue: ${input}\ntype: ${typeof input}`;
-    }];
+		return `Input is not a number! \nvalue: ${input}\ntype: ${typeof input}`;
+	}];
 };
 
 export const tsValidateBoolean = (mandatory = true): Validator<boolean> => {
-    return [tsValidateExists(mandatory), (input?: boolean) => {
-        // noinspection SuspiciousTypeOfGuard
-        if (typeof input === 'boolean')
-            return;
+	return [tsValidateExists(mandatory), (input?: boolean) => {
+		// noinspection SuspiciousTypeOfGuard
+		if (typeof input === 'boolean')
+			return;
 
-        return `Input is not a boolean! \nvalue: ${input}\ntype: ${typeof input}`;
-    }];
+		return `Input is not a boolean! \nvalue: ${input}\ntype: ${typeof input}`;
+	}];
 };
 
 export const tsValidateValue = (values: string[], mandatory = true): Validator<string> => {
-    return [tsValidateExists(mandatory), (input?: string) => {
-        if (values.includes(input!))
-            return;
+	return [tsValidateExists(mandatory), (input?: string) => {
+		if (values.includes(input!))
+			return;
 
-        return `Input is not valid:\n  input: ${input}\n  options: ${__stringify(values)}\n`;
-    }];
+		return `Input is not valid:\n  input: ${input}\n  options: ${__stringify(values)}\n`;
+	}];
 };
 
 export const tsValidateRange = (ranges: [number, number][], mandatory = true): Validator<number> => {
-    return [tsValidateExists(mandatory), (input?: number) => {
-        for (const range of ranges) {
-            if (range[0] <= input! && input! <= range[1])
-                return;
-        }
+	return [tsValidateExists(mandatory), (input?: number) => {
+		for (const range of ranges) {
+			if (range[0] <= input! && input! <= range[1])
+				return;
+		}
 
-        return `Input is not valid:\n  input: ${input}\n  Expected Range: ${__stringify(ranges)}\n`;
-    }];
+		return `Input is not valid:\n  input: ${input}\n  Expected Range: ${__stringify(ranges)}\n`;
+	}];
 };
 
 export const tsValidateRegexp = (regexp: RegExp, mandatory = true): Validator<string> => {
-    return [tsValidateExists(mandatory), (input?: string) => {
-        // console.log({input, regexp});
-        if (regexp.test(input!))
-            return;
+	return [tsValidateExists(mandatory), (input?: string) => {
+		// console.log({input, regexp});
+		if (regexp.test(input!))
+			return;
 
-        return `Input does not match regexp:\n  input: ${input}\n  regexp: ${regexp}\n`;
-    }];
+		return `Input does not match regexp:\n  input: ${input}\n  regexp: ${regexp}\n`;
+	}];
 };
 
 export const tsValidateMD5 = (mandatory = true): Validator<string> => {
-    return tsValidateRegexp(/[a-zA-Z\d]{32}/, mandatory);
+	return tsValidateRegexp(/[a-zA-Z\d]{32}/, mandatory);
 };
 
 export const tsValidateTimestamp = (interval?: number, mandatory = true): Validator<number> => {
-    return [tsValidateExists(mandatory), (input?: number) => {
-        const now = currentTimeMillis();
-        const minTimestamp = now - (interval || now);
-        if (minTimestamp <= input! && input! <= now)
-            return;
+	return [tsValidateExists(mandatory), (input?: number) => {
+		const now = currentTimeMillis();
+		const minTimestamp = now - (interval || now);
+		if (minTimestamp <= input! && input! <= now)
+			return;
 
-        return `Input is not valid:\n  input: ${input}\n  Expected Interval: ${minTimestamp} - ${now}\n`;
-    }];
+		return `Input is not valid:\n  input: ${input}\n  Expected Interval: ${minTimestamp} - ${now}\n`;
+	}];
 };
 
 export const tsValidateAudit = (range?: RangeTimestamp) => {
-    return (audit?: AuditBy) => {
-        return tsValidate(audit?.auditAt?.timestamp, tsValidateRange([[0, Number.MAX_VALUE]]));
-    };
+	return (audit?: AuditBy) => {
+		return tsValidate(audit?.auditAt?.timestamp, tsValidateRange([[0, Number.MAX_VALUE]]));
+	};
 };
