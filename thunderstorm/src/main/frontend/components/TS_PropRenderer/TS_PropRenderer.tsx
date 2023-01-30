c4fe56e13f9c22e539cb5aca14a990efe85f4f2d
Cipher <itayleybovich@gmail.com>
2023-01-30 13:48:41 +0200

    utility funcs, ts-proprenderer

--- thunderstorm/src/main/frontend/components/TS_PropRenderer/TS_PropRenderer.tsx
+++ thunderstorm/src/main/frontend/components/TS_PropRenderer/TS_PropRenderer.tsx
@@ -1,20 +1,31 @@
 import './TS_PropRenderer.scss';
 import * as React from 'react';
-import {ReactNode} from 'react';
-import {LL_H_C} from '../Layouts';
+import {LinearLayoutProps, LL_H_C, LL_V_L} from '../Layouts';
 import {_className} from '../../utils/tools';
 
-type Props = {
+type Props = LinearLayoutProps & {
 	label: string,
-	children?: ReactNode
-	onClick?: (e: React.MouseEvent) => void;
-	className?: string;
 }
 
-export const TS_PropRenderer = (props: Props) => {
+export const TS_PropRenderer_Horizontal = (props: Props) => {
 	const className = _className('ts-prop-renderer', props.className);
-	return <LL_H_C className={className} onClick={e => props.onClick?.(e)}>
-		<div className="ts-prop-renderer__label">{props.label}</div>
+	const {label, ..._props} = props;
+	return <LL_H_C {..._props} className={className}>
+		<div className="ts-prop-renderer__label">{label}</div>
 		{props.children}
 	</LL_H_C>;
+};
+
+export const TS_PropRenderer_Vertical = (props: Props) => {
+	const className = _className('ts-prop-renderer', props.className);
+	const {label, ..._props} = props;
+	return <LL_V_L {..._props} className={className}>
+		<div className="ts-prop-renderer__label">{props.label}</div>
+		{props.children}
+	</LL_V_L>;
+};
+
+export const TS_PropRenderer = {
+	Vertical: TS_PropRenderer_Vertical,
+	Horizontal: TS_PropRenderer_Horizontal,
 };
