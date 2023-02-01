a2251f14a4f4fcc81ab06eacaa45a2c94299bd64
alon ninio <alonninio@alons-MacBook-Pro.local>
2023-02-01 17:19:46 +0200

    added tests for firebase

--- firebase/src/test/database/test/remove-data.ts
+++ firebase/src/test/database/test/remove-data.ts
@@ -20,68 +20,68 @@ import {__scenario} from '@nu-art/testelot';
 import {myDb} from '../_core/database-wrapper';
 import {assert, BadImplementationException} from '@nu-art/ts-common';
 
-const removeData = () => myDb.processDirty('Removing a node', async db => {
-	await db.set(objectModel.path, objectModel.value);
-	await db.delete(objectModel.path);
-	const nullVal = await db.get(objectModel.path);
-	assert('Values don\'t match', nullVal, undefined);
+export const removeData = () => myDb.processDirty('Removing a node', async db => {
+    await db.set(objectModel.path, objectModel.value);
+    await db.delete(objectModel.path);
+    const nullVal = await db.get(objectModel.path);
+    assert('Values don\'t match', nullVal, undefined);
 });
 
-const removeHigherNode = () => myDb.processDirty('Remove a higher node deleted data under it', async db => {
-	await db.set(objectModel.path, objectModel.value);
-	await db.delete(basePath, basePath);
-	const nullVal = await db.get(objectModel.path);
-	assert('Values don\'t match', nullVal, undefined);
+export const removeHigherNode = () => myDb.processDirty('Remove a higher node deleted data under it', async db => {
+    await db.set(objectModel.path, objectModel.value);
+    await db.delete(basePath, basePath);
+    const nullVal = await db.get(objectModel.path);
+    assert('Values don\'t match', nullVal, undefined);
 });
 
-const removeLowerNode = () => myDb.processDirty('Remove a lower node only deleted the lower part of it', async db => {
-	await db.set(objectModel.path, objectModel.value);
-	await db.delete(`${objectModel.path}/a`);
-	const val = await db.get(objectModel.path);
-	const compareVal = objectModel.value;
-	delete compareVal.a;
-	assert('Values don\'t match', val, compareVal);
+export const removeLowerNode = () => myDb.processDirty('Remove a lower node only deleted the lower part of it', async db => {
+    await db.set(objectModel.path, objectModel.value);
+    await db.delete(`${objectModel.path}/a`);
+    const val = await db.get(objectModel.path);
+    const compareVal = objectModel.value;
+    delete compareVal.a;
+    assert('Values don\'t match', val, compareVal);
 });
 
-const removeFail = (model: ModelFail) => myDb.processDirty(model.label, async db => {
-	await db.delete(model.path as string);
+export const removeFail = (model: ModelFail) => myDb.processDirty(model.label, async db => {
+    await db.delete(model.path as string);
 }).expectToFail(BadImplementationException, e => e.message.startsWith(model.errorMessage));
 
 type ModelFail = {
-	path?: string
-	errorMessage: string
-	label: string
+    path?: string
+    errorMessage: string
+    label: string
 }
 
 const models: ModelFail[] = [
-	{
-		path: undefined,
-		errorMessage: 'Falsy value, path: ',
-		label: 'Pass undefined to remove should fail'
-	},
-	{
-		path: '',
-		errorMessage: 'Falsy value, path: ',
-		label: 'Pass empty string to remove should fail'
-	},
-	{
-		path: '/',
-		errorMessage: `path: '/'  does not match assertion: `,
-		label: 'Root removal without check is not allowed'
-	},
-	{
-		path: '/some',
-		errorMessage: `path: '/some'  does not match assertion: `,
-		label: 'First level removal without check is not allowed'
-	}
+    {
+        path: undefined,
+        errorMessage: 'Falsy value, path: ',
+        label: 'Pass undefined to remove should fail'
+    },
+    {
+        path: '',
+        errorMessage: 'Falsy value, path: ',
+        label: 'Pass empty string to remove should fail'
+    },
+    {
+        path: '/',
+        errorMessage: `path: '/'  does not match assertion: `,
+        label: 'Root removal without check is not allowed'
+    },
+    {
+        path: '/some',
+        errorMessage: `path: '/some'  does not match assertion: `,
+        label: 'First level removal without check is not allowed'
+    }
 ];
 
 const basePath = '/test';
 const path = `${basePath}/object`;
 const objectModel: { path: string; label: string; value: { a?: number; b: string } } = {
-	path: path,
-	value: {a: 1, b: 'cc'},
-	label: 'Removing a node'
+    path: path,
+    value: {a: 1, b: 'cc'},
+    label: 'Removing a node'
 };
 
 export const scenarioRemoveData = __scenario('Remove data');
