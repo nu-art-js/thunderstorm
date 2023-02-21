c352cb578be090888134da10415bdf1f00ee567c
Matan Koby <matan.k@quai.md>
2023-02-21 16:24:45 +0200

    more tests

---
+++ ts-common/src/test/array-tools/cases/removeFromArray.ts
@@ -0,0 +1,62 @@
+import {expect} from 'chai';
+import {removeFromArray, TestSuite} from '../../_main';
+
+type Input<T = any> = {
+	array: T[]
+	item: (item: T) => boolean
+}
+
+const TestCase_removeFromArray: TestSuite<Input, any> ['testcases'] = [
+	{
+		description: 'Test 1',
+		result: [1, 3],
+		input: {
+			array: [1, 2, 3],
+			item: (item) => item % 2 === 0
+		}
+	},
+	{
+		description: 'Test 2',
+		result: [4, 8, 10],
+		input: {
+			array: [4, 8, 10, 9],
+			item: (item) => item % 2 === 1
+		}
+	},
+	{
+		description: 'Test 3',
+		result: [{a: 1}, {a: 2}],
+		input: {
+			array: [{a: 1}, {a: 2}, {a: 3}],
+			item: (item) => JSON.stringify(item) === JSON.stringify({a: 3})
+		}
+	},
+	{
+		description: 'Test 4',
+		result: ['one', 'two'],
+		input: {
+			array: ['one', 'two', 'three'],
+			item: (item) => item === 'three'
+		}
+	},
+	{
+		description: 'Test 5',
+		result: [{a: 1}, {a: 2}, {b: 5, a: 3}],
+		input: {
+			array: [{a: 1}, {a: 2}, {b: 5, a: 3}],
+			item: (item) => JSON.stringify(item) === JSON.stringify({a: 3, b: 5})
+		}
+	},
+];
+
+export const TestSuite_removeFromArray: TestSuite<Input, any> = {
+	label: 'removeFromArray',
+	testcases: TestCase_removeFromArray,
+	processor: async (testCase) => {
+		it(testCase.description, () => {
+			const result = removeFromArray(testCase.input.array, testCase.input.item);
+			const expected = testCase.result;
+			expect(result).to.eql(expected);
+		});
+	}
+};
--- ts-common/src/test/array-tools/tests.ts
+++ ts-common/src/test/array-tools/tests.ts
@@ -1,13 +1,22 @@
-import {testSuitTester} from '../_main';
+import {reduceToMap, removeFromArrayByIndex, testSuitTester} from '../_main';
 import {TestSuite_arrayToMap} from './cases/arrayToMap';
 import {TestSuite_removeItemFromArray} from './cases/removeItemFromArray';
+import {TestSuite_removeFromArray} from './cases/removeFromArray';
+import {TestSuite_removeFromArrayByIndex} from './cases/removeFromArrayByIndex';
+import {TestSuite_reduceToMap} from './cases/reduceToMap';
 
 export const arrayToolsTests = {
 	arrayToMap: () => testSuitTester(TestSuite_arrayToMap),
 	removeItemFromArray: () => testSuitTester(TestSuite_removeItemFromArray),
+	removeFromArray: () => testSuitTester(TestSuite_removeFromArray),
+	removeFromArrayByIndex: () => testSuitTester(TestSuite_removeFromArrayByIndex),
+	reduceToMap: () => testSuitTester(TestSuite_reduceToMap),
 };
 
 export function runAllTests() {
 	arrayToolsTests.arrayToMap();
 	arrayToolsTests.removeItemFromArray();
+	arrayToolsTests.removeFromArray();
+	arrayToolsTests.removeFromArrayByIndex();
+	arrayToolsTests.reduceToMap();
 }
