09af06740cc9ff892ee1fb5afb865deff1bbefc1
Matan Koby <matan.k@quai.md>
2023-02-21 11:48:38 +0200

    test compilation fix

--- firebase/src/test/firestore/insert/cases.ts
+++ firebase/src/test/firestore/insert/cases.ts
@@ -1,8 +1,8 @@
-import {cloneObj, compare, TestSuit_V3} from '@nu-art/ts-common';
 import {expect} from 'chai';
 import {FirestoreCollection, ModuleBE_Firebase} from '../../../main/backend';
 import {testInstance1, testString1} from '../_core/consts';
 import {FB_Type} from '../_core/types';
+import {TestSuit} from '@nu-art/ts-common/testing/test-case';
 
 
 type Input = {
@@ -12,7 +12,7 @@ type Input = {
 }
 
 
-export const TestCase_ts_FB_insert: TestSuit_V3<Input, FB_Type> ['testcases'] = [
+export const TestCase_ts_FB_insert: TestSuit<Input, FB_Type> ['testcases'] = [
 	{
 		description: 'Insert one item and find in getAll[0]',
 		result: testInstance1,
@@ -40,7 +40,7 @@ export const TestCase_ts_FB_insert: TestSuit_V3<Input, FB_Type> ['testcases'] = 
 	}
 ];
 
-export const TestSuit_ts_FB_insert: TestSuit_V3<Input, any> = {
+export const TestSuit_ts_FB_insert: TestSuit<Input, any> = {
 	label: 'Firestore insertion tests',
 	testcases: TestCase_ts_FB_insert,
 	processor: async (testCase) => {
