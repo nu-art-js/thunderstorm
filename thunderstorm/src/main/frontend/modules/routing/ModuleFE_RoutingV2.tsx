b44970094da354d32525eef0c2d4f00a9ab77148
Cipher <itayleybovich@gmail.com>
2023-04-16 16:44:03 +0300

    dt align, virtualizedList fix, RoutingV2 fixes and improvements

--- thunderstorm/src/main/frontend/modules/routing/ModuleFE_RoutingV2.tsx
+++ thunderstorm/src/main/frontend/modules/routing/ModuleFE_RoutingV2.tsx
@@ -1,9 +1,10 @@
 import * as React from 'react';
-import {BrowserRouter, Navigate, NavigateFunction, Route, Routes} from 'react-router-dom';
+import {BrowserRouter, Navigate, NavigateFunction, NavLink, NavLinkProps, Route, Routes} from 'react-router-dom';
 import {TS_Route} from './types';
 import {BadImplementationException, composeUrl, Module, removeItemFromArray} from '@nu-art/ts-common';
 import {LocationChangeListener} from './LocationChangeListener';
 import {QueryParams} from '../../../shared';
+import {mouseEventHandler} from '../../utils/tools';
 
 
 class ModuleFE_RoutingV2_Class
@@ -43,7 +44,6 @@ class ModuleFE_RoutingV2_Class
 
 	private routeBuilder = (route: TS_Route<any>, _path: string = '') => {
 		const path = `${_path}/`;
-		console.log('route: ', `${route.key} - ${route.path} - ${path}`);
 		this.routesMapByKey[route.key] = {route, fullPath: _path};
 		this.routesMapByPath[_path] = route;
 
@@ -79,6 +79,10 @@ class ModuleFE_RoutingV2_Class
 		return this.routesMapByKey[routeKey]?.route;
 	}
 
+	getFullPath(routeKey: string): string | undefined {
+		return this.routesMapByKey[routeKey]?.fullPath;
+	}
+
 	getCurrentRouteKey() {
 		return this.routesMapByPath[window.location.pathname];
 	}
@@ -87,5 +91,22 @@ class ModuleFE_RoutingV2_Class
 		this.navigate = navigate;
 	}
 }
+
+export const TS_NavLink = (props: { route: TS_Route } & Partial<NavLinkProps>) => {
+	const {route, children, ..._props} = props;
+
+	const fullPath = ModuleFE_RoutingV2.getFullPath(route.key);
+	if (!fullPath)
+		throw new BadImplementationException(`Route with key ${route.key} is not defined in routing module`);
+
+	return <NavLink
+		{..._props}
+		to={fullPath}
+		key={route.key}
+		onMouseUp={e => mouseEventHandler(e, {
+			middle: () => window.open(fullPath, '_blank'),
+		})}
+	>{children}</NavLink>;
+};
 
 export const ModuleFE_RoutingV2 = new ModuleFE_RoutingV2_Class();
