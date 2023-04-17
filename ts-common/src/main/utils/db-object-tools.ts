016eccf5beab942b3439cb172fef247146a68104
Matan Koby <matan.k@quai.md>
2023-04-17 12:33:28 +0300

    function to remove dbObject keys from instance

--- ts-common/src/main/utils/db-object-tools.ts
+++ ts-common/src/main/utils/db-object-tools.ts
@@ -1,8 +1,15 @@
-import {DB_Object} from './types';
+import {DB_Object, OmitDBObject} from './types';
+import {deepClone} from './object-tools';
 
 
 export const KeysOfDB_Object: (keyof DB_Object)[] = ['_id', '_v', '__created', '__updated'];
 
 export function dbObjectToId(i: DB_Object) {
 	return i._id;
+}
+
+export function removeDBObjectKeys<T extends DB_Object>(instance: T): OmitDBObject<T> {
+	const _instance = deepClone(instance);
+	KeysOfDB_Object.forEach(key => delete _instance[key]);
+	return _instance;
 }
