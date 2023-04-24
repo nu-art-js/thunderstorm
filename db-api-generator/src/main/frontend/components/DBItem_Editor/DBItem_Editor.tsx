c4ec5d48cbd98b9f41eb3c0123b8384c787ffc74
Harel  Dagan <hareldagan@Harels-MacBook-Pro.local>
2023-04-24 15:40:01 +0300

    dbitem_editor is now item_editor, also fixed key issue with additional overlays in appwrapper

--- db-api-generator/src/main/frontend/components/DBItem_Editor/DBItem_Editor.tsx
+++ db-api-generator/src/main/frontend/components/DBItem_Editor/DBItem_Editor.tsx
@@ -1,8 +1,6 @@
 import * as React from 'react';
 import {Component} from 'react';
-import {TS_Input, TS_PropRenderer} from '@nu-art/thunderstorm/frontend';
-import {DB_Object} from '@nu-art/ts-common';
-import {EditableDBItem} from '../../utils/EditableDBItem';
+import {EditableItem, TS_Input, TS_PropRenderer} from '@nu-art/thunderstorm/frontend';
 
 
 type AssetValueType<T, K extends keyof T, Ex> = T[K] extends Ex ? K : never
@@ -10,31 +8,36 @@ type AssetValueType<T, K extends keyof T, Ex> = T[K] extends Ex ? K : never
 type InputProps<Value, Ex extends string | undefined = string | undefined> = {
 	disabled?: boolean,
 	className?: string,
+	onBlur?: (value: string) => void,
 	readProcessor?: (value: Value) => Ex
 	writeProcessor?: (value: Ex) => Value
 };
 
-export class DBItem_Editor<DBItem extends DB_Object, State extends {} = {}>
-	extends Component<{ editable: EditableDBItem<DBItem> }, State> {
-	input = <K extends keyof DBItem, Ex extends string | undefined = string | undefined>(prop: AssetValueType<DBItem, K, Ex>, inputProps?: InputProps<DBItem[K], Ex>) => {
+export class Item_Editor<Item, State extends {} = {}>
+	extends Component<{ editable: EditableItem<Item> }, State> {
+	input = <K extends keyof Item, Ex extends string | undefined = string | undefined>(prop: AssetValueType<Item, K, Ex>, inputProps?: InputProps<Item[K], Ex>) => {
 		const value = this.props.editable.item[prop] as string | undefined;
 		return {
 			vertical: (label: string, props?: { className: string }) => {
-				const {readProcessor, writeProcessor, ...restProps} = inputProps || {};
+				const {readProcessor, writeProcessor, onBlur, ...restProps} = inputProps || {};
 				return <TS_PropRenderer.Vertical label={label} {...props}>
-					<TS_Input {...restProps}
-										type="text"
-										value={readProcessor?.(value as unknown as DBItem[K]) || value}
-										onBlur={value => this.props.editable.update(prop, writeProcessor?.(value as Ex) || value as unknown as DBItem[K])}/>
+					<TS_Input type="text"
+										value={readProcessor?.(value as unknown as Item[K]) || value}
+										onBlur={value => {
+											onBlur ? onBlur(value) : this.props.editable.update(prop, writeProcessor?.(value as Ex) || value as unknown as Item[K]);
+										}}
+										{...restProps}/>
 				</TS_PropRenderer.Vertical>;
 			},
 			horizontal: (label: string, props?: { className: string }) => {
-				const {readProcessor, writeProcessor, ...restProps} = inputProps || {};
+				const {readProcessor, writeProcessor, onBlur, ...restProps} = inputProps || {};
 				return <TS_PropRenderer.Horizontal label={label} {...props}>
-					<TS_Input {...restProps}
-										type="text"
-										value={readProcessor?.(value as unknown as DBItem[K]) || value}
-										onBlur={value => this.props.editable.update(prop, writeProcessor?.(value as Ex) || value as unknown as DBItem[K])}/>
+					<TS_Input type="text"
+										value={readProcessor?.(value as unknown as Item[K]) || value}
+										onBlur={value => {
+											onBlur ? onBlur(value) : this.props.editable.update(prop, writeProcessor?.(value as Ex) || value as unknown as Item[K]);
+										}}
+										{...restProps}/>
 				</TS_PropRenderer.Horizontal>;
 			}
 		};
