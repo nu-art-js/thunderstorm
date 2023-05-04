15534c1376aeff8d846bdcf3deb6f5134152c012
TacB0sS <adam.vdk@nu-art-software.com>
2023-05-04 11:14:59 +0300

    app tools simplified multiselect css

--- thunderstorm/src/main/frontend/components/TS_AppTools/types.ts
+++ thunderstorm/src/main/frontend/components/TS_AppTools/types.ts
@@ -1,11 +1,9 @@
 import * as React from 'react';
 
-export type AppToolsScreenType = 'dev' | 'playground'
 
 export type AppToolsScreen = {
+	key?: string;
 	name: string;
-	key: string;
-	type: AppToolsScreenType
 	renderer: React.ComponentType;
 	icon?: React.ComponentType;
 }
