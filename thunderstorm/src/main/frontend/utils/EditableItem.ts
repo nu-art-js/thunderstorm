70174096082931b9e4a3c874310d162c4c8ffa7f
Harel  Dagan <hareldagan@Harels-MacBook-Pro.local>
2023-05-11 16:14:50 +0300

    auto save prop in editable item

--- thunderstorm/src/main/frontend/utils/EditableItem.ts
+++ thunderstorm/src/main/frontend/utils/EditableItem.ts
@@ -1,7 +1,8 @@
-import {cloneObj} from '@nu-art/ts-common';
+import {cloneObj, resolveContent} from '@nu-art/ts-common';
 
 export class EditableItem<T> {
 	item: Partial<T>;
+	autoSave: boolean = false;
 
 	constructor(item: Partial<T>, saveAction: (item: T) => Promise<any>, deleteAction: (item: T) => Promise<any>) {
 		this.item = Object.isFrozen(item) ? cloneObj(item) : item;
@@ -12,23 +13,24 @@ export class EditableItem<T> {
 	protected readonly saveAction: (item: T) => Promise<T>;
 	protected readonly deleteAction: (item: T) => Promise<void>;
 
-	set<K extends keyof T>(key: K, value: T[K] | undefined) {
-		if (value === undefined)
-			delete this.item[key];
-		else {
-			this.item[key] = value;
-		}
+	setAutoSave(mode: boolean) {
+		this.autoSave = mode;
+
+		return this;
 	}
 
 	async update<K extends keyof T>(key: K, value: ((item?: T[K]) => T[K]) | T[K] | undefined) {
-		let finalValue;
-		if (typeof value === 'function') { // @ts-ignore
-			finalValue = value(this.item[key]);
-		} else
-			finalValue = value;
+		const finalValue = resolveContent(value);
+		if (finalValue === this.item[key])
+			return;
 
-		this.set(key, finalValue);
-		return this.save();
+		if (value === undefined)
+			delete this.item[key];
+		else
+			this.item[key] = finalValue;
+
+		if (this.autoSave)
+			return this.save();
 	}
 
 	async save() {
@@ -36,7 +38,7 @@ export class EditableItem<T> {
 	}
 
 	clone(): EditableItem<T> {
-		return new EditableItem<T>(this.item, this.saveAction, this.deleteAction);
+		return new EditableItem<T>(this.item, this.saveAction, this.deleteAction).setAutoSave(this.autoSave);
 	}
 
 	async delete<K extends keyof T>() {
@@ -44,6 +46,6 @@ export class EditableItem<T> {
 	}
 
 	editProp<K extends keyof T>(key: K, defaultValue: Partial<NonNullable<T[K]>>) {
-		return new EditableItem<NonNullable<T[K]>>(this.item[key] || (this.item[key] = defaultValue as NonNullable<T[K]>), async (item: T[K]) => this.update(key, item), () => this.delete());
+		return new EditableItem<NonNullable<T[K]>>(this.item[key] || (this.item[key] = defaultValue as NonNullable<T[K]>), async (item: T[K]) => this.update(key, item), () => this.delete()).setAutoSave(this.autoSave);
 	}
 }
