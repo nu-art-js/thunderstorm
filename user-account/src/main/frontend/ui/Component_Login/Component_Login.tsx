56e77ee63c46771e29745dc9d3def395c3888606
Cipher <itayleybovich@gmail.com>
2023-05-18 18:11:03 +0300

    login and register components updated

--- user-account/src/main/frontend/ui/Component_Login/Component_Login.tsx
+++ user-account/src/main/frontend/ui/Component_Login/Component_Login.tsx
@@ -21,11 +21,12 @@ import {_keys} from '@nu-art/ts-common';
 import {ModuleFE_Account} from '../../modules/ModuleFE_Account';
 import {Request_LoginAccount} from '../../../shared/api';
 import './Component_Login.scss';
-import {LL_V_C, ModuleFE_Toaster, TS_Button, TS_Input} from '@nu-art/thunderstorm/frontend';
+import {ComponentSync, LL_V_C, ModuleFE_Toaster, TS_BusyButton, TS_Input, TS_PropRenderer} from '@nu-art/thunderstorm/frontend';
 
 
 type State<T> = {
 	data: Partial<T>
+	errorMessages?: string[];
 }
 
 type Props = {}
@@ -52,10 +53,23 @@ const form: Form<Request_LoginAccount> = {
 };
 
 export class Component_Login
-	extends React.Component<Props, State<Request_LoginAccount>> {
+	extends ComponentSync<Props, State<Request_LoginAccount>> {
 
-	state = {
-		data: {} as Partial<Request_LoginAccount>,
+	protected deriveStateFromProps(nextProps: Props, state: State<Request_LoginAccount>) {
+		state ??= this.state ? {...this.state} : {} as State<Request_LoginAccount>;
+		state.data ??= {};
+		return state;
+	}
+
+	private renderErrorMessages = () => {
+		if (!this.state.errorMessages?.length)
+			return '';
+
+		return <ul className={'ts-account__error-messages'}>
+			{this.state.errorMessages.map((message, i) => {
+				return <li key={i}>{message}</li>;
+			})}
+		</ul>;
 	};
 
 	render() {
@@ -63,29 +77,29 @@ export class Component_Login
 		return <LL_V_C className="ts-account__authenticate">
 			{_keys(form).map((key, i) => {
 					const field = form[key];
-					return <TS_Input
-						key={i}
-						id={key}
-						value={data[key]}
-						type={field.type}
-						placeholder={field.hint}
-						onChange={this.onValueChanged}
-						onAccept={this.loginClicked}
-					/>;
+					return <TS_PropRenderer.Vertical label={field.label} key={i}>
+						<TS_Input
+							id={key}
+							value={data[key]}
+							type={field.type}
+							onChange={this.onValueChanged}
+							onAccept={this.loginClicked}
+						/>
+					</TS_PropRenderer.Vertical>;
 				}
 			)}
-			<TS_Button onClick={this.loginClicked} className={`clickable ts-account__action-button`}>Login</TS_Button>
+			{this.renderErrorMessages()}
+			<TS_BusyButton onClick={this.loginClicked} className={`clickable ts-account__action-button`}>Login</TS_BusyButton>
 		</LL_V_C>;
 	}
 
 	private onValueChanged = (value: string, id: keyof Request_LoginAccount) => {
-		this.setState(state => {
-			state.data[id] = value;
-			return state;
-		});
+		const data = {...this.state.data};
+		data[id] = value;
+		this.setState({data, errorMessages: undefined});
 	};
 
-	private loginClicked = () => {
+	private loginClicked = async () => {
 		const data: Partial<Request_LoginAccount> = this.state.data;
 		const errors = _keys(form).map(key => {
 			const field = form[key];
@@ -99,6 +113,10 @@ export class Component_Login
 		if (errors.length > 0)
 			return ModuleFE_Toaster.toastError(`Wrong input:\n${errors.join('\n')}`);
 
-		ModuleFE_Account.v1.login(this.state.data as Request_LoginAccount).execute();
+		try {
+			await ModuleFE_Account.v1.login(this.state.data as Request_LoginAccount).executeSync();
+		} catch (err) {
+			this.setState({errorMessages: ['Email or password incorrect']});
+		}
 	};
 }
