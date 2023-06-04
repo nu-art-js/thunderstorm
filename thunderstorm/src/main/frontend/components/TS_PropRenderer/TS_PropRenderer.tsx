23b38822d41278d5bde4a78c85ad97b157593a78
Harel  Dagan <hareldagan@Harels-MacBook-Pro.local>
2023-06-04 13:57:29 +0300

    added vertical alignment props to ts prop renderer

--- thunderstorm/src/main/frontend/components/TS_PropRenderer/TS_PropRenderer.tsx
+++ thunderstorm/src/main/frontend/components/TS_PropRenderer/TS_PropRenderer.tsx
@@ -1,6 +1,6 @@
 import './TS_PropRenderer.scss';
 import * as React from 'react';
-import {LinearLayoutProps, LL_H_C, LL_V_L} from '../Layouts';
+import {LinearLayoutProps, LL_H_B, LL_H_C, LL_H_T, LL_V_L} from '../Layouts';
 import {_className} from '../../utils/tools';
 import {resolveContent} from '@nu-art/ts-common';
 
@@ -10,14 +10,32 @@ type Props = Omit<LinearLayoutProps, 'label'> & {
 	error?: string;
 }
 
-const TS_PropRenderer_Horizontal = (props: Props) => {
+type Props_Horizontal = Props & Partial<{
+	verticalAlignment: 'top' | 'center' | 'bottom'
+}>
+
+const TS_PropRenderer_Horizontal = (props: Props_Horizontal) => {
 	const className = _className('ts-prop-renderer horizontal', props.className);
 	const {label, error, ..._props} = props;
-	return <LL_H_C {..._props} className={className}>
+	let _LinearComponent: React.ElementType;
+
+	switch (props.verticalAlignment) {
+		case 'top':
+			_LinearComponent = LL_H_T;
+			break;
+		case 'bottom':
+			_LinearComponent = LL_H_B;
+			break;
+		default:
+			_LinearComponent = LL_H_C;
+			break;
+	}
+
+	return <_LinearComponent {..._props} className={className}>
 		<div className={'ts-prop-renderer__label'}>{resolveContent(label)}</div>
 		{props.children}
 		{/*<div className={'ts-prop-renderer__error'}>{props.error}</div>*/}
-	</LL_H_C>;
+	</_LinearComponent>;
 };
 
 const TS_PropRenderer_Vertical = (props: Props) => {
