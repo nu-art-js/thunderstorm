ceaa6ff031b2b6be0cba4f23496f8aedde468c6c
Yuval Talby <yuval@quai.md>
2023-06-18 18:00:30 +0300

    fixes to watch tests

--- firebase/src/test/firestore-v2/insert/insert.ts
+++ firebase/src/test/firestore-v2/insert/insert.ts
@@ -1,10 +1,10 @@
 import {expect} from 'chai';
-import {ModuleBE_Firebase} from '../../../main/backend';
 import {testInstance1, testInstance2} from '../_core/consts';
 import {DB_Type} from '../_core/types';
 import {TestSuite} from '@nu-art/ts-common/test-index';
 import {compare, PreDB} from '@nu-art/ts-common';
 import {FirestoreCollectionV2} from '../../../main/backend/firestore-v2/FirestoreCollectionV2';
+import {firestore} from "./__test";
 
 
 type Input = {
@@ -33,7 +33,6 @@ export const TestSuit_FirestoreV2_Insert: Test = {
 	label: 'Firestore insertion tests',
 	testcases: TestCases_FB_Insert,
 	processor: async (testCase) => {
-		const firestore = ModuleBE_Firebase.createAdminSession().getFirestoreV2();
 		const collection = firestore.getCollection<DB_Type>('firestore-insertion-tests');
 		const ref = collection.getDocumentRef(testCase.input.value);
 		await ref.set(testCase.input.value);
