85e19b431cf4e8707ef184b594c488783ba667c1
Yuval Talby <yuval@quai.md>
2023-06-18 18:00:30 +0300

    fixes to watch tests

--- firebase/src/test/firestore-v2/insert/__test.ts
+++ firebase/src/test/firestore-v2/insert/__test.ts
@@ -1,15 +1,17 @@
 import {testSuiteTester} from '@nu-art/ts-common/testing/consts';
 import {ModuleBE_Auth} from '@nu-art/google-services/backend';
-import {FIREBASE_DEFAULT_PROJECT_ID} from '../../../main/backend';
+import {FIREBASE_DEFAULT_PROJECT_ID, ModuleBE_Firebase} from '../../../main/backend';
 import {TestSuit_FirestoreV2_Insert} from './insert';
+import {generateHex} from "@nu-art/ts-common";
 
 
 const config = {
-	project_id: 'test',
+	project_id: generateHex(4),
 	databaseURL: 'http://localhost:8102/?ns=quai-md-dev',
 };
 
 ModuleBE_Auth.setDefaultConfig({auth: {[FIREBASE_DEFAULT_PROJECT_ID]: config}});
+export const firestore = ModuleBE_Firebase.createAdminSession().getFirestoreV2();
 
 describe('Firestore v2 - Insert', () => {
 	testSuiteTester(TestSuit_FirestoreV2_Insert);
