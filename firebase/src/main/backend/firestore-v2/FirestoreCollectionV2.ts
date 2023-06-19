cce8a5235cf8a6660044a6fa4176bc4f16ab4036
Matan Koby <matan.k@quai.md>
2023-06-19 10:57:32 +0300

    firestore v2 wip compiles

--- firebase/src/main/backend/firestore-v2/FirestoreCollectionV2.ts
+++ firebase/src/main/backend/firestore-v2/FirestoreCollectionV2.ts
@@ -16,13 +16,32 @@
  * limitations under the License.
  */
 
-import {BadImplementationException, DB_Object, exists, generateHex, PreDB, StaticLogger, Subset} from '@nu-art/ts-common';
-import {FirestoreType_Collection, FirestoreType_DocumentReference, FirestoreType_DocumentSnapshot} from '../firestore/types';
+import {
+	BadImplementationException,
+	batchAction,
+	currentTimeMillis,
+	DB_Object,
+	exists,
+	generateHex,
+	PreDB,
+	StaticLogger,
+	Subset
+} from '@nu-art/ts-common';
+import {
+	FirestoreType_Collection,
+	FirestoreType_DocumentReference,
+	FirestoreType_DocumentSnapshot
+} from '../firestore/types';
 import {Clause_Where, FilterKeys, FirestoreQuery} from '../../shared/types';
 import {FirestoreWrapperBEV2} from './FirestoreWrapperBEV2';
 import {Transaction} from 'firebase-admin/firestore';
 import {FirestoreInterfaceV2} from './FirestoreInterfaceV2';
+import {FirestoreTransaction} from '../firestore/FirestoreTransaction';
 
+export const dbIdLength = 32;
+
+type BatchOpMethod = 'delete' | 'set' | 'create' | 'update';
+type BulkOpMethod = 'delete' | 'set' | 'create' | 'update';
 
 /**
  * FirestoreCollection is a class for handling Firestore collections. It takes in the name, FirestoreWrapperBE instance, and uniqueKeys as parameters.
@@ -66,11 +85,17 @@ export class FirestoreCollectionV2<Type extends DB_Object> {
 	}
 
 	getDocumentRef(value: PreDB<Type>) {
-		value._id ??= generateHex(32);
+		if (!exists(value._id))
+			throw new BadImplementationException('Cannot create DocWrapper without _id!');
+
 		const doc = this.wrapper.firestore.doc(`${this.name}/${value._id}`) as FirestoreType_DocumentReference<Type>;
 		return new DocWrapperV2<Type>(this.wrapper, doc);
 	}
 
+	/**
+	 * Get the db objects from the query
+	 * @param ourQuery
+	 */
 	async queryInstances(ourQuery: FirestoreQuery<Type>): Promise<Type[]> {
 		return (await this._query(ourQuery)).map(result => result.data() as Type);
 	}
@@ -80,6 +105,10 @@ export class FirestoreCollectionV2<Type extends DB_Object> {
 		return (await myQuery.get()).docs as FirestoreType_DocumentSnapshot[];
 	}
 
+	/**
+	 * Get DocWrappers per the db objects from the query
+	 * @param ourQuery
+	 */
 	async newQuery(ourQuery: FirestoreQuery<Type>): Promise<DocWrapperV2<Type>[]> {
 		const docs = await this._query(ourQuery) as FirestoreType_DocumentSnapshot<Type>[];
 		return docs.filter(doc => doc.exists).map(doc => new DocWrapperV2<Type>(this.wrapper, doc.ref, doc.data()));
@@ -87,6 +116,95 @@ export class FirestoreCollectionV2<Type extends DB_Object> {
 
 	async getAll() {
 
+	}
+
+	async test_DeleteAll() {
+		await this.collection.doc().delete();
+	}
+
+	private async assertInstance(dbInstance: Type, transaction?: FirestoreTransaction, request?: Express.Request) {
+		await this.upgradeInstances([dbInstance]);
+		await this.preUpsertProcessing(dbInstance, transaction, request);
+		this.validateImpl(dbInstance);
+		await this.assertUniqueness(dbInstance, transaction, request);
+	}
+
+	private upgradeInstances(dbInstances: Type[]) {
+		//todo - maybe should be filled only in extending modules
+	}
+
+	private async preUpsertProcessing(dbInstance: Type, transaction?: FirestoreTransaction, request?: Express.Request) {
+		//todo - maybe should be filled only in extending modules
+	}
+
+	/**
+	 * Validate
+	 * @param dbInstance dbObject to validate
+	 * @private
+	 */
+	private validateImpl(dbInstance: Type) {
+		//todo validation using validator
+	}
+
+	private assertUniqueness(dbInstance: Type, transaction?: FirestoreTransaction, request?: Express.Request) {
+	}
+
+	async insertAll(preDBInstances: PreDB<Type>[]) {
+		// await this.batchOperation(preDBInstances, 'set');
+		await this.bulkOperation(preDBInstances, 'set');
+	}
+
+	async bulkOperation(preDBInstances: PreDB<Type>[], method: BulkOpMethod) {
+		return await batchAction(preDBInstances.map(this.prepareObjForInsert), 200, async (instances) => {
+			const bulk = this.wrapper.firestore.bulkWriter();
+
+			switch (method) {
+				case 'set':
+					return await instances.reduce((_bulk, instance) => {
+						_bulk.set(this.getDocumentRef(instance).ref, instance);
+						return _bulk;
+					}, bulk).flush();
+				default:
+					break;
+			}
+		});
+	}
+
+
+	async batchOperation(preDBInstances: PreDB<Type>[], method: BatchOpMethod) {
+		return await batchAction(preDBInstances.map(this.prepareObjForInsert), 200, async (instances) => {
+			const batch = this.wrapper.firestore.batch();
+
+			switch (method) {
+				case 'set':
+					return await instances.reduce((_batch, instance) => {
+						return _batch.set(this.getDocumentRef(instance).ref, instance);
+					}, batch).commit();
+				default:
+					break;
+			}
+		});
+
+	}
+
+	async insert(preDBInstance: PreDB<Type>) {
+		const dbInstance = this.prepareObjForInsert(preDBInstance);
+		await this.assertInstance(dbInstance);
+		const doc = this.getDocumentRef(dbInstance);
+		return doc.set(dbInstance);
+	}
+
+	protected generateId() {
+		return generateHex(dbIdLength);
+	}
+
+
+	prepareObjForInsert(preDBObject: PreDB<Type>): Type {
+		const now = currentTimeMillis();
+		preDBObject._id = this.generateId();
+		preDBObject.__updated = preDBObject.__created = now;
+
+		return preDBObject as Type;
 	}
 }
 
