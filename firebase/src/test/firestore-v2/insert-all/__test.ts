4ae35c0adfde8f5474e917d7eff5eb8a5ec4a748
Matan Koby <matan.k@quai.md>
2023-06-19 11:07:16 +0300

    more test files

---
+++ firebase/src/test/firestore-v2/insert-all/__test.ts
@@ -0,0 +1,9 @@
+// import {testSuiteTester} from '@nu-art/ts-common/testing/consts';
+// import {TestSuit_FirestoreV2_InsertAll} from './insert-all';
+// import {prepare__Test} from '../_core/consts';
+//
+// prepare__Test();
+//
+// describe('Firestore v2 - InsertAll', () => {
+// 	testSuiteTester(TestSuit_FirestoreV2_InsertAll);
+// });
--- firebase/src/test/firestore-v2/insert/__test.ts
+++ firebase/src/test/firestore-v2/insert/__test.ts
@@ -1,17 +1,8 @@
 import {testSuiteTester} from '@nu-art/ts-common/testing/consts';
-import {ModuleBE_Auth} from '@nu-art/google-services/backend';
-import {FIREBASE_DEFAULT_PROJECT_ID, ModuleBE_Firebase} from '../../../main/backend';
 import {TestSuit_FirestoreV2_Insert} from './insert';
-import {generateHex} from "@nu-art/ts-common";
+import {prepare__Test} from '../_core/consts';
 
-
-const config = {
-	project_id: generateHex(4),
-	databaseURL: 'http://localhost:8102/?ns=quai-md-dev',
-};
-
-ModuleBE_Auth.setDefaultConfig({auth: {[FIREBASE_DEFAULT_PROJECT_ID]: config}});
-export const firestore = ModuleBE_Firebase.createAdminSession().getFirestoreV2();
+prepare__Test();
 
 describe('Firestore v2 - Insert', () => {
 	testSuiteTester(TestSuit_FirestoreV2_Insert);
