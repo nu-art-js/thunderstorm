85e1e1e484fd8172a1c9759608b416d65fcdef64
Yuval Talby <yuval@quai.md>
2023-06-20 17:10:46 +0300

    WIP update tests

--- firebase/src/test/firestore-v2/__test.ts
+++ firebase/src/test/firestore-v2/__test.ts
@@ -2,11 +2,11 @@ import {testSuiteTester} from '@nu-art/ts-common/testing/consts';
 import {TestSuit_FirestoreV2_Insert} from './insert/insert';
 import {TestSuite_FirestoreV2_Delete} from './delete/delete';
 import {TestSuit_FirestoreV2_InsertAll} from './insert/insert-all';
-import {TestSuite_FirestoreV2_Upsert} from "./upsert/upsert";
+import {TestSuite_FirestoreV2_Update} from "./update/update";
 
 describe('Firestore v2 - All Tests', () => {
 	testSuiteTester(TestSuit_FirestoreV2_Insert);
 	testSuiteTester(TestSuit_FirestoreV2_InsertAll);
 	testSuiteTester(TestSuite_FirestoreV2_Delete);
-	testSuiteTester(TestSuite_FirestoreV2_Upsert);
+	testSuiteTester(TestSuite_FirestoreV2_Update);
 });
