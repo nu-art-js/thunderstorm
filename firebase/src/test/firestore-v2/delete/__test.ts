5c5bbb7805d8609a3938573d4af47ab91cdf3bec
Yuval Talby <yuval@quai.md>
2023-06-20 11:10:27 +0300

    changed insert to match conventions

--- firebase/src/test/firestore-v2/delete/__test.ts
+++ firebase/src/test/firestore-v2/delete/__test.ts
@@ -1,5 +1,5 @@
 import {testSuiteTester} from '@nu-art/ts-common/testing/consts';
-import {TestSuite_FirestoreV2_Delete} from './delete';
+import {TestSuite_FirestoreV2_Delete} from "./delete";
 
 describe('Firestore v2 - Insert', () => {
 	testSuiteTester(TestSuite_FirestoreV2_Delete);
