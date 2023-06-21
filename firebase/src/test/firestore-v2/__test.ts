ccfaa11d4d80824660388124bbf18b2d281d80ab
Matan Koby <matan.k@quai.md>
2023-06-21 10:03:08 +0300

    query tests wip

--- firebase/src/test/firestore-v2/__test.ts
+++ firebase/src/test/firestore-v2/__test.ts
@@ -3,14 +3,15 @@ import {TestSuite_FirestoreV2_Insert} from './insert/insert';
 import {TestSuite_FirestoreV2_Delete} from './delete/delete';
 import {TestSuite_FirestoreV2_InsertAll} from './insert/insert-all';
 import {TestSuite_FirestoreV2_Update} from './update/update';
-import {TestSuite_FirestoreV2_Query} from './query/query';
-import {TestSuite_FirestoreV2_Upsert} from "./upsert/upsert";
+import {TestSuite_FirestoreV2_QueryInsert} from './query/query';
+import {TestSuite_FirestoreV2_Upsert} from './upsert/upsert';
 
 describe('Firestore v2 - All Tests', () => {
 	testSuiteTester(TestSuite_FirestoreV2_Insert);
 	testSuiteTester(TestSuite_FirestoreV2_InsertAll);
 	testSuiteTester(TestSuite_FirestoreV2_Delete);
-	testSuiteTester(TestSuite_FirestoreV2_Query);
+	testSuiteTester(TestSuite_FirestoreV2_QueryInsert);
+	// testSuiteTester(TestSuite_FirestoreV2_QueryUpsert);
 	testSuiteTester(TestSuite_FirestoreV2_Update);
 	testSuiteTester(TestSuite_FirestoreV2_Upsert);
 });
--- firebase/src/test/firestore-v2/query/query.ts
+++ firebase/src/test/firestore-v2/query/query.ts
@@ -1,19 +1,55 @@
-import {QueryTest, queryTestCases} from './consts';
+import {queryIdTestCases, QueryTest, queryTestCases} from './consts';
 import {firestore} from '../_core/consts';
 import {DB_Type} from '../_core/types';
-import {deepClone} from '@nu-art/ts-common';
+import {dbObjectToId, deepClone} from '@nu-art/ts-common';
 
-export const TestCases_FB_Query: QueryTest['testcases'] = [
+export const TestCases_FB_QueryInsert: QueryTest['testcases'] = [
 	...queryTestCases
 ];
 
-export const TestSuite_FirestoreV2_Query: QueryTest = {
+export const TestCases_FB_QueryUpsert: QueryTest['testcases'] = [
+	...queryIdTestCases
+];
+
+export const TestCases_FB_QueryComplex1: QueryTest['testcases'] = [
+	// queryComplexTestCases[0],
+];
+
+export const TestSuite_FirestoreV2_QueryInsert: QueryTest = {
 	label: 'Firestore query tests',
-	testcases: TestCases_FB_Query,
+	testcases: TestCases_FB_QueryInsert,
 	processor: async (testCase) => {
 		const collection = firestore.getCollection<DB_Type>('firestore-query-tests');
 		await collection.deleteCollection();
 		const toInsert = deepClone(testCase.input.value);
 		await collection.insert.all(toInsert);
+		await testCase.input.check!(collection, testCase.result);
+	}
+};
+
+export const TestSuite_FirestoreV2_QueryUpsert: QueryTest = {
+	label: 'Firestore query tests',
+	testcases: TestCases_FB_QueryUpsert,
+	processor: async (testCase) => {
+		const collection = firestore.getCollection<DB_Type>('firestore-query-tests');
+		await collection.deleteCollection();
+		const toInsert = deepClone(testCase.input.value);
+		const resultDBObjects = await collection.insert.all(toInsert);
+		await testCase.input.check!(collection, testCase.result, resultDBObjects.map(dbObjectToId));
+	}
+};
+
+export const TestSuite_FirestoreV2_QueryComplex1: QueryTest = {
+	label: 'Firestore query tests',
+	testcases: TestCases_FB_QueryComplex1,
+	processor: async (testCase) => {
+		const collection = firestore.getCollection<DB_Type>('firestore-query-tests');
+		await collection.deleteCollection();
+		//todo insert 2 different collections, obj that points to obj
+		//todo receive outer obj id - test is to find and retrieve the inner objects referenced by the outer obj.
+		//todo receive outer obj id and inner obj name - test is to find the inner specific obj referenced by the outer obj.
+		const toInsert = deepClone(testCase.input.value);
+		const resultDBObjects = await collection.insert.all(toInsert);
+		await testCase.input.check!(collection, testCase.result, resultDBObjects.map(dbObjectToId));
 	}
 };
