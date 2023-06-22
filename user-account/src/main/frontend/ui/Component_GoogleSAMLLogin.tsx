04bd4be967d80d49ac6b7b9f6e0c968d8eaa05d0
Cipher <itayleybovich@gmail.com>
2023-06-22 10:38:57 +0300

    saml login for google done

--- user-account/src/main/frontend/ui/Component_GoogleSAMLLogin.tsx
+++ user-account/src/main/frontend/ui/Component_GoogleSAMLLogin.tsx
@@ -5,7 +5,8 @@ import {ModuleFE_Account} from '../modules/ModuleFE_Account';
 export const Component_GoogleSAMLLogin = () => {
 
 	const onClick = async () => {
-		await ModuleFE_Account.v1.loginSaml({redirectUrl: window.location.href}).executeSync();
+		const url = ModuleFE_Account.composeSAMLUrl();
+		await ModuleFE_Account.v1.loginSaml({redirectUrl: url}).executeSync();
 	};
 
 	return <TS_BusyButton
