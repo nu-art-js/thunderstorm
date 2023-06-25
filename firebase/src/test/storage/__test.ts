7b12dc4d1ed3d89cf4e6bc2df34b13dcbe551e91
Harel  Dagan <hareldagan@Harels-MacBook-Pro.local>
2023-06-25 10:38:03 +0300

    storage delete tests

--- firebase/src/test/storage/__test.ts
+++ firebase/src/test/storage/__test.ts
@@ -2,9 +2,13 @@ import {testSuiteTester} from '@nu-art/ts-common/testing/consts';
 import {TestSuite_Storage_Write} from './write-to-files/write-to-files';
 import {TestSuite_GetAdminBucket} from './bucket-utils/getMainBucket';
 import {TestSuite_GetOrCreateBucket} from './bucket-utils/getOrCreateBucket';
+import {TestSuite_FileDelete} from './delete-file/file-delete';
+import {TestSuite_DeleteFiles} from './delete-file/deleteFiles';
 
 describe('Firebase Storage - All Tests', () => {
-	testSuiteTester(TestSuite_Storage_Write);
 	testSuiteTester(TestSuite_GetAdminBucket);
 	testSuiteTester(TestSuite_GetOrCreateBucket);
+	testSuiteTester(TestSuite_Storage_Write);
+	testSuiteTester(TestSuite_FileDelete);
+	testSuiteTester(TestSuite_DeleteFiles);
 });
