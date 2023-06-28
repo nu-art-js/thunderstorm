42ccd3000d365876e589b0cd5cbcd804fb95b0c5
Matan Koby <matan.k@quai.md>
2023-06-28 16:58:13 +0300

    More MemStorage usage

--- thunderstorm/src/main/backend/modules/server/consts.ts
+++ thunderstorm/src/main/backend/modules/server/consts.ts
@@ -11,5 +11,5 @@ export const MemKey_HttpRequestHeaders = new MemKey<IncomingHttpHeaders>('http-r
 export const MemKey_HttpRequestBody = new MemKey<any>('http-request--incoming-body', true);
 export const MemKey_HttpRequestQuery = new MemKey<QueryParams>('http-request--query', true);
 export const MemKey_HttpRequestUrl = new MemKey<string>('http-request--url', true);
-export const MemKey_HttpRequestOriginalUrl = new MemKey<string>('http-request--original-url', true);
+export const MemKey_HttpRequestPath = new MemKey<string>('http-request--original-url', true);
 export const MemKey_HttpRequestMethod = new MemKey<any>('http-request--method', true);
