ac060fe253a1d77eff8f4e06a2f683a26fa70ea0
CosmicSpectrum <hareldagan@Harels-MacBook-Pro.local>
2023-07-05 15:44:54 +0300

    zevel

--- db-api-generator/src/main/backend/ModuleBE_SyncEnvV2.ts
+++ db-api-generator/src/main/backend/ModuleBE_SyncEnvV2.ts
@@ -7,91 +7,91 @@ import {ModuleBE_Firebase} from '@nu-art/firebase/backend';
 
 
 type Config = {
-	urlMap: TypedMap<string>
-	fetchBackupDocsSecretsMap: TypedMap<string>
+    urlMap: TypedMap<string>
+    fetchBackupDocsSecretsMap: TypedMap<string>,
+    operationLimit: number
 }
 
 class ModuleBE_SyncEnvV2_Class
-	extends Module<Config> {
+    extends Module<Config> {
 
-	constructor() {
-		super();
-		addRoutes([createBodyServerApi(ApiDef_SyncEnvV2.vv1.fetchFromEnv, this.fetchFromEnv)]);
-	}
+    constructor() {
+        super();
+        this.setDefaultConfig({operationLimit: 1000});
+        addRoutes([createBodyServerApi(ApiDef_SyncEnvV2.vv1.fetchFromEnv, this.fetchFromEnv)]);
+    }
 
-	// private formatCsvToMapper = (csvContent: CsvRow[]) => {
-	//
-	// };
+    // private formatCsvToMapper = (csvContent: CsvRow[]) => {
+    //
+    // };
 
-	fetchFromEnv = async (body: Request_FetchFromEnv) => {
-		this.logInfoBold('Received API call Fetch From Env!');
-		this.logInfo(`Origin env: ${body.env}, bucketId: ${body.backupId}`);
-		if (!body.env)
-			throw new BadImplementationException(`Did not receive env in the fetch from env api call!`);
+    fetchFromEnv = async (body: Request_FetchFromEnv) => {
+        this.logInfoBold('Received API call Fetch From Env!');
+        this.logInfo(`Origin env: ${body.env}, bucketId: ${body.backupId}`);
+        if (!body.env)
+            throw new BadImplementationException(`Did not receive env in the fetch from env api call!`);
 
-		if (body.onlyModules && body.excludedModules)
-			this.logWarningBold('excludedModules config exists alongside onlyModules, so excludedModules are ignored.');
+        if (body.onlyModules && body.excludedModules)
+            this.logWarningBold('excludedModules config exists alongside onlyModules, so excludedModules are ignored.');
 
-		this.logInfo('\n\n\n\n\noyoyoyoyo');
-		this.logInfo(this.config.urlMap);
-		this.logInfo('\n\n\n\n\n');
-		const url: string = `${this.config.urlMap[body.env]}/v1/fetch-backup-docs-v2`;
-		const outputDef: ApiDef<QueryApi<any>> = {method: HttpMethod.GET, path: '', fullUrl: url};
+        this.logInfo('\n\n\n\n\noyoyoyoyo');
+        this.logInfo(this.config.urlMap);
+        this.logInfo('\n\n\n\n\n');
+        const url: string = `${this.config.urlMap[body.env]}/v1/fetch-backup-docs-v2`;
+        const outputDef: ApiDef<QueryApi<any>> = {method: HttpMethod.GET, path: '', fullUrl: url};
 
-		const requestBody: Request_BackupId = {backupId: body.backupId};
+        const requestBody: Request_BackupId = {backupId: body.backupId};
 
-		const response: Response_BackupDocsV2 = await AxiosHttpModule
-			.createRequest(outputDef)
-			.setUrlParams(requestBody)
-			.addHeader('x-secret', this.config.fetchBackupDocsSecretsMap[body.env])
-			.addHeader('x-proxy', 'fetch-env')
-			.executeSync();
+        const response: Response_BackupDocsV2 = await AxiosHttpModule
+            .createRequest(outputDef)
+            .setUrlParams(requestBody)
+            .addHeader('x-secret', this.config.fetchBackupDocsSecretsMap[body.env])
+            .addHeader('x-proxy', 'fetch-env')
+            .executeSync();
 
-		const backupInfo = response.backupInfo;
+        const backupInfo = response.backupInfo;
 
-		const wrongBackupIdDescriptor = backupInfo._id !== body.backupId;
-		if (wrongBackupIdDescriptor)
-			throw new BadImplementationException(`Received backup descriptors with wrong backupId! provided id: ${body.backupId} received id: ${backupInfo._id}`);
+        const wrongBackupIdDescriptor = backupInfo?._id !== body.backupId;
+        if (wrongBackupIdDescriptor)
+            throw new BadImplementationException(`Received backup descriptors with wrong backupId! provided id: ${body.backupId} received id: ${backupInfo?._id}`);
 
 
-		const firebaseSessionAdmin = ModuleBE_Firebase.createAdminSession();
-		const storage = firebaseSessionAdmin.getStorage();
-		const bucket = await storage.getMainBucket();
-		const backupDoc = await bucket.getFile(backupInfo.filePath);
+        const firebaseSessionAdmin = ModuleBE_Firebase.createAdminSession();
+        const storage = firebaseSessionAdmin.getStorage();
+        const bucket = await storage.getMainBucket();
+        const backupDoc = await bucket.getFile(backupInfo.filePath);
 
-		const firestore = firebaseSessionAdmin.getFirestoreV2().firestore;
-		const bulkWriter = firestore.bulkWriter();
-		const hasOnlyModulesArray = body.onlyModules && !!body.onlyModules.length;
+        const firestore = firebaseSessionAdmin.getFirestoreV2().firestore;
+        const bulkWriter = firestore.bulkWriter({throttling: true});
+        const hasOnlyModulesArray = body.onlyModules && !!body.onlyModules.length;
 
-		bulkWriter.onWriteError((err) => {
-			this.logErrorBold(err);
-			return true;
-		});
+        bulkWriter.onWriteError((err) => {
+            this.logErrorBold(err);
+            return false;
+        });
 
-		const dataCallback = async (row: any) => {
+        const dataCallback = (row: any) => {
 
-			_keys(row).map(key => {
-				row[(key as string).trim()] = row[key];
-			});
+            _keys(row).map(key => {
+                row[(key as string).trim()] = (row[key] as string).trim();
+            });
 
-			if (hasOnlyModulesArray && !body.onlyModules?.includes(row.collectionName)) {
-				this.logWarning(`Row ${row._id} from collection ${row.collectionName} is skipped from syncing due to not being in the list of specified modules to sync.`);
-				return;
-			}
+            if (hasOnlyModulesArray && !body.onlyModules?.includes(row.collectionName)) {
+                return;
+            }
 
-			if (!hasOnlyModulesArray && body.excludedModules?.includes(row.collectionName)) {
-				this.logWarning(`Row ${row._id} from collection: ${row.collectionName} is skipped from syncing due to being in the excluded modules list.`);
-				return;
-			}
+            if (!hasOnlyModulesArray && body.excludedModules?.includes(row.collectionName)) {
+                return;
+            }
 
-			const documentRef = firestore.doc(`${row.collectionName}/${row._id}`);
-			const data = JSON.parse(row.document);
-			await bulkWriter.set(documentRef, data);
-		};
+            const documentRef = firestore.doc(`${row.collectionName}/${row._id}`);
+            const data = JSON.parse(row.document);
+            bulkWriter.set(documentRef, data);
+        };
 
-		await CSVModule.forEachCsvRowFromStream(backupDoc.file.createReadStream(), dataCallback);
-		await bulkWriter.close();
-	};
+        await CSVModule.forEachCsvRowFromStreamSync(backupDoc.file.createReadStream(), dataCallback);
+        return await bulkWriter.close();
+    };
 }
 
 export const ModuleBE_SyncEnvV2 = new ModuleBE_SyncEnvV2_Class();
