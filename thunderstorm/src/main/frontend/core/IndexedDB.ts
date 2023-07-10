5cc745496ad90cae57df630388d4dd8f5c1c9dc3
TacB0sS <adam.vdk@nu-art-software.com>
2023-07-10 12:00:11 +0300

    indexkeys and unique params moved to ts common

--- thunderstorm/src/main/frontend/core/IndexedDB.ts
+++ thunderstorm/src/main/frontend/core/IndexedDB.ts
@@ -40,8 +40,6 @@ export type DBConfig<T extends DB_Object, Ks extends keyof T> = {
 	upgradeProcessor?: (db: IDBDatabase) => void
 };
 
-export type IndexKeys<T extends DB_Object, Ks extends keyof T> = { [K in Ks]: T[K] };
-
 export type IndexDb_Query = {
 	query?: string | number | string[] | number[],
 	indexKey?: string,
@@ -129,7 +127,7 @@ export class IndexedDB<T extends DB_Object, Ks extends keyof T> {
 	};
 
 	private cursorHandler = (cursorRequest: IDBRequest<IDBCursorWithValue | null>, perValueCallback: (value: T) => void,
-							 endCallback: () => void, limiterCallback?: () => boolean) => {
+													 endCallback: () => void, limiterCallback?: () => boolean) => {
 		cursorRequest.onsuccess = (event) => {
 			const cursor: IDBCursorWithValue = (event.target as IDBRequest).result;
 
