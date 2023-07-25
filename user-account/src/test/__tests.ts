c87e3af939129f6befb4dd9b9b6ae86dc9270e5e
Matan Koby <matan.k@quai.md>
2023-07-25 17:17:28 +0300

    wip login tests

---
+++ user-account/src/test/__tests.ts
@@ -0,0 +1,9 @@
+import {testSuiteTester} from '@nu-art/ts-common/testing/consts';
+import {TestSuite_Accounts_Register} from './register/__test';
+import {TestSuite_Accounts_Login} from './login/__test';
+
+describe('Accounts - All Tests', () => {
+	testSuiteTester(TestSuite_Accounts_Register);
+	testSuiteTester(TestSuite_Accounts_Login);
+
+});
