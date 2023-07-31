80d334cfe893eb152d67c277133a6f7525f3489e
Yuval Talby <yuval@quai.md>
2023-07-31 10:09:55 +0300

    added type to DB_Account and user creation

--- user-account/src/main/shared/v2/types.ts
+++ user-account/src/main/shared/v2/types.ts
@@ -1,5 +1,7 @@
 import {AuditableV2, DB_Object} from '@nu-art/ts-common';
 
+export const accountTypes = ['user', 'service'] as const;
+export type AccountType = typeof accountTypes[number];
 
 export type DB_Session_V2 = DB_Object & {
 	accountId: string
@@ -9,7 +11,11 @@ export type DB_Session_V2 = DB_Object & {
 
 export type DB_Account_V2 = DB_Object & AuditableV2 & {
 	email: string
+	type: AccountType;
 
 	salt?: string
 	saltedPassword?: string
+
+	displayName?: string
+	thumbnail?: string
 }
