fc3c5245fb2eb83b33ff4a2e0a0d2deddaa19b6a
Yuval Talby <yuval@quai.md>
2023-08-01 18:12:14 +0300

    created basic setup for permissions test

--- permissions/src/test/projects/__test-project.ts
+++ permissions/src/test/projects/__test-project.ts
@@ -1,26 +1,51 @@
 import {TestSuite} from '@nu-art/ts-common/testing/types';
 import '../_core/consts';
-import {PreDB} from '@nu-art/ts-common';
+import {PreDB, UniqueId} from '@nu-art/ts-common';
 import {testSuiteTester} from '@nu-art/ts-common/testing/consts';
-import {DB_PermissionProject} from '../../main';
+import {DB_PermissionAccessLevel, DB_PermissionApi, DB_PermissionDomain, DB_PermissionProject} from '../../main';
 import {ModuleBE_PermissionProject} from '../../main/backend/modules/management/ModuleBE_PermissionProject';
 import {ModuleBE_PermissionApi} from '../../main/backend/modules/management/ModuleBE_PermissionApi';
 import {ModuleBE_PermissionDomain} from '../../main/backend/modules/management/ModuleBE_PermissionDomain';
 import {ModuleBE_PermissionAccessLevel} from '../../main/backend/modules/management/ModuleBE_PermissionAccessLevel';
 import {ModuleBE_PermissionsAssert} from '../../main/backend';
-import {MemStorage} from '@nu-art/ts-common/mem-storage/MemStorage';
+import {MemKey, MemStorage} from '@nu-art/ts-common/mem-storage/MemStorage';
 
-
-export type ProjectInput = {
-	project: PreDB<DB_PermissionProject>
+type InputPermissionsSetup = {
+	setup: {
+		project: PreDB<DB_PermissionProject>;
+		domain: Omit<PreDB<DB_PermissionDomain>, 'projectId'>;
+		accessLevels: Omit<PreDB<DB_PermissionAccessLevel>, 'domainId'>[];
+		apis: (Omit<PreDB<DB_PermissionApi>, 'accessLevelIds' | 'projectId'> & { accessLevelNames: string[] })[];
+	},
+	userAccessLevelNames: string[];
+	check: (projectId: UniqueId) => Promise<any>;
 }
 
-type CreateAccountTest = TestSuite<ProjectInput, boolean>;
+type CreateAccountTest = TestSuite<InputPermissionsSetup, boolean>;
 
 const TestCases_FB_Create: CreateAccountTest['testcases'] = [
 	{
 		description: 'Create Project',
-		input: {project: {name: 'Test Project',}},
+		input: {
+			setup: {
+				project: {name: 'Test Project'},
+				domain: {namespace: 'test domain'},
+				accessLevels: [
+					{name: 'NoAccess', value: 0},
+					{name: 'Read', value: 100},
+					{name: 'Write', value: 200},
+					{name: 'Delete', value: 300},
+					{name: 'Admin', value: 1000},
+				],
+				apis: [
+					{path: 'v1/stam', accessLevelNames: ['Read', 'Delete']}
+				]
+			},
+			userAccessLevelNames: ['Write'],
+			check: async (projectId: UniqueId) => {
+				await ModuleBE_PermissionsAssert.assertUserPermissions(projectId, 'v1/stam', {});
+			}
+		},
 		result: true
 	},
 ];
@@ -29,24 +54,24 @@ export const TestSuite_Permissions_BasicSetup: CreateAccountTest = {
 	label: 'Basic Permissions Setup',
 	testcases: TestCases_FB_Create,
 	processor: async (testCase) => {
+		const setup = testCase.input.setup;
+		const MemKey_UserPermissions = new MemKey<DB_PermissionAccessLevel[]>('user-permissions');
 
 		await new MemStorage().init(async () => {
-			const dbProject = await ModuleBE_PermissionProject.create.item(testCase.input.project);
-			const dbDomain = await ModuleBE_PermissionDomain.create.item({projectId: dbProject._id, namespace: 'test domain'});
-			const dbAccessLevels = await ModuleBE_PermissionAccessLevel.create.all([
-				{domainId: dbDomain._id, name: 'NoAccess', value: 0},
-				{domainId: dbDomain._id, name: 'Read', value: 100},
-				{domainId: dbDomain._id, name: 'Write', value: 200},
-				{domainId: dbDomain._id, name: 'Delete', value: 300},
-				{domainId: dbDomain._id, name: 'Admin', value: 1000},
-			]);
+			const dbProject = await ModuleBE_PermissionProject.create.item(setup.project);
+			const dbDomain = await ModuleBE_PermissionDomain.create.item({...setup.domain, projectId: dbProject._id});
+			const dbAccessLevels = await ModuleBE_PermissionAccessLevel.create.all(setup.accessLevels.map(_level => ({..._level, domainId: dbDomain._id})));
+			const dbApi = await ModuleBE_PermissionApi.create.all(setup.apis.map(_api => {
+				return {
+					projectId: dbProject._id,
+					path: _api.path,
+					accessLevelIds: _api.accessLevelNames.map(_name => dbAccessLevels.find(_level => _level.name === _name)._id)
+				};
+			}));
 
-			const dbApi = await ModuleBE_PermissionApi.create.item({path: 'v1/stam', projectId: dbProject._id, accessLevelIds: [dbAccessLevels[2]._id]});
-
-			await ModuleBE_PermissionsAssert.assertUserPermissions();
+			MemKey_UserPermissions.set(testCase.input.userAccessLevelNames.map(_name => dbAccessLevels.find(_level => _level.name === _name)))
+			await testCase.input.check(dbProject._id);
 		});
-
-		// TODO: ASSERT PROJECT CREATED
 	}
 };
 
