0aaff98ef90cdc9a8faf73999834ee2c539e6c59
TacB0sS <adam.vdk@nu-art-software.com>
2023-08-07 18:41:22 +0300

    permissions test setups

--- permissions/src/test/_core/consts.ts
+++ permissions/src/test/_core/consts.ts
@@ -1,5 +1,6 @@
 import {DB_PermissionGroup} from '../../main';
 
+
 export const Default_TestEmail = 'test@test.test';
 export const Default_TestPassword = '1234';
 export const TestProject__Name = 'test-project';
@@ -21,3 +22,11 @@ export const Groups_ToCreate = [
 	Group_ToCreate_Delete,
 	Group_ToCreate_NoAccess
 ];
+
+export const Test_AccessLevel_NoAccess = 'NoAccess';
+export const Test_AccessLevel_Read = 'Read';
+export const Test_AccessLevel_Write = 'Write';
+export const Test_AccessLevel_Delete = 'Delete';
+export const Test_AccessLevel_Admin = 'Admin';
+
+export const Test_Domain1 = 'test-domain-1';
