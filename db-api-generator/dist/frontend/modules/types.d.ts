23191084c1819dd3895a336320eab6eac38c1581
TacB0sS <adam.vdk@nu-art-software.com>
2023-08-24 00:19:48 +0200

    removing again db api gen

--- db-api-generator/dist/frontend/modules/types.d.ts
+++
@@ -1,7 +0,0 @@
-import { DB_Object } from '@nu-art/ts-common';
-import { MultiApiEvent, SingleApiEvent } from '../types';
-import { ModuleFE_BaseDB } from './ModuleFE_BaseDB';
-export type ApiCallerEventType<DBType extends DB_Object> = [SingleApiEvent, DBType] | [MultiApiEvent, DBType[]];
-export interface OnSyncStatusChangedListener<DBType extends DB_Object> {
-    __onSyncStatusChanged: (module: ModuleFE_BaseDB<DBType, any>) => void;
-}
--- db-api-generator/dist/shared/index.d.ts
+++
@@ -1,6 +0,0 @@
-export * from './api';
-export * from './apiV2';
-export * from './apiV3';
-export * from './sync-env';
-export * from './archiving/apis';
-export * from './app-config';
