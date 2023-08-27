56c67cca7179002e4753cd8911c441e57d270599
Matan Koby <matan.k@quai.md>
2023-08-27 14:42:38 +0300

    fix

--- permissions/src/main/frontend/ui/index.ts
+++ permissions/src/main/frontend/ui/index.ts
@@ -3,4 +3,3 @@ export * from './permission-editors/permission-domains-editor';
 export * from './permission-editors/permission-projects-editor';
 export * from './permission-editors/permission-groups-editor';
 export * from './permission-editors/permission-users-editor';
-export * from './Component_UserPermissionsEditor/Component_UserPermissionsEditor';
