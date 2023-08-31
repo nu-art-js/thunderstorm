f7944e837ebf65266ab204133d898ff72a476946
Matan Koby <matan.k@quai.md>
2023-08-31 10:57:11 +0300

    wip permission key editor, needs css

--- permissions/src/main/frontend/ui/ATS_ComponentPermissionKeys/subEditors/permission-keys-editor.tsx
+++ permissions/src/main/frontend/ui/ATS_ComponentPermissionKeys/subEditors/permission-keys-editor.tsx
@@ -6,23 +6,27 @@ import {
 	SmartComponent,
 	State_SmartComponent
 } from '@nu-art/thunderstorm/frontend';
-import {PermissionKey_FE} from '../../PermissionKey_FE';
-import {ModuleFE_PermissionsAssert} from '../../modules/ModuleFE_PermissionsAssert';
-import {_keys, BadImplementationException, sortArray, TypedMap} from '@nu-art/ts-common';
+import {_keys, BadImplementationException, cloneArr, sortArray, TypedMap} from '@nu-art/ts-common';
 import * as React from 'react';
-import './permission-keys-editor.scss';
-import {ModuleFE_PermissionsAccessLevel} from '../../modules/manage/ModuleFE_PermissionsAccessLevel';
-import {DB_PermissionKeyData} from '../../../shared/types';
+import '../permission-keys-editor.scss';
+import {DB_PermissionAccessLevel} from '../../../../shared';
+import {Component_AccessLevelsEditor} from './Component_AccessLevelsEditor';
+import {DB_PermissionKeyData} from '../../../../shared/types';
+import {PermissionKey_FE} from '../../../PermissionKey_FE';
+import {ModuleFE_PermissionsAccessLevel} from '../../../modules/manage/ModuleFE_PermissionsAccessLevel';
+import {ModuleFE_PermissionsAssert} from '../../../modules/ModuleFE_PermissionsAssert';
 
 type Props = Props_SmartComponent & {};
 
+type EditedItemType = {
+	key: string,
+	data: Partial<DB_PermissionKeyData>
+};
 type State = State_SmartComponent & {
 	keys: TypedMap<PermissionKey_FE<any>>;
 	selectedItemKey?: string;
-	editedItem?: {
-		key: string,
-		data: Partial<DB_PermissionKeyData>
-	}
+	editedItem?: EditedItemType
+	selectedItemAccessLevels: DB_PermissionAccessLevel[];
 };
 
 export class PermissionKeysEditor
@@ -34,17 +38,25 @@ export class PermissionKeysEditor
 
 	protected async deriveStateFromProps(nextProps: Props, state: State): Promise<State> {
 		state.keys = ModuleFE_PermissionsAssert.getAllPermissionKeys();
+		state.selectedItemAccessLevels = cloneArr(ModuleFE_PermissionsAccessLevel.cache.filter(dbLevel => !!this.state.editedItem?.data?.accessLevelIds?.includes(dbLevel._id)));
+
 		return state;
 	}
 
 	private selectItem = (itemKey?: string) => {
+		let _editedItem: EditedItemType | undefined = undefined;
+
 		if (itemKey) {
 			const item = this.state.keys[itemKey];
 			if (!item)
 				throw new BadImplementationException(`Could not find item with key ${itemKey}`);
+
+			_editedItem = itemKey === this.state.selectedItemKey
+				? this.state.editedItem
+				: {key: itemKey, data: {type: 'permission-key', accessLevelIds: [], _accessLevels: {}}};
 		}
 
-		return this.reDeriveState({selectedItemKey: itemKey});
+		return this.reDeriveState({selectedItemKey: itemKey, editedItem: _editedItem});
 	};
 
 	private renderKey = (item: { key: string, value: PermissionKey_FE<any> }) => {
@@ -67,24 +79,26 @@ export class PermissionKeysEditor
 		</LL_V_L>;
 	};
 
-	private getDataFromKey(key: string) {
-		return ModuleFE_PermissionsAssert.getPermissionKey(key).get();
-	}
+	// private getDataFromKey(key: string) {
+	// 	return ModuleFE_PermissionsAssert.getPermissionKey(key).get();
+	// }
+
 
 	private renderAccessLevels() {
-		if(!this.state.selectedItemKey)
+		if (!this.state.selectedItemKey)
 			return null;
 
-		const data = this.getDataFromKey(this.state.selectedItemKey);
-		if(!data){
-			// todo
-			return null;
-		}
-		return null;
+		const sortedLevels = sortArray(this.state.selectedItemAccessLevels, level => level.name);
+
+		return <Component_AccessLevelsEditor levels={sortedLevels} onChange={() => {
+			const accessLevels = this.state.selectedItemAccessLevels;
+			this.logInfo(`array length: ${accessLevels.length}`);
+			this.setState({selectedItemAccessLevels: sortArray(accessLevels, level => level.name)});
+		}}/>;
 	}
 
 	private renderKeyEditor = () => {
-		return this.state.selectedItemKey !== undefined && <LL_V_L>
+		return this.state.selectedItemKey !== undefined && <LL_V_L className={'item-editor'}>
             <div className={'item-list__header'}>{this.state.selectedItemKey}</div>
 			{this.renderAccessLevels()}
         </LL_V_L>;
