a31a0f8702a74c3806ad06b061b650b23bef3cc1
Matan Koby <matan.k@quai.md>
2023-08-31 14:48:46 +0300

    Added PermissionKey_BE creation to ModuleBE_PermissionAssert, wip permission key access level editor

--- permissions/src/main/shared/types.ts
+++ permissions/src/main/shared/types.ts
@@ -18,7 +18,8 @@ export type UI_PermissionKeyData = {
 	_accessLevels?: { [domainId: UniqueId]: number }
 }
 
-export type PermissionKeyType = 'permission-key';
+export const Const_PermissionKeyType = 'permission-key';
+export type PermissionKeyType = typeof Const_PermissionKeyType;
 export type DB_PermissionKeyData = {
 	type: PermissionKeyType
 	accessLevelIds: UniqueId[];
