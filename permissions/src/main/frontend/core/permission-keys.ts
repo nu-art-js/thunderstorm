46e9b44538e1bb083738ce5bf91ba37e836819b0
TacB0sS <adam.vdk@nu-art-software.com>
2023-11-22 13:33:16 +0200

    permissions keys are a part of the domain

--- permissions/src/main/frontend/core/permission-keys.ts
+++ permissions/src/main/frontend/core/permission-keys.ts
@@ -1,4 +1,7 @@
 import {PermissionKey_FE} from '../PermissionKey_FE';
+import {PermissionKey_DeveloperAdmin, PermissionKey_DeveloperViewer, PermissionKey_DeveloperWriter} from '../../shared/permission-keys';
 
 
-export const PermissionKeyFE_DeveloperAdmin = new PermissionKey_FE('permission-key--developer-admin');
+export const PermissionKeyFE_DeveloperViewer = new PermissionKey_FE(PermissionKey_DeveloperViewer);
+export const PermissionKeyFE_DeveloperEditor = new PermissionKey_FE(PermissionKey_DeveloperWriter);
+export const PermissionKeyFE_DeveloperAdmin = new PermissionKey_FE(PermissionKey_DeveloperAdmin);
