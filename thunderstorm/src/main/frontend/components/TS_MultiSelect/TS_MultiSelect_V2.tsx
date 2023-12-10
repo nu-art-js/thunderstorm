4dcfc4e89152f689653a924268550f2b8010ac39
Cipher <itayleybovich@gmail.com>
2023-12-10 17:21:58 +0200

    TS_MultiSelect_V2 items direction

--- thunderstorm/src/main/frontend/components/TS_MultiSelect/TS_MultiSelect_V2.tsx
+++ thunderstorm/src/main/frontend/components/TS_MultiSelect/TS_MultiSelect_V2.tsx
@@ -3,7 +3,7 @@ import {Component, ReactNode} from 'react';
 import {removeItemFromArray, SubsetKeys} from '@nu-art/ts-common';
 import './TS_MultiSelect.scss';
 import {EditableItem} from '../../utils/EditableItem';
-import {LL_H_C} from '../Layouts';
+import {LL_H_C, LL_V_L} from '../Layouts';
 import {_className} from '../../utils/tools';
 
 
@@ -22,6 +22,7 @@ export type DynamicProps_TS_MultiSelect_V2<EnclosingItem, Prop extends keyof Enc
 
 export type StaticProps_TS_MultiSelect_V2<ItemType> = {
 	className?: string
+	itemsDirection?: 'vertical' | 'horizontal';
 	// mandatory
 	itemRenderer: (item?: ItemType, onDelete?: () => Promise<void>) => ReactNode
 	selectionRenderer: React.ComponentType<MultiSelect_Selector<ItemType>>,
@@ -59,8 +60,11 @@ export class TS_MultiSelect_V2<Binder extends Binder_MultiSelect<any, any, any>>
 
 		const props = this.props;
 		const SelectionRenderer = props.selectionRenderer;
+		const direction = this.props.itemsDirection ?? 'horizontal';
 
-		return <LL_H_C className={_className('ts-multi-select__list', this.props.className)}>
+		const Wrapper = direction === 'horizontal' ? LL_H_C : LL_V_L;
+
+		return <Wrapper className={_className('ts-multi-select__list', this.props.className)}>
 			{existingItems.map((item, i) => {
 				return <LL_H_C className="ts-multi-select__list-value" key={i}>
 					{props.itemRenderer(item, async () => {
@@ -75,7 +79,7 @@ export class TS_MultiSelect_V2<Binder extends Binder_MultiSelect<any, any, any>>
 				onSelected={addInnerItem}
 				existingItems={existingItems}
 			/>
-		</LL_H_C>;
+		</Wrapper>;
 	}
 }
 
