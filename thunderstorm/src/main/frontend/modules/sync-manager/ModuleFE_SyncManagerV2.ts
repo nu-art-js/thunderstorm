1737d4a7dbf3714ad2deb44d2b85b8096a48717b
TacB0sS <adam.vdk@nu-art-software.com>
2024-01-15 12:04:51 +0200

    smart sync ready for be

--- thunderstorm/src/main/frontend/modules/sync-manager/ModuleFE_SyncManagerV2.ts
+++ thunderstorm/src/main/frontend/modules/sync-manager/ModuleFE_SyncManagerV2.ts
@@ -19,11 +19,14 @@
  * limitations under the License.
  */
 
-import {Dispatcher, Module, Queue} from '@nu-art/ts-common';
+import {DB_Object, Dispatcher, Module, Queue} from '@nu-art/ts-common';
 import {apiWithBody, apiWithQuery} from '../../core/typed-api';
 import {
 	ApiStruct_SyncManager,
 	DBSyncData,
+	DeltaSyncModule,
+	FullSyncModule,
+	Response_DBSync,
 	Response_DBSyncData,
 	Response_SmartSync,
 	SmartSync_DeltaSync,
@@ -34,6 +37,7 @@ import {ApiDef_SyncManagerV2} from '../../../shared/sync-manager/apis';
 import {Thunder} from '../../core/Thunder';
 import {ModuleFE_BaseApi} from '../db-api-gen/ModuleFE_BaseApi';
 import {ThunderDispatcher} from '../../core/thunder-dispatcher';
+import {DataStatus, EventType_Query} from '../../core/db-api-gen/consts';
 
 
 export type SyncIfNeeded = {
@@ -57,12 +61,14 @@ export class ModuleFE_SyncManagerV2_Class
 
 	readonly v1;
 	private syncQueue;
-	private permissibleModuleNames: string[] | undefined = [];
+
+	// All the modules that a user has permissions to view and with the last updated timestamp of each collection
+	private syncedModules: { dbName: string, lastUpdated: number }[] = [];
 
 	constructor() {
 		super();
 
-		this.syncQueue = new Queue('Sync Queue');
+		this.syncQueue = new Queue('Sync Queue').setParallelCount(4);
 		this.v1 = {
 			checkSync: apiWithQuery(ApiDef_SyncManagerV2.v1.checkSync, this.onReceivedSyncData),
 			smartSync: apiWithBody(ApiDef_SyncManagerV2.v1.smartSync, this.onSmartSyncCompleted)
@@ -70,38 +76,94 @@ export class ModuleFE_SyncManagerV2_Class
 	}
 
 	public onReceivedSyncData = async (response: Response_DBSyncData) => {
-		this.permissibleModuleNames = response.syncData.map(item => item.name);
+		this.syncedModules = response.syncData.map(item => ({dbName: item.name, lastUpdated: item.lastUpdated}));
+
 		dispatch_OnPermissibleModulesUpdated.dispatchUI();
 		await dispatch_syncIfNeeded.dispatchModuleAsync(response.syncData);
 		dispatch_onSyncCompleted.dispatchModule();
 	};
 
-	public getPermissibleModuleNames = () => this.permissibleModuleNames ? [...this.permissibleModuleNames] : undefined;
+	public getPermissibleModuleNames = () => this.syncedModules ? this.syncedModules.map(moduleSyncData => moduleSyncData.dbName) : undefined;
 
 	public onSmartSyncCompleted = async (response: Response_SmartSync) => {
-		const modulesToSync = response.modules.filter(module => module.sync === SmartSync_FullSync);
-		modulesToSync.forEach(moduleToSync => {
-			const module = Thunder.getInstance().modules.find(module => (module as unknown as ApiModule['dbModule']).dbDef?.dbName === moduleToSync.name);
+		const modulesToFullySync = response.modules.filter(module => module.sync === SmartSync_FullSync) as FullSyncModule[];
+		modulesToFullySync.forEach(moduleToSync => {
+			const module = Thunder.getInstance().modules
+				.find(module => (module as unknown as ApiModule['dbModule']).dbDef?.dbName === moduleToSync.dbName) as ModuleFE_BaseApi<any>;
+
 			if (!module)
-				return this.logError(`Couldn't find module with dbName: '${moduleToSync.name}'`);
+				return this.logError(`Couldn't find module with dbName: '${moduleToSync.dbName}'`);
 
 			this.syncQueue.addItem(async () => {
-				this.logInfo(`Full sync for: '${moduleToSync.name}'`);
-				await (module as ModuleFE_BaseApi<any>).v1.sync().executeSync();
+				try {
+					await this.performFullSync(module);
+				} catch (e: any) {
+					this.logError(`Error while syncing ${module.dbDef.dbName}`, e);
+					throw e;
+				}
 			});
 		});
 
-		const modulesToUpdate = response.modules.filter(module => module.sync === SmartSync_DeltaSync);
+		const modulesToUpdate = response.modules.filter(module => module.sync === SmartSync_DeltaSync) as DeltaSyncModule[];
 		for (const moduleToUpdate of modulesToUpdate) {
 			const module = Thunder.getInstance().modules
-				.find(module => (module as unknown as ApiModule['dbModule']).dbDef?.dbName === moduleToUpdate.name) as ModuleFE_BaseApi<any>;
+				.find(module => (module as unknown as ApiModule['dbModule']).dbDef?.dbName === moduleToUpdate.dbName) as ModuleFE_BaseApi<any>;
 
-			await module.onEntriesUpdated(moduleToUpdate.items?.toUpdate ?? []);
-			await module.onEntriesDeleted(moduleToUpdate.items?.toDelete ?? []);
+			await this.performDeltaSync(module, moduleToUpdate.items);
 		}
 
+		this.syncedModules = response.modules.map(item => ({dbName: item.dbName, lastUpdated: item.lastUpdated}));
+		dispatch_OnPermissibleModulesUpdated.dispatchUI();
 		dispatch_onSyncCompleted.dispatchModule();
 	};
+
+	performFullSync = async (module: ModuleFE_BaseApi<any>) => {
+		module.logInfo(`Full sync for: '${module.dbDef.dbName}'`);
+		module.logVerbose(`Firing event (DataStatus.NoData): ${module.dbDef.dbName}`);
+		module.setDataStatus(DataStatus.NoData);
+
+		// if the backend have decided module collection needs a full sync, we need to clean local idb and cache
+		module.logVerbose(`Cleaning IDB: ${module.dbDef.dbName}`);
+		await module.IDB.clear();
+		module.logVerbose(`Cleaning Cache: ${module.dbDef.dbName}`);
+		module.cache.clear();
+
+		module.logVerbose(`Firing event (DataStatus.UpdatingData): ${module.dbDef.dbName}`);
+		module.setDataStatus(DataStatus.UpdatingData);
+
+		// for full sync go fetch all db items
+		module.logVerbose(`Syncing: ${module.dbDef.dbName}`);
+		const allItems = await module.v1.query({where: {}}).executeSync();
+
+		module.logVerbose(`Updating IDB: ${module.dbDef.dbName}`);
+		await module.IDB.syncIndexDb(allItems);
+		module.logVerbose(`Updating Cache: ${module.dbDef.dbName}`);
+		await module.cache.load();
+
+		module.logVerbose(`Firing event (DataStatus.ContainsData): ${module.dbDef.dbName}`);
+		module.setDataStatus(DataStatus.ContainsData);
+
+		module.logVerbose(`Firing event (EventType_Query): ${module.dbDef.dbName}`);
+		module.dispatchMulti(EventType_Query, allItems);
+
+		module.logDebug(`Full Sync Completed: ${module.dbDef.dbName}`);
+	};
+
+	performDeltaSync = async <T extends DB_Object>(module: ModuleFE_BaseApi<T>, syncData: Response_DBSync<T>) => {
+		module.logInfo(`Delta sync for: '${module.dbDef.dbName}'`);
+
+		module.logVerbose(`Firing event (DataStatus.UpdatingData): ${module.dbDef.dbName}`);
+		module.setDataStatus(DataStatus.UpdatingData);
+
+		await module.onEntriesUpdated(syncData.toUpdate ?? []);
+		await module.onEntriesDeleted((syncData.toDelete ?? []) as T[]);
+
+		module.logVerbose(`Firing event (DataStatus.ContainsData): ${module.dbDef.dbName}`);
+		module.setDataStatus(DataStatus.ContainsData);
+
+		module.logDebug(`Delta Sync Completed: ${module.dbDef.dbName}`);
+	};
+
 }
 
 export const ModuleFE_SyncManagerV2 = new ModuleFE_SyncManagerV2_Class();
