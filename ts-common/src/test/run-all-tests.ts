cb5ef81c489c12c37954b5c67b5a92c0806396e3
TacB0sS <adam.vdk@nu-art-software.com>
2024-01-25 09:01:09 +0200

    queue v2

--- ts-common/src/test/run-all-tests.ts
+++ ts-common/src/test/run-all-tests.ts
@@ -1,9 +1,10 @@
 import {runAllArrayToolsTests} from './array-tools/tests';
 import {runAllMergeToolsTests} from './merge-tools/tests';
 import {runAllObjectToolsTests} from './object-tools/tests';
-import {runAllUiToolsTests} from "./ui-tools/tests";
+import {runAllUiToolsTests} from './ui-tools/tests';
+import './queue/test';
+// runAllArrayToolsTests();
+// runAllObjectToolsTests();
+// runAllMergeToolsTests();
+// runAllUiToolsTests()
 
-runAllArrayToolsTests();
-runAllObjectToolsTests();
-runAllMergeToolsTests();
-runAllUiToolsTests()
