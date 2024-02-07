bed9c00e9d90aa092fa7ccb9d1b80a8e98bfda90
CosmicSpectrum <hareldagan@Harels-MacBook-Pro.local>
2024-02-07 16:41:47 +0200

    fix ts editable status component

--- thunderstorm/src/main/frontend/components/TS_EditableItemStatus/TS_EditableItemStatus.tsx
+++ thunderstorm/src/main/frontend/components/TS_EditableItemStatus/TS_EditableItemStatus.tsx
@@ -19,7 +19,7 @@ export class TS_EditableItemStatus
 	private getStatusLabel(state: State & EditableRef<any>) {
 		const editable = state.editable;
 
-		if (editable.hasErrors() && !editable.hasChanges())
+		if (editable.hasErrors() && !editable.hasChanges() && editable.get('_id'))
 			return state.status = 'Saved With Errors';
 
 		if (editable.hasErrors())
