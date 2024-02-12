551d5b15421e4c1f60cd581f8c57786ee5181b4c
Matan Koby <matan.k@quai.md>
2024-02-12 09:03:53 +0200

    Added ts-messaging, for message/topic collections

---
+++ ts-messaging/src/main/_entity/message/shared/types.ts
@@ -0,0 +1,36 @@
+import {AuditableV2, DB_Object, DBProto, Proto_DB_Object, UniqueId, VersionsDeclaration} from '@nu-art/ts-common';
+
+type VersionTypes_Message = {
+	'1.0.0': DB_Message
+}
+type Versions = VersionsDeclaration<['1.0.0'], VersionTypes_Message>;
+type Dependencies = {
+//
+}
+
+type UniqueKeys = '_id';
+type GeneratedProps = '_auditorId';
+type Proto = Proto_DB_Object<DB_Message, GeneratedProps, Versions, UniqueKeys, Dependencies>;
+export const MessageType_Text = 'text' as const;
+export const MessageType_Image = 'image' as const;
+export const MessageType_Video = 'video' as const;
+
+export type DBProto_Message = DBProto<Proto>;
+
+export type UI_Message = DBProto_Message['uiType'];
+
+export type MessageTypes_DataMap = {
+	text: { text: string }
+	image: { url: string }
+	video: {}
+}
+export type MessageType = keyof MessageTypes_DataMap
+export type BaseMessage = {
+	topicId: UniqueId
+}
+export type DB_Message_Text = BaseMessage & { type: typeof MessageType_Text, text: string };
+export type DB_Message_Image = BaseMessage & { type: typeof MessageType_Image, url: string };
+export type DB_Message_Video = BaseMessage & { type: typeof MessageType_Video };
+export type MessageContent = DB_Message_Text | DB_Message_Image | DB_Message_Video;
+
+export type DB_Message = DB_Object & AuditableV2 & MessageContent
---
+++ ts-messaging/src/main/_entity/topic/shared/types.ts
@@ -0,0 +1,21 @@
+import {DB_Object, DBProto, Proto_DB_Object, VersionsDeclaration} from '@nu-art/ts-common';
+
+type VersionTypes_Topic = {
+	'1.0.0': DB_Topic
+}
+type Versions = VersionsDeclaration<['1.0.0'], VersionTypes_Topic>;
+type Dependencies = {
+//
+}
+
+type UniqueKeys = '_id';
+type GeneratedProps = 'type' | 'refId'
+type Proto = Proto_DB_Object<DB_Topic, GeneratedProps, Versions, UniqueKeys, Dependencies>;
+
+export type DBProto_Topic = DBProto<Proto>;
+
+export type UI_Topic = DBProto_Topic['uiType'];
+export type DB_Topic = DB_Object & {
+	type: string; // collection name- e.g 'variables' / 'values'
+	refId: string; // a collection object's _id
+}
