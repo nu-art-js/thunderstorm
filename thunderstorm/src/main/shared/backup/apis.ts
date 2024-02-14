b7475e859bcbdf072e6f566590a7a08e40138f79
CosmicSpectrum <hareldagan@Harels-MacBook-Pro.local>
2024-02-14 14:55:10 +0200

    backup api to return object

--- thunderstorm/src/main/shared/backup/apis.ts
+++ thunderstorm/src/main/shared/backup/apis.ts
@@ -23,7 +23,7 @@ export type ApiStruct_Backup = {
 
 export type ApiStruct_BackupV2 = {
 	vv1: {
-		initiateBackup: QueryApi<string | undefined>,
+		initiateBackup: QueryApi<{ pathToBackup: string } | undefined>,
 		fetchBackupDocs: QueryApi<Response_BackupDocsV2, Request_BackupId>,
 	}
 }
