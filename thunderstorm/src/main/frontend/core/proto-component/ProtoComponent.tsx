2904f83fb196b58c8de8c341ea6802c5e929fc34
Cipher <itayleybovich@gmail.com>
2024-02-18 11:16:41 +0200

    more fixes, ATS_ProtoComponentDouble

--- thunderstorm/src/main/frontend/core/proto-component/ProtoComponent.tsx
+++ thunderstorm/src/main/frontend/core/proto-component/ProtoComponent.tsx
@@ -2,7 +2,7 @@ import {ComponentSync} from '../ComponentSync';
 import {
 	_keys,
 	BadImplementationException,
-	compare,
+	compare, RecursiveObjectOfPrimitives,
 	ThisShouldNotHappenException
 } from '@nu-art/ts-common';
 import {ModuleFE_BrowserHistoryV2, OnUrlParamsChangedListenerV2, QueryParamKey} from '../../modules/ModuleFE_BrowserHistoryV2';
@@ -15,6 +15,11 @@ export abstract class ProtoComponent<T extends ProtoComponentDef<any, any>, P ex
 
 	// ######################## Life Cycle ########################
 
+	/**
+	 * Listener for URL changes triggered by ModuleFE_BrowserHistoryV2.
+	 * Will trigger deriveStateFromProps with the new state of the query params set to be the previous state,
+	 * if there were changes in the hash that are relevant to this component.
+	 */
 	__onUrlParamsChangedV2 = () => {
 		const queryParams = this.getQueryParamResultsObject();
 		if (!compare(queryParams, this.state?.previousResultsObject)) {
@@ -24,6 +29,7 @@ export abstract class ProtoComponent<T extends ProtoComponentDef<any, any>, P ex
 
 	protected _deriveStateFromProps(nextProps: Props, state?: Partial<State>): State {
 		this.logVerbose('Deriving state from props');
+		//Initial state set to be the query param object generated from the query params keys given in props.
 		state ??= this.state ? {...this.state} : {queryParams: this.getQueryParamObject()} as State;
 		const _state = this.deriveStateFromProps(nextProps, state);
 		this.mounted && _state && this.setState(_state);
@@ -32,6 +38,10 @@ export abstract class ProtoComponent<T extends ProtoComponentDef<any, any>, P ex
 
 	// ######################## Class Methods ########################
 
+	/**
+	 * Generates an object connecting each query param key given in the props to a QueryParamKey class instance.
+	 * @private
+	 */
 	private getQueryParamObject(): ProtoComponent_QueryParamMapImpl<T> {
 		const queryParams = {} as ProtoComponent_QueryParamMapImpl<T>;
 		this.props.queryParamsKeys?.forEach(key => {
@@ -40,6 +50,10 @@ export abstract class ProtoComponent<T extends ProtoComponentDef<any, any>, P ex
 		return queryParams;
 	}
 
+	/**
+	 * Generates an object connection each query param key given in the props to its current value as read from the URL
+	 * @private
+	 */
 	private getQueryParamResultsObject() {
 		const queryParamObject = this.getQueryParamObject();
 		return _keys(queryParamObject).reduce((map, key) => {
@@ -48,6 +62,14 @@ export abstract class ProtoComponent<T extends ProtoComponentDef<any, any>, P ex
 		}, {} as ProtoComponent_QueryParamResultsMap<T>);
 	}
 
+	/**
+	 * Returns a QueryParamKey class instance by a given key.</br>
+	 * Will throw an error if the given key was not given in props.queryParamsKeys.</br>
+	 * If state was not yet instantiated, will create a new QueryParamKey class instance and return it.
+	 * If state was instantiated, will return the QueryParamKey class instance from the this.state.queryParams.
+	 * @param key
+	 * @private
+	 */
 	private getQueryParamKeyForKey(key: T['queryParamKeys']) {
 		if (!this.props.queryParamsKeys?.includes(key))
 			throw new BadImplementationException(`key ${key} is not defined in Props.queryParamsKeys, did you forget to add it?`);
@@ -66,16 +88,29 @@ export abstract class ProtoComponent<T extends ProtoComponentDef<any, any>, P ex
 
 	// ######################## Singulars
 
+	/**
+	 * Will set a value in the URL hash for a given key.
+	 * @param key
+	 * @param value
+	 */
 	setQueryParam<K extends T['queryParamKeys'] = T['queryParamKeys']>(key: K, value: T['queryParamDef'][K]) {
 		const queryKey = this.getQueryParamKeyForKey(key);
 		queryKey.set(value);
 	}
 
+	/**
+	 * Returns the value of a query param by given key
+	 * @param key
+	 */
 	getQueryParam(key: T['queryParamKeys']) {
 		const queryKey = this.getQueryParamKeyForKey(key);
 		return queryKey.get();
 	}
 
+	/**
+	 * Clears the value of a query param by a given key
+	 * @param key
+	 */
 	deleteQueryParam(key: T['queryParamKeys']) {
 		const queryKey = this.getQueryParamKeyForKey(key);
 		queryKey.delete();
@@ -83,8 +118,13 @@ export abstract class ProtoComponent<T extends ProtoComponentDef<any, any>, P ex
 
 	// ######################## Multiples
 
+	/**
+	 * Will merge the given query object into the URL.
+	 * @param query
+	 */
 	setQueryParams(query: Partial<T['queryParamDef']>) {
-		ModuleFE_BrowserHistoryV2.replace(query);
-		//No need to setState or re-derive since the listener should catch the change and trigger a re-derive.
+		const currentState = ModuleFE_BrowserHistoryV2.getState();
+		const merged = {...currentState, ...query};
+		ModuleFE_BrowserHistoryV2.setState(merged as RecursiveObjectOfPrimitives);
 	}
 }
