d2dde4b0d2d3ce2d877d0e9c1af6c67407a6b01b
Cipher <itayleybovich@gmail.com>
2024-02-19 18:04:33 +0200

    permissions proto dependancy

--- permissions/src/main/frontend/ui/permission-editors/permission-groups-editor.tsx
+++ permissions/src/main/frontend/ui/permission-editors/permission-groups-editor.tsx
@@ -1,5 +1,13 @@
 import * as React from 'react';
-import {EditableDBItemV3, EventType_Create, EventType_Delete, EventType_Update, TS_ErrorBoundary, TS_PropRenderer} from '@nu-art/thunderstorm/frontend';
+import {
+	EditableDBItemV3,
+	EventType_Create,
+	EventType_Delete,
+	EventType_Update,
+	ModuleFE_Toaster,
+	TS_ErrorBoundary,
+	TS_PropRenderer
+} from '@nu-art/thunderstorm/frontend';
 import {MUSTNeverHappenException, UniqueId} from '@nu-art/ts-common';
 import {MultiSelect} from '../ui-props';
 import {TS_Icons} from '@nu-art/ts-styles';
@@ -12,7 +20,7 @@ import {
 	ModuleFE_PermissionGroup,
 	ModuleFE_PermissionProject
 } from '../../_entity';
-import {EditorBase, State_EditorBase} from './editor-base';
+import {EditorBase, Permissions_MenuAction, State_EditorBase} from './editor-base';
 import {ApiCallerEventTypeV3, DispatcherInterface} from '@nu-art/thunderstorm/frontend/core/db-api-gen/v3_types';
 import {Input_Text_Blur} from './components';
 import {DropDown_PermissionProject} from '../../../_entity/permission-project/frontend/ui-components';
@@ -47,6 +55,25 @@ export class PermissionGroupsEditor
 		if (params[0] === EventType_Delete)
 			this.reDeriveState({selectedItemId: undefined, editedItem: undefined});
 	}
+
+	//######################### Logic #########################
+
+	protected getItemMenuActions = (item: DB_PermissionGroup): Permissions_MenuAction[] => {
+		return [
+			{
+				label: 'Delete Group',
+				action: async () => {
+					try {
+						await ModuleFE_PermissionGroup.v1.delete(item).executeSync();
+						return true;
+					} catch (err: any) {
+						this.logError({...err});
+						ModuleFE_Toaster.toastError(err.errorResponse.debugMessage.split('\n')[0]);
+					}
+				}
+			}
+		];
+	};
 
 	//######################### Render #########################
 
