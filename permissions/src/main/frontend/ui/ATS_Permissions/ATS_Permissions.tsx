211882993fee44699fdf903fc19ab590e5b2eaa7
CosmicSpectrum <hareldagan@Harels-MacBook-Pro.local>
2024-04-18 18:18:17 +0300

    wip permission editor - stable

--- permissions/src/main/frontend/ui/ATS_Permissions/ATS_Permissions.tsx
+++ permissions/src/main/frontend/ui/ATS_Permissions/ATS_Permissions.tsx
@@ -1,31 +1,31 @@
 import * as React from 'react';
 import {
+	_className,
 	AppToolsScreen,
 	ComponentSync,
 	genericNotificationAction,
 	LL_H_C,
-	LL_V_L,
-	Tab,
+	LL_V_L, ModuleFE_RoutingV2,
 	TS_AppTools,
 	TS_Button,
-	TS_Tabs
+	TS_NavLink
 } from '@nu-art/thunderstorm/frontend';
 import './ATS_Permissions.scss';
 import {ModuleFE_PermissionsAssert} from '../../modules/ModuleFE_PermissionsAssert';
 import {ModuleFE_Account} from '@nu-art/user-account/frontend';
-import {timeout} from '@nu-art/ts-common';
+import {arrayIncludesAny, timeout} from '@nu-art/ts-common';
 import {SessionKey_StrictMode_FE} from '../../consts';
-import {PermissionProjectsEditor} from '../permission-editors/permission-projects-editor';
-import {PermissionDomainsEditor} from '../permission-editors/permission-domains-editor';
-import {PermissionGroupsEditor} from '../permission-editors/permission-groups-editor';
-import {PermissionUsersEditor} from '../permission-editors/permission-users-editor';
 import {ModuleFE_PermissionAccessLevel} from '../../../_entity/permission-access-level/frontend';
 import {ModuleFE_PermissionAPI} from '../../../_entity/permission-api/frontend';
 import {ModuleFE_PermissionProject} from '../../../_entity/permission-project/frontend';
 import {ModuleFE_PermissionDomain} from '../../../_entity/permission-domain/frontend';
 import {ModuleFE_PermissionGroup} from '../../../_entity/permission-group/frontend';
 import {ModuleFE_PermissionUser} from '../../../_entity/permission-user/frontend';
-
+import {Outlet} from 'react-router-dom';
+import {PermissionUsersEditor} from '../permission-editors/permission-users-editor';
+import {PermissionProjectsEditor} from '../permission-editors/permission-projects-editor';
+import {PermissionDomainsEditor} from '../permission-editors/permission-domains-editor';
+import {PermissionGroupsEditor} from '../permission-editors/permission-groups-editor';
 
 export class ATS_Permissions
 	extends ComponentSync {
@@ -43,7 +43,23 @@ export class ATS_Permissions
 			ModuleFE_PermissionGroup,
 			ModuleFE_PermissionUser,
 		],
+		children: [
+			PermissionProjectsEditor.Route,
+			PermissionDomainsEditor.Route,
+			PermissionGroupsEditor.Route,
+			PermissionUsersEditor.Route,
+		]
 	};
+
+	componentDidMount() {
+		const routeKeys = [PermissionProjectsEditor.Route.key,
+			PermissionDomainsEditor.Route.key,
+			PermissionGroupsEditor.Route.key,
+			PermissionUsersEditor.Route.key,
+		];
+		if (!arrayIncludesAny(window.location.pathname.split('/'), routeKeys))
+			ModuleFE_RoutingV2.goToRoute(PermissionProjectsEditor.Route);
+	}
 
 	//######################### Logic #########################
 
@@ -71,25 +87,43 @@ export class ATS_Permissions
 						Mode</TS_Button>
 				</LL_H_C>
 			</LL_H_C>
-			{this.renderTabs()}
+			<LL_H_C className={'links-container'}>
+				<TS_NavLink
+					key={PermissionProjectsEditor.Route.key}
+					route={PermissionProjectsEditor.Route}
+					ignoreClickOnSameRoute={true}
+					className={({isActive}) => _className('link-button', isActive && 'selected')}
+				>
+					Project
+				</TS_NavLink>
+				<TS_NavLink
+					key={PermissionDomainsEditor.Route.key}
+					route={PermissionDomainsEditor.Route}
+					ignoreClickOnSameRoute={true}
+					className={({isActive}) => _className('link-button', isActive && 'selected')}
+				>
+					Domains
+				</TS_NavLink>
+				<TS_NavLink
+					key={PermissionGroupsEditor.Route.key}
+					route={PermissionGroupsEditor.Route}
+					ignoreClickOnSameRoute={true}
+					className={({isActive}) => _className('link-button', isActive && 'selected')}
+				>
+					Groups
+				</TS_NavLink>
+				<TS_NavLink
+					key={PermissionUsersEditor.Route.key}
+					route={PermissionUsersEditor.Route}
+					ignoreClickOnSameRoute={true}
+					className={({isActive}) => _className('link-button', isActive && 'selected')}
+				>
+					Users
+				</TS_NavLink>
+			</LL_H_C>
+			<Outlet/>
 		</LL_V_L>;
 	}
-
-	private renderTabs = () => {
-		const tabs: Tab[] = [
-			{title: 'Projects', uid: 'projects', content: <PermissionProjectsEditor/>},
-			{
-				title: 'Domains',
-				uid: 'domains',
-				content: () => <PermissionDomainsEditor/>
-			},
-			{
-				title: 'Groups',
-				uid: 'groups',
-				content: <PermissionGroupsEditor/>
-			},
-			{title: 'Users', uid: 'users', content: <PermissionUsersEditor/>},
-		];
-		return <TS_Tabs tabs={tabs}/>;
-	};
 }
+
+
