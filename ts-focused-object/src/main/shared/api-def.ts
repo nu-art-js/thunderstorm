4b62d4354d7acd6e8083778cddc19352a931546e
Cipher <itayleybovich@gmail.com>
2024-04-24 16:33:01 +0300

    focused object revamp works well

--- ts-focused-object/src/main/shared/api-def.ts
+++ ts-focused-object/src/main/shared/api-def.ts
@@ -1,42 +1,29 @@
 import {ApiDefResolver, BodyApi, HttpMethod} from '@nu-art/thunderstorm';
-import {Focused, FocusEvent} from './types';
-import {TypedMap, UniqueId} from '@nu-art/ts-common';
-
-export type Request_UpdateFocusObject = {
-	focusData: Focused[]
-	event: FocusEvent
-}
-export type Request_SetFocusStatus = {
-	event: FocusEvent
-}
-export type Request_ReleaseObject = {
-	objectsToRelease: Focused[]
-}
-export type Request_ReleaseTabId = {}
+import {FocusedEntity} from './types';
 
 export type FocusedItem_Update = {
 	request: {
-		currentlyFocused: TypedMap<UniqueId[]>;
+		focusedEntities: FocusedEntity[];
 	};
 	response: void;
 }
 
 export type ApiStruct_FocusedObject = {
 	_v1: {
-		setFocusStatusByTabId: BodyApi<void, Request_SetFocusStatus>,
-		updateFocusData: BodyApi<void, Request_UpdateFocusObject>,
-		releaseObject: BodyApi<void, Request_ReleaseObject>,
-		releaseByTabId: BodyApi<void, Request_ReleaseTabId>,
+		// setFocusStatusByTabId: BodyApi<void, Request_SetFocusStatus>,
+		// updateFocusData: BodyApi<void, Request_UpdateFocusObject>,
+		// releaseObject: BodyApi<void, Request_ReleaseObject>,
+		// releaseByTabId: BodyApi<void, Request_ReleaseTabId>,
 		update: BodyApi<FocusedItem_Update['response'], FocusedItem_Update['request']>
 	}
 }
 
 export const ApiDef_FocusedObject: ApiDefResolver<ApiStruct_FocusedObject> = {
 	_v1: {
-		setFocusStatusByTabId: {method: HttpMethod.POST, path: '/v1/focus-object/set-focus-status'},
-		updateFocusData: {method: HttpMethod.POST, path: '/v1/focus-object/focus'},
-		releaseObject: {method: HttpMethod.POST, path: '/v1/focus-object/release'},
-		releaseByTabId: {method: HttpMethod.POST, path: '/v1/focus-object/release-tab'}, // Tab Id is sent as header, so we know what tab to release.
+		// setFocusStatusByTabId: {method: HttpMethod.POST, path: '/v1/focus-object/set-focus-status'},
+		// updateFocusData: {method: HttpMethod.POST, path: '/v1/focus-object/focus'},
+		// releaseObject: {method: HttpMethod.POST, path: '/v1/focus-object/release'},
+		// releaseByTabId: {method: HttpMethod.POST, path: '/v1/focus-object/release-tab'}, // Tab Id is sent as header, so we know what tab to release.
 		update: {method: HttpMethod.POST, path: '/v1/focus-object/update'},
 	}
 };
--- user-account/src/main/_entity/session/shared/consts.ts
+++ user-account/src/main/_entity/session/shared/consts.ts
@@ -2,6 +2,7 @@ export const accountGroupName = 'account';
 
 export const HeaderKey_SessionId = 'x-session-id';
 export const HeaderKey_TabId = 'tab-id';
+export const HeaderKey_DeviceId = 'device-id';
 export const HeaderKey_Application = 'x-application';
 export const HeaderKey_Email = 'x-email';
 
