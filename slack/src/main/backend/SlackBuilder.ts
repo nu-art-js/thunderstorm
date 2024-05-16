50e490244b2f216b0c52578c76849c4377e2e4db
Cipher <itayleybovich@gmail.com>
2024-05-16 10:23:23 +0300

    Slack builder to handle files as replies to messages

--- slack/src/main/backend/SlackBuilder.ts
+++ slack/src/main/backend/SlackBuilder.ts
@@ -1,7 +1,8 @@
 import {Stream} from 'stream';
-import {asArray, filterInstances, Logger} from '@nu-art/ts-common';
+import {asArray, Logger} from '@nu-art/ts-common';
 import {Block, KnownBlock} from '@slack/web-api';
 import {ModuleBE_Slack, ThreadPointer} from './ModuleBE_Slack';
+import {HttpCodes} from '@nu-art/ts-common/core/exceptions/http-codes';
 
 export type SlackFile = {
 	file: Buffer | Stream;
@@ -14,7 +15,7 @@ export type SlackBlock = Block | KnownBlock;
 export class SlackBuilder
 	extends Logger {
 
-	private images: SlackFile[] = [];
+	private files: SlackFile[] = [];
 	private blocks: SlackBlock[] = [];
 	private replies: SlackBlock[][] = [];
 	private readonly channel?: string;
@@ -26,8 +27,8 @@ export class SlackBuilder
 		this.channel = channel;
 	}
 
-	addImages = (images: SlackFile | SlackFile[]) => {
-		asArray(images).forEach(image => this.images.push(image));
+	addFiles = (files: SlackFile | SlackFile[]) => {
+		asArray(files).forEach(image => this.files.push(image));
 		return this;
 	};
 
@@ -44,46 +45,34 @@ export class SlackBuilder
 	};
 
 	send = async () => {
-		const imageBlocks = await this.sendFiles();
-		const messageResponse = await this.sendMessage(imageBlocks);
-		await this.sendReplies(messageResponse!);
+		const tp = await this.sendMessage();
+		if (!tp)
+			throw HttpCodes._5XX.INTERNAL_SERVER_ERROR(
+				'Error while sending slack message',
+				'Did not get thread pointer from sending slack message'
+			);
+		await this.sendFiles(tp);
+		await this.sendReplies(tp);
 	};
 
 	// ######################## Internal Logic ########################
 
-	private sendFiles = async () => {
-		this.logInfo('Sending Files');
-		const results = await Promise.all(this.images.map(async image => {
-			let tp: ThreadPointer | undefined;
-
-			if (this.channel)
-				tp = {channel: this.channel};
-
-			const response = await ModuleBE_Slack.postFile(image.file, image.fileName, tp);
-			if (!response.ok)
-				return this.logError(response.error);
-
-			return {
-				type: 'image',
-				// @ts-ignore
-				image_url: response.file.permalink,
-			} as SlackBlock;
-		}));
-		return filterInstances(results);
-	};
-
-	private sendMessage = async (fileBlocks: SlackBlock[]) => {
-		this.logInfo('Sending Message');
+	private sendMessage = async () => {
 		return ModuleBE_Slack.postStructuredMessage({
 			channel: this.channel,
-			blocks: [...this.blocks, ...fileBlocks],
-			unfurl_links: true,
-			unfurl_media: true
+			blocks: this.blocks,
 		});
 	};
 
+	private sendFiles = async (tp: ThreadPointer) => {
+		await Promise.all(this.files.map(async file => {
+			const response = await ModuleBE_Slack.postFile(file.file, file.fileName, tp);
+			if (!response.ok)
+				return this.logError(response.error);
+		}));
+	};
+
 	private sendReplies = async (tp: ThreadPointer) => {
-		this.logInfo('Sending Replies');
 		for (const reply of this.replies) {
 			await ModuleBE_Slack.postStructuredMessage({
 				blocks: reply
