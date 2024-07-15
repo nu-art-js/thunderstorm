021ad43167b492d92d6ed4e85499088f7feb90f1
Matan Koby <matan.k@quai.md>
2024-07-15 14:08:00 +0300

    Setup Project to have priority, dispatch turned into collector that receives priority and processor to run

--- thunderstorm/src/main/backend/modules/action-processor/Action_SetupProject.ts
+++ thunderstorm/src/main/backend/modules/action-processor/Action_SetupProject.ts
@@ -1,16 +1,31 @@
 import {ActionDeclaration} from './types';
-import {Dispatcher, Logger} from '@nu-art/ts-common';
+import {_keys, Dispatcher, Logger} from '@nu-art/ts-common';
 
+
+export type SetupProjectPromise = { priority: number, processor: () => Promise<void> };
 
 export interface PerformProjectSetup {
-	__performProjectSetup(): Promise<void>;
+	__performProjectSetup(): SetupProjectPromise;
 }
 
 const dispatcher_ProjectSetup = new Dispatcher<PerformProjectSetup, '__performProjectSetup'>('__performProjectSetup');
 
+type PromisesMap = { [priority: number]: { processors: (() => Promise<void>)[] } };
 const Action_SetupProject = async (logger: Logger) => {
 	logger.logInfo('Setting up Project');
-	await dispatcher_ProjectSetup.dispatchModuleAsync();
+	const promises = dispatcher_ProjectSetup.dispatchModule();
+	const promiseMap = promises.reduce<PromisesMap>((resultMap, promise) => {
+		resultMap[promise.priority] = resultMap[promise.priority] ?? {processors: []};
+		resultMap[promise.priority].processors.push(promise.processor);
+
+		return resultMap;
+	}, {});
+
+	const priorities = _keys(promiseMap).sort(); // Start with priority 0, as most important
+	for (const priorityKey of priorities) {
+		await Promise.all(promiseMap[priorityKey].processors);
+	}
+
 	logger.logInfo('Project Setup Completed!');
 };
 
