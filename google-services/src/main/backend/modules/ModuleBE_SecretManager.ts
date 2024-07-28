6130ff1fb50483a55edd2d2982a58356cab2a4e3
Cipher <itayleybovich@gmail.com>
2024-07-28 18:51:44 +0300

    ModuleBE_SecretManager create secret

--- google-services/src/main/backend/modules/ModuleBE_SecretManager.ts
+++ google-services/src/main/backend/modules/ModuleBE_SecretManager.ts
@@ -1,7 +1,17 @@
-import {__stringify, Module, MUSTNeverHappenException} from '@nu-art/ts-common';
+import {__stringify, BadImplementationException, Module, MUSTNeverHappenException} from '@nu-art/ts-common';
 import {SecretManagerServiceClient, v1} from '@google-cloud/secret-manager';
+import {google} from '@google-cloud/secret-manager/build/protos/protos';
 
 type Config = {}
+
+type SecretUpsertProps = {
+	//Path to secret, i.e. "projects/{parent}"
+	parent: string;
+	//Secret name
+	name: string;
+	//Secret data
+	data: string
+}
 
 export class ModuleBE_SecretManager_Class
 	extends Module<Config> {
@@ -28,6 +38,60 @@ export class ModuleBE_SecretManager_Class
 			throw e;
 		}
 	}
+
+	public async upsertSecret(props: SecretUpsertProps): Promise<string> {
+		const secret = await this.getOrCreateSecret(props.parent, props.name);
+		if (!secret.name)
+			throw new BadImplementationException(`Got string with no name on it for ${__stringify(props)}`);
+
+		await this.updateSecret(secret, props.data);
+		return secret.name;
+	}
+
+	//######################### Inner Logic #########################
+
+	private getOrCreateSecret = async (parent: string, name: string): Promise<google.cloud.secretmanager.v1.ISecret> => {
+		try {
+			const pathToSecret = `projects/${parent}/secrets/${name}`;
+			const [secret] = await this.secretManagerClient.getSecret({name: pathToSecret});
+			//Secret exists, return it
+			this.logVerbose(`Secret exists: ${secret.name}`);
+			return secret;
+		} catch (err: any) {
+			if (err.code !== 5) {
+				this.logError('Failed to get secret', err);
+				throw err;
+			}
+			//Secret did not exist, create and return it
+			const [secret] = await this.secretManagerClient.createSecret({
+				parent: `projects/${parent}`,
+				secretId: name,
+				secret: {
+					name: name,
+					replication: {
+						automatic: {},
+					}
+				}
+			});
+			this.logVerbose(`Created secret ${secret.name}`);
+			return secret;
+		}
+	};
+
+	private updateSecret = async (secret: google.cloud.secretmanager.v1.ISecret, data: string): Promise<void> => {
+		try {
+			const [version] = await this.secretManagerClient.addSecretVersion({
+				parent: secret.name,
+				payload: {
+					data: Buffer.from(data, 'utf-8')
+				},
+			});
+			this.logVerbose(`Updated secret ${secret.name} version ${version.name}`);
+		} catch (err: any) {
+			this.logError(`Failed to update secret ${secret.name}`);
+			throw err;
+		}
+	};
 }
 
 export const ModuleBE_SecretManager = new ModuleBE_SecretManager_Class();
