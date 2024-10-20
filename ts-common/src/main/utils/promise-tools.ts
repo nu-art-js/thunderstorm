9492ea590ab7d48a1acedea779f4d8c339ceb2ae
CosmisSpectrum <hareldaganh@gmail.com>
2024-10-20 10:46:04 +0300

    fix no loader issue in ts-button-v2

--- ts-common/src/main/utils/promise-tools.ts
+++ ts-common/src/main/utils/promise-tools.ts
@@ -10,3 +10,11 @@ export type PromiseCallbackFunction<T> = (resolve: ResolvePromiseFunction<T>, re
 export const promiseWrapper = <T>(callback: PromiseCallbackFunction<T>) => {
 	return new Promise<T>((resolve, reject) => callback(resolve, reject));
 };
+
+/**
+ * Check if a result of a function call is a promise
+ * @param obj The function call to validate
+ */
+export const isPromise = <T = any>(obj: any): obj is Promise<T> => {
+	return !!obj && typeof obj.then === 'function' && typeof obj.catch === 'function';
+};
