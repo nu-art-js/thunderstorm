6b3ba1e272dadcd6ba3c22ba40c98529082f11e3
Matan Koby <matan.k@quai.md>
2024-12-15 14:10:40 +0200

    Allow providing an outside sourced lastSync timestamp to IDBCache

--- thunderstorm/src/main/frontend/modules/db-api-gen/ModuleFE_BaseApi.ts
+++ thunderstorm/src/main/frontend/modules/db-api-gen/ModuleFE_BaseApi.ts
@@ -21,12 +21,12 @@
 
 import {_EmptyQuery, FirestoreQuery} from '@nu-art/firebase';
 import {BadImplementationException, DB_BaseObject, DBDef_V3, DBProto, IndexKeys, TypedMap} from '@nu-art/ts-common';
-import {MemCache, ModuleFE_BaseDB} from './ModuleFE_BaseDB';
+import {ModuleFE_BaseDB} from './ModuleFE_BaseDB';
 import {ApiDefCaller, ApiStruct_DBApiGenIDBV3, BaseHttpRequest, DBApiDefGeneratorIDBV3, HttpException, TypedApi} from '../../shared';
 import {DBApiFEConfig} from '../../core/db-api-gen/db-def';
 import {ThunderDispatcher} from '../../core/thunder-dispatcher';
 import {apiWithBody, apiWithQuery} from '../../core/typed-api';
-import {ModuleSyncType} from './types';
+import {CustomMemCreators, ModuleSyncType} from './types';
 
 
 type RequestType = 'upsert' | 'patch' | 'delete';
@@ -50,8 +50,8 @@ export abstract class ModuleFE_BaseApi<Proto extends DBProto<any>, _Config exten
 	readonly v1: ApiDefCaller<ApiStruct_DBApiGenIDBV3<Proto>>['v1'];
 	private operations: TypedMap<Operation> = {};
 
-	protected constructor(dbDef: DBDef_V3<Proto>, defaultDispatcher: ThunderDispatcher<any, string>, version?: string, customMemCacheCreator?: (_config: DBApiFEConfig<Proto>, _this: ModuleFE_BaseDB<Proto, any>) => MemCache<Proto>) {
-		super(dbDef, defaultDispatcher, ModuleSyncType.APISync, customMemCacheCreator);
+	protected constructor(dbDef: DBDef_V3<Proto>, defaultDispatcher: ThunderDispatcher<any, string>, version?: string, customMemCreators?: CustomMemCreators<Proto>) {
+		super(dbDef, defaultDispatcher, ModuleSyncType.APISync, customMemCreators);
 
 		const apiDef = this.resolveApiDef(dbDef, version);
 
