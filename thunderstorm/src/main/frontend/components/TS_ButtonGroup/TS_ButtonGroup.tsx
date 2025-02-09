ba87d26bb89288f0a636df08124a92bf0e595e94
Cipher <itayleybovich@gmail.com>
2025-02-09 13:13:34 +0200

    button group fixes

--- thunderstorm/src/main/frontend/components/TS_ButtonGroup/TS_ButtonGroup.tsx
+++ thunderstorm/src/main/frontend/components/TS_ButtonGroup/TS_ButtonGroup.tsx
@@ -101,7 +101,7 @@ export class TS_ButtonGroup<ButtonKey extends string = string>
 		return <React.Fragment key={button.key}>
 			{(index !== 0) && <div className={separatorClassName}/>}
 			<Button
-				variant={'text'}
+				variant={'button-group-button'}
 				className={buttonClassName}
 				id={`button-${button.key}`}
 				onClick={e => this.handleClick(e, button.key)}
