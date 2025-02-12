fb11d2c57e648c6f48557118dce2e09228a23483
stav-adimor <stav@quai.md>
2025-02-12 15:12:24 +0200

    Add Checkbox Group component ##6800

--- thunderstorm/src/main/frontend/components/ats-group.ts
+++ thunderstorm/src/main/frontend/components/ats-group.ts
@@ -1,6 +1,8 @@
 import {ATS_Button} from './Button/ATS_Button';
 import {AppToolsScreen} from './TS_AppTools';
+import {ATS_CheckboxGroup} from "./TS_CheckboxGroup/ATS_CheckboxGroup";
 
 export const ATSGroup_ThunderstormComponents: AppToolsScreen[] = [
 	ATS_Button.Screen,
+	ATS_CheckboxGroup.Screen
 ];
