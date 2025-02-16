7c7fec342a189e068bd1327f4996c9ec3b231e54
Cipher <itayleybovich@gmail.com>
2025-02-16 13:36:28 +0200

    Label component

--- thunderstorm/src/main/frontend/components/ats-group.ts
+++ thunderstorm/src/main/frontend/components/ats-group.ts
@@ -1,8 +1,10 @@
 import {ATS_Button} from './Button/ATS_Button';
+import {ATS_Label} from './Label/ATS_Label';
 import {AppToolsScreen} from './TS_AppTools';
-import {ATS_CheckboxGroup} from "./TS_CheckboxGroup/ATS_CheckboxGroup";
+import {ATS_CheckboxGroup} from './TS_CheckboxGroup/ATS_CheckboxGroup';
 
 export const ATSGroup_ThunderstormComponents: AppToolsScreen[] = [
 	ATS_Button.Screen,
-	ATS_CheckboxGroup.Screen
+	ATS_CheckboxGroup.Screen,
+	ATS_Label.Screen,
 ];
