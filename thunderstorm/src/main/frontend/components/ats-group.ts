b34bf553c7d78ff2b34942aa0f5f28f0f3582e96
Cipher <itayleybovich@gmail.com>
2025-02-18 17:43:14 +0200

    ModuleFE_Print and printing react elements

--- thunderstorm/src/main/frontend/components/ats-group.ts
+++ thunderstorm/src/main/frontend/components/ats-group.ts
@@ -1,3 +1,4 @@
+import {ATS_Printable} from '../_ats/ATS_Printables/ATS_Printable';
 import {ATS_Button} from './Button/ATS_Button';
 import {ATS_Label} from './Label/ATS_Label';
 import {AppToolsScreen} from './TS_AppTools';
@@ -7,4 +8,5 @@ export const ATSGroup_ThunderstormComponents: AppToolsScreen[] = [
 	ATS_Button.Screen,
 	ATS_CheckboxGroup.Screen,
 	ATS_Label.Screen,
+	ATS_Printable.Screen,
 ];
--- thunderstorm/src/main/frontend/consts.ts
+++ thunderstorm/src/main/frontend/consts.ts
@@ -1,1 +1,2 @@
 export const thunderstormATSGroups = 'Thunderstorm Components';
+export const thunderstormCapabilitiesGroup = 'Thunderstorm Capabilities';
