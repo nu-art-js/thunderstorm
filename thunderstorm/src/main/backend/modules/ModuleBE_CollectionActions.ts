aa345009a35078e8a3caa89a59c8469f7031bc86
Cipher <itayleybovich@gmail.com>
2025-03-02 14:14:18 +0200

    WIP

---
+++ thunderstorm/src/main/backend/modules/ModuleBE_CollectionActions.ts
@@ -0,0 +1,77 @@
+import {filterInstances, LogLevel, Module, Promise_all_sequentially, RuntimeModules} from '@nu-art/ts-common';
+import {addRoutes} from './ModuleBE_APIs';
+import {createBodyServerApi} from '../core/typed-api';
+import {ApiDef_CollectionActions, CollectionActions_Check, CollectionActions_Upgrade} from '../../shared/collection-actions/api-def';
+import {ModuleBE_BaseDB} from './db-api-gen/ModuleBE_BaseDB';
+
+class ModuleBE_CollectionActions_Class
+	extends Module {
+
+	constructor() {
+		super();
+		this.setMinLevel(LogLevel.Verbose);
+	}
+
+	protected init() {
+		super.init();
+		addRoutes([
+			//Add upgrade routes
+			createBodyServerApi(ApiDef_CollectionActions.upgrade.collections, this.upgrade_Collections),
+			createBodyServerApi(ApiDef_CollectionActions.upgrade.all, this.upgrade_All),
+			//Add check routes
+			createBodyServerApi(ApiDef_CollectionActions.check.usage, this.check_Usage),
+		]);
+	}
+
+	// ##################### Internal Logic #####################
+
+	private getUpgradableModules = async (limitKeys: string[], forceUpgrade?: boolean): Promise<ModuleBE_BaseDB<any>[]> => {
+		const filterModule = async (_module: Module) => {
+			const module = _module as ModuleBE_BaseDB<any>;
+			const dbKey = module?.dbDef?.dbKey;
+			//Assert dbKey exists
+			if (!dbKey)
+				return undefined;
+
+			//Assert not an advisor collection module
+			if ((module as ModuleBE_BaseDB<any> & { advisorCollectionModule: boolean }).advisorCollectionModule)
+				return undefined;
+
+			//Assert in the user given keys
+			if (limitKeys.length > 0 && !limitKeys.includes(dbKey))
+				return undefined;
+
+			const isUpToDate = await module.isCollectionUpToDate();
+			return (!isUpToDate || forceUpgrade) ? module : undefined;
+		};
+		const allModules = RuntimeModules().all;
+		return filterInstances(await Promise.all(allModules.map(filterModule)));
+	};
+
+	// ##################### API Callbacks - Upgrade #####################
+
+	public upgrade_Collections = async (req: CollectionActions_Upgrade['collections']['request']): Promise<CollectionActions_Upgrade['collections']['response']> => {
+		this.logInfo(`Upgrade - Collections${req.force ? ', Forcefully' : ''}`);
+		if (req.dbKeys.length)
+			this.logInfo(`Limited to ${req.dbKeys.length} collections:`, req.dbKeys.join(', '));
+		else
+			this.logInfo('No collection limit');
+
+		const modules = await this.getUpgradableModules(req.dbKeys, req.force);
+		this.logInfo(`Will upgrade ${modules.length} modules`);
+		await Promise_all_sequentially(modules.map(module => () => module.upgradeCollection(req.force)));
+	};
+
+	public upgrade_All = async (req: CollectionActions_Upgrade['all']['request']): Promise<CollectionActions_Upgrade['all']['response']> => {
+		this.logInfo('Upgrade - All');
+		await this.upgrade_Collections({dbKeys: [], force: req.force});
+	};
+
+	// ##################### API Callbacks - Check #####################
+
+	public check_Usage = async (req: CollectionActions_Check['usage']['request']): Promise<CollectionActions_Check['usage']['response']> => {
+
+	};
+}
+
+export const ModuleBE_CollectionActions = new ModuleBE_CollectionActions_Class();
