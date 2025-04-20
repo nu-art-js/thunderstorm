771fe7e04dc705a9afcf4aa903c12551823ff88d
stav-adimor <stav@quai.md>
2025-04-20 16:34:29 +0300

    changes - now km refresh works

--- user-account/src/main/_entity/account/frontend/consts.ts
+++ user-account/src/main/_entity/account/frontend/consts.ts
@@ -1,8 +1,10 @@
 import {StorageKey} from '@nu-art/thunderstorm/frontend';
-import {_SessionKey_Account} from '../shared';
+import {_SessionKey_Account, DB_Account} from '../shared';
 import {HeaderKey_DeviceId, HeaderKey_TabId} from '@nu-art/thunderstorm/shared/headers';
 import {SessionKey_FE} from '../../session/frontend/ModuleFE_Session';
+import {TypedKeyValue} from '@nu-art/ts-common';
 
 export const SessionKeyFE_Account = new SessionKey_FE<_SessionKey_Account>('account');
 export const StorageKey_DeviceId = new StorageKey<string>(`storage--${HeaderKey_DeviceId}`).withstandDeletion();
 export const StorageKey_TabId = new StorageKey<string>(`storage--${HeaderKey_TabId}`, false).withstandDeletion();
+export const SessionKey_Account = new SessionKey_FE<TypedKeyValue<'account', DB_Account>>('account');
