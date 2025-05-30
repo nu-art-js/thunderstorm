0269de06e3db3154190a2519c0454ec154528dac
TacB0sS <adam.vdk@nu-art-software.com>
2025-05-30 23:10:35 +0300

    added session module to the account module pack

--- user-account/src/main/_entity/account/frontend/module-pack.ts
+++ user-account/src/main/_entity/account/frontend/module-pack.ts
@@ -1,4 +1,5 @@
 import {Module} from '@nu-art/ts-common';
 import {ModuleFE_Account} from './ModuleFE_Account';
+import {ModuleFE_Session} from '../../session/frontend/ModuleFE_Session';
 
-export const ModulePackFE_AccountDB: Module[] = [ModuleFE_Account];
+export const ModulePackFE_AccountDB: Module[] = [ModuleFE_Account, ModuleFE_Session];
