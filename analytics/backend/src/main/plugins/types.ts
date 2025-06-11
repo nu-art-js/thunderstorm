80c682fdad7929c6ead276291835365d39d298aa
Cipher <itayleybovich@gmail.com>
2025-06-11 14:54:45 +0300

    compiles, BE done

--- analytics/backend/src/main/plugins/types.ts
+++ analytics/backend/src/main/plugins/types.ts
@@ -1,33 +1,6 @@
-import {TSAnalyticsEvent} from '@nu-art/analytics-shared/src/main';
-
-/**
- * C - plugin specific config, to be used later in interface implementations
- * R - Translation response
- */
-export interface TSAnalyticsPlugin<C extends Record<string, any> = {}, R extends any = any> {
-	/**
-	 * Unique key used to identify this plugin and retrieve its config.
-	 * Example: 'mixpanel', 'amplitude', 'customLogger'
-	 */
-	key: string;
-
-	/**
-	 * Called to initialize the plugin with configuration data (fetched from Firebase or elsewhere).
-	 */
-	init(config: C): void;
-
-	/**
-	 * Translates the in-house AnalyticsEvent into the format this provider expects.
-	 */
-	translate(event: TSAnalyticsEvent): R;
-
-	/**
-	 * Sends the event to the provider.
-	 */
-	send(event: TSAnalyticsEvent): Promise<void>;
-
-	/**
-	 * Sends multiple events to the provider.
-	 */
-	bulkSend?(events: TSAnalyticsEvent[]): Promise<void>;
+export type AnalyticsPluginBaseConfig = {
+	token: string;
+	eventPacketSize: number;
 }
+
+export type AnalyticPanelConfig<C> = AnalyticsPluginBaseConfig & C;
--- analytics/shared/src/main/api-def.ts
+++ analytics/shared/src/main/api-def.ts
@@ -2,17 +2,17 @@ import {ApiDefResolver, BodyApi, HttpMethod} from '@nu-art/thunderstorm';
 
 export type Analytics_SendEvent = {
 	request: {};
-	response: {};
+	response: void;
 };
 
 export type ApiStruct_Analytics = {
 	_v1: {
-		sendEvent: BodyApi<Analytics_SendEvent['response'],Analytics_SendEvent['request']>;
+		sendEvent: BodyApi<Analytics_SendEvent['response'], Analytics_SendEvent['request']>;
 	}
 }
 
-export const ApiDef_FocusedObject: ApiDefResolver<ApiStruct_Analytics> = {
+export const ApiDef_Analytics: ApiDefResolver<ApiStruct_Analytics> = {
 	_v1: {
 		sendEvent: {method: HttpMethod.POST, path: '/v1/analytics/send-event'},
 	}
-}
+};
