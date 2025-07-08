42454c7439fe26de8e340e0c62b95f4189624eaf
TacB0sS <adam.vdk@nu-art-software.com>
2025-07-08 23:57:34 +0300

    param will be resolved completely and not by length

--- commando/src/main/cli-params/CLIParamsResolver.ts
+++ commando/src/main/cli-params/CLIParamsResolver.ts
@@ -1,9 +1,4 @@
-import {
-	asArray,
-	exists,
-	filterDuplicates,
-	StaticLogger
-} from '@nu-art/ts-common';
+import {asArray, exists, filterDuplicates, StaticLogger} from '@nu-art/ts-common';
 import {BaseCliParam, CliParam, CliParams} from './types';
 import {DefaultProcessorsMapper} from './consts';
 
@@ -84,16 +79,15 @@ export class CLIParamsResolver<T extends BaseCliParam<string, any>[], Output ext
 	 * @private
 	 */
 	private findMatchingParamToResolve(inputParam: string) {
-		let maxKeyLength = 0;
 		let matchingParam: CliParam<string, any> | undefined;
 
-		// look for the longest fitting param in order to make sure we find the perfect match
 		this.params.forEach((param) => {
 			param.keys.forEach((key) => {
-				if (inputParam.startsWith(key) && key.length > maxKeyLength) {
-					maxKeyLength = key.length;
+				if (inputParam === key)
 					matchingParam = param;
-				}
+
+				if (inputParam.startsWith(`${key}=`))
+					matchingParam = param;
 			});
 		});
 
