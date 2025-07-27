4995255438630c12ccf8fcab7b5fb23abd03bc38
TacB0sS <adam.vdk@nu-art-software.com>
2025-07-27 14:34:04 +0300

    switch view v2

---
+++ permissions/src/main/frontend/ui/Component_SwitchViewV2.tsx
@@ -0,0 +1,50 @@
+import * as React from 'react';
+import {ComponentSync} from '@nu-art/thunderstorm/frontend';
+import {ResolvableContent, resolveContent} from '@nu-art/ts-common';
+import {PermissionKey_FE} from '../PermissionKey_FE';
+import {AccessLevel} from '../modules/ModuleFE_PermissionsAssert';
+
+type Modes<T extends string = 'view' | 'edit'> = {
+	[key in T]: ResolvableContent<React.ReactNode | undefined>;
+}
+
+type Props<T extends string = 'view' | 'edit'> = {
+	mode: T;
+	modes: Modes<T>;
+};
+
+type State<T extends string = 'view' | 'edit'> = {
+	mode: T;
+	modes: Modes<T>;
+};
+
+export const permissionsGuardedRenderer = (permissionKey: PermissionKey_FE, renderer: ResolvableContent<React.ReactNode>) => {
+	const accessLevel = permissionKey.getAccessLevel();
+	if (accessLevel === AccessLevel.Undefined) {
+		console.log(`Data for permission key ${permissionKey.key} does not exist`);
+		return;
+	}
+
+	if (accessLevel < AccessLevel.HasAccess)
+		return;
+
+	return resolveContent(renderer);
+};
+
+export class Component_SwitchViewV2
+	extends ComponentSync<Props, State> {
+
+	// ######################## Lifecycle ########################
+
+	protected deriveStateFromProps(nextProps: Props, state: State) {
+		state.mode = nextProps.mode;
+		state.modes = nextProps.modes;
+		return state;
+	}
+
+	// ######################## Render ########################
+
+	render() {
+		return resolveContent(this.state.modes[this.state.mode]);
+	}
+}
