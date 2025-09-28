7fee8151de251f2638376f8159d0f0c82756fcff
Cipher <itayleybovich@gmail.com>
2025-09-28 12:12:32 +0300

    remove option to download video

--- thunderstorm/src/main/frontend/components/Video/Video.tsx
+++ thunderstorm/src/main/frontend/components/Video/Video.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import {ComponentSync} from '../../core/ComponentSync';
-import {TS_Video_Props, TS_VideoConfiguration, TS_VideoSource} from './types';
+import {TS_Video_Props, TS_VideoConfiguration, TS_VideoControlsConfiguration, TS_VideoSource} from './types';
+import {exists} from '@nu-art/ts-common';
 
 const defaultVideoUnsupportedMessage = 'Video not supported in this browser';
 
@@ -28,16 +29,34 @@ export class TS_Video
 
 	//######################### Logic #########################
 
+	private getControlsList = (config?: TS_VideoControlsConfiguration): string | undefined => {
+		if (!config)
+			return;
+
+		const list: string[] = [];
+		if (config.noDownload)
+			list.push('nodownload');
+		return list.join(' ');
+	};
+
+	private getProps = (): React.HTMLProps<HTMLVideoElement> => {
+		return {
+			width: this.state.width,
+			height: this.state.height,
+			autoPlay: this.state.configuration.autoplay,
+			muted: this.state.configuration.muted,
+
+			//Controls props
+			controls: exists(this.state.configuration.controls),
+			// @ts-ignore
+			controlsList: this.getControlsList(this.state.configuration.controls)
+		};
+	};
+
 	//######################### Render #########################
 
 	render() {
-		return <video
-			width={this.state.width}
-			height={this.state.height}
-			controls={this.state.configuration.controls}
-			autoPlay={this.state.configuration.autoplay}
-			muted={this.state.configuration.muted}
-		>
+		return <video {...this.getProps()}>
 			<source src={this.state.source.url} type={`video/${this.state.source.format}`}/>
 			{this.state.unsupportedVideoMessage}
 		</video>;
--- thunderstorm/src/main/frontend/components/Video/types.ts
+++ thunderstorm/src/main/frontend/components/Video/types.ts
@@ -1,10 +1,16 @@
 export type TS_VideoSourceFormat = 'mp4' | 'webm' | 'ogg';
 export type TS_VideoSource = { url: string, format: TS_VideoSourceFormat };
+
+export type TS_VideoControlsConfiguration = {
+	noDownload?: boolean;
+}
+
 export type TS_VideoConfiguration = {
-	controls?: boolean;
+	controls?: TS_VideoControlsConfiguration;
 	autoplay?: boolean;
 	muted?: boolean;
 }
+
 
 export type TS_Video_Props = {
 	source: TS_VideoSource;
