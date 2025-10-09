c7c134e2caa07ecedb2cb980d547f19d3b1f4151
Cipher <itayleybovich@gmail.com>
2025-10-09 14:19:08 +0300

    checkbox v2

--- thunderstorm/src/main/frontend/components/ats-group.ts
+++ thunderstorm/src/main/frontend/components/ats-group.ts
@@ -2,6 +2,7 @@ import {ATS_Printable} from '../_ats/ATS_Printables/ATS_Printable';
 import {ATS_Button} from './Button/ATS_Button';
 import {ATS_Label} from './Label/ATS_Label';
 import {AppToolsScreen} from './TS_AppTools';
+import {ATS_CheckboxV2} from './TS_Checkbox/ATS_CheckboxV2';
 import {ATS_CheckboxGroup} from './TS_CheckboxGroup/ATS_CheckboxGroup';
 import {ATS_CollapsableContainerV2} from './TS_CollapsableContainerV2';
 
@@ -11,4 +12,5 @@ export const ATSGroup_ThunderstormComponents: AppToolsScreen[] = [
 	ATS_Label.Screen,
 	ATS_Printable.Screen,
 	ATS_CollapsableContainerV2,
+	ATS_CheckboxV2,
 ];
