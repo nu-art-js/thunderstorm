f80bff59216192b8a6d65aa8e284898b1741f1d7
Cipher <itayleybovich@gmail.com>
2025-11-05 15:31:20 +0200

    floating windows core functionality

---
+++ floating-windows/src/main/frontend/_modules/ModuleFE_FloatingWindows.ts
@@ -0,0 +1,30 @@
+import {Module, removeFromArrayByIndex} from '@nu-art/ts-common';
+import {Model_FloatingWindow} from '../types';
+import {dispatcher_FloatingWindows_FocusWindow} from '../_dispatchers/focus-window';
+import {dispatch_FloatingWindows_WindowsUpdated} from '../_dispatchers/models-updated';
+
+class ModuleFE_FloatingWindow_Class
+	extends Module {
+
+	public readonly windowModels: Model_FloatingWindow[] = [];
+
+	public window = {
+		add: (model: Model_FloatingWindow): void => {
+			if (this.windowModels.find(item => item.key === model.key))
+				return void dispatcher_FloatingWindows_FocusWindow.dispatchUI(model.key);
+
+			this.windowModels.push({...model});
+			dispatch_FloatingWindows_WindowsUpdated.dispatchUI();
+		},
+		remove: (key: string): void => {
+			const index = this.windowModels.findIndex(model => model.key === key);
+			if (index === -1)
+				return;
+
+			removeFromArrayByIndex(this.windowModels, index);
+			dispatch_FloatingWindows_WindowsUpdated.dispatchUI();
+		}
+	};
+}
+
+export const ModuleFE_FloatingWindows = new ModuleFE_FloatingWindow_Class();
---
+++ floating-windows/src/main/frontend/_ui/TS_FloatingWindows/TS_FloatingWindows.tsx
@@ -0,0 +1,224 @@
+import * as React from 'react';
+import {_className, ComponentSync} from '@nu-art/thunderstorm/frontend';
+import {ModuleFE_FloatingWindows} from '../../_modules/ModuleFE_FloatingWindows';
+import {FloatingWindows_WindowsUpdated} from '../../_dispatchers/models-updated';
+import {FloatingWindows_FocusWindow} from '../../_dispatchers/focus-window';
+import './TS_FloatingWindows.scss';
+import {BadImplementationException, Debounce, Second} from '@nu-art/ts-common';
+
+type State = {
+	windowModels: typeof ModuleFE_FloatingWindows['windowModels'];
+};
+
+export class TS_FloatingWindows
+	extends ComponentSync<unknown, State>
+	implements FloatingWindows_WindowsUpdated {
+
+	//######################### Life Cycle #########################
+
+	__onFloatingWindowsUpdated = () => {
+		this.forceUpdate();
+	};
+
+	protected deriveStateFromProps(nextProps: unknown, state: State) {
+		state.windowModels ??= ModuleFE_FloatingWindows.windowModels;
+		return state;
+	}
+
+	componentDidUpdate() {
+		if (!this.state.windowModels.length)
+			TS_FloatingWindow.runningZIndex = 0;
+	}
+
+	//######################### Logic #########################
+
+	//######################### Render #########################
+
+	render() {
+		if (!this.state.windowModels?.length)
+			return;
+
+		return <div id={'ts-floating-windows'}>
+			{this.state.windowModels.map(model => {
+				return <TS_FloatingWindow
+					key={model.key}
+					model={model}
+				/>;
+			})}
+		</div>;
+	}
+}
+
+type Props_FloatingWindow = { model: typeof ModuleFE_FloatingWindows['windowModels'][number] };
+
+class TS_FloatingWindow
+	extends ComponentSync<Props_FloatingWindow>
+	implements FloatingWindows_FocusWindow {
+
+	static runningZIndex: number = 0;
+
+	private wrapperRef: React.RefObject<HTMLDivElement> = React.createRef();
+	private anchorRef: React.RefObject<HTMLDivElement> = React.createRef();
+
+	private focusTimeout?: NodeJS.Timeout;
+	private resizeDebouncer = new Debounce(() => this.onResize(), 100, 250);
+	private resizeObserver = new ResizeObserver(() => this.resizeDebouncer.trigger());
+	private dragOffset?: { x: number; y: number };
+
+	//######################### Life Cycle #########################
+
+	//Logic for programmatic focus
+	__onFocusFloatingWindow = (windowKey: string) => {
+		if (windowKey !== this.props.model.key)
+			return;
+
+		const wrapper = this.wrapperRef.current;
+		if (!wrapper)
+			return;
+
+		wrapper.focus();
+		wrapper.classList.add('focus');
+		//Bring to foreground
+		if (Number(wrapper.style.zIndex) !== TS_FloatingWindow.runningZIndex)
+			wrapper.style.zIndex = `${++TS_FloatingWindow.runningZIndex}`;
+
+		this.focusTimeout = setTimeout(() => {
+			wrapper.classList.remove('focus');
+			delete this.focusTimeout;
+		}, Second);
+	};
+
+	componentDidMount() {
+		const wrapper = this.getWrapperElement();
+		//Focus the window when it first mounts
+		wrapper.focus();
+		this.resizeObserver.observe(wrapper);
+	}
+
+	componentWillUnmount() {
+		clearTimeout(this.focusTimeout);
+		this.resizeObserver.disconnect();
+	}
+
+	//######################### Logic #########################
+
+	private closeWindow = () => {
+		ModuleFE_FloatingWindows.window.remove(this.props.model.key);
+	};
+
+	private getClassName = () => {
+		const model = this.props.model;
+		return _className(
+			'ts-floating-window',
+			model.resizable && 'resizable',
+			model.className,
+		);
+	};
+
+	private getStyle = (): React.CSSProperties => {
+		const model = this.props.model;
+		return {
+			left: model.rect.x,
+			top: model.rect.y,
+			width: model.rect.width,
+			height: model.rect.height,
+			zIndex: ++TS_FloatingWindow.runningZIndex,
+		};
+	};
+
+	private getWrapperElement = () => {
+		const wrapper = this.wrapperRef.current;
+		if (!wrapper)
+			throw new BadImplementationException('Wrapper ref disconnected!');
+
+		return wrapper;
+	};
+
+	private getAnchorElement = () => {
+		const anchorElement = this.anchorRef.current;
+		if (!anchorElement)
+			throw new BadImplementationException('Anchor ref disconnected!');
+
+		return anchorElement;
+	};
+
+	//######################### Event Callbacks #########################
+
+	private onResize = () => {
+		const wrapper = this.getWrapperElement();
+		const model = this.props.model;
+		const rect = wrapper.getBoundingClientRect();
+		model.rect.x = rect.x;
+		model.rect.y = rect.y;
+		model.rect.height = rect.height;
+		model.rect.width = rect.width;
+	};
+
+	//Logic for UI focus
+	private onFocus = () => {
+		const wrapper = this.getWrapperElement();
+		//Bring to foreground
+		if (Number(wrapper.style.zIndex) !== TS_FloatingWindow.runningZIndex)
+			wrapper.style.zIndex = `${++TS_FloatingWindow.runningZIndex}`;
+	};
+
+	private onDragStart = (e: React.MouseEvent<HTMLDivElement>) => {
+		const anchor = this.getAnchorElement();
+		const wrapper = this.getWrapperElement();
+		anchor.addEventListener('pointermove', this.onDragMove);
+		anchor.addEventListener('pointerup', this.onDragEnd);
+		wrapper.classList.add('dragging');
+		//Calc x & y offset from wrapper top-left corner
+		this.dragOffset = {
+			x: e.clientX - this.props.model.rect.x,
+			y: e.clientY - this.props.model.rect.y,
+		};
+	};
+
+	private onDragMove = (e: MouseEvent) => {
+		const newX = e.clientX - (this.dragOffset?.x ?? 0);
+		const newY = e.clientY - (this.dragOffset?.y ?? 0);
+		this.props.model.rect.x = newX;
+		this.props.model.rect.y = newY;
+		const wrapper = this.getWrapperElement();
+		wrapper.style.left = `${newX}px`;
+		wrapper.style.top = `${newY}px`;
+	};
+
+	private onDragEnd = () => {
+		const anchor = this.getAnchorElement();
+		const wrapper = this.getWrapperElement();
+		anchor.removeEventListener('pointermove', this.onDragMove);
+		anchor.removeEventListener('pointerup', this.onDragEnd);
+		wrapper.classList.remove('dragging');
+		delete this.dragOffset;
+	};
+
+	//######################### Render #########################
+
+	render() {
+		const model = this.props.model;
+		return <div
+			ref={this.wrapperRef}
+			className={this.getClassName()}
+			style={this.getStyle()}
+			tabIndex={0}
+			onFocus={this.onFocus}
+		>
+			{this.render_DragAnchor()}
+			{model.content(this.closeWindow)}
+		</div>;
+	}
+
+	private render_DragAnchor = () => {
+		const model = this.props.model;
+		if (!model.moveable)
+			return;
+
+		return <div
+			ref={this.anchorRef}
+			className={'ts-floating-window__drag-anchor'}
+			onPointerDown={this.onDragStart}
+		/>;
+	};
+}
