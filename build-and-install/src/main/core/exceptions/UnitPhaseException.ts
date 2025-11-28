0dc12393fef5b0746471e24ccc21c945ed8cda91
TacB0sS <adam.vdk@nu-art-software.com>
2025-11-28 12:37:30 +0200

    deploying for real

---
+++ build-and-install/src/main/core/exceptions/UnitPhaseException.ts
@@ -0,0 +1,10 @@
+import {CustomException} from '@nu-art/ts-common';
+import {BaseUnit} from '../../v3/units/index.js';
+
+export class UnitPhaseException
+	extends CustomException {
+
+	constructor(cause: Error, unit: BaseUnit, phase: string) {
+		super(UnitPhaseException, `Error in ${unit.config.key} (${phase})`, cause);
+	}
+}
