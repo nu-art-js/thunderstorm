ab1434f39bc200a08363e0fb88ae4327b348393b
TacB0sS <adam.vdk@nu-art-software.com>
2026-01-19 13:28:11 +0200

    fixed slack package

--- slack/shared/src/main/types.ts
+++ slack/shared/src/main/types.ts
@@ -1,4 +1,4 @@
 import {PartialProperties} from '@nu-art/ts-common';
 import {ChatPostMessageArguments} from '@slack/web-api';
 
-export type PreSendSlackStructuredMessage = PartialProperties<ChatPostMessageArguments, 'channel' | 'text'>
+export type PreSendSlackStructuredMessage = PartialProperties<ChatPostMessageArguments, 'channel'>
