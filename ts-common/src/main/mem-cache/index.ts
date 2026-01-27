dcf99b1b108233fbd807a127252d477c38f96ffe
TacB0sS <adam.vdk@nu-art-software.com>
2026-01-27 15:42:19 +0200

    memcache

---
+++ ts-common/src/main/mem-cache/index.ts
@@ -0,0 +1,7 @@
+/*
+ * ts-common - Core TypeScript infrastructure
+ * Copyright (C) 2020 Adam van der Kruk aka TacB0sS
+ * Licensed under the Apache License, Version 2.0
+ */
+
+export * from './MemCache.js';
