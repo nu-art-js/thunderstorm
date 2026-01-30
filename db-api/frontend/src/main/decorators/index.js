9cac1e9ee8ee41eb7b2b3e457bfd514817a7e4ed
TacB0sS <adam.vdk@nu-art-software.com>
2026-01-30 23:49:42 +0200

    cleanup

--- db-api/frontend/src/main/decorators/index.js
+++
@@ -1,8 +0,0 @@
-/*
- * @nu-art/db-api-frontend - Database API infrastructure for Thunderstorm frontend
- * Copyright (C) 2026 Adam van der Kruk aka TacB0sS
- * Licensed under the Apache License, Version 2.0
- */
-export * from '@nu-art/http-client';
-export * from './types.js';
-//# sourceMappingURL=index.js.map
--- db-api/frontend/src/main/index.d.ts
+++
@@ -1,4 +0,0 @@
-export * from './decorators/index.js';
-export * from './base/ModuleFE_BaseApi.js';
-export * from './base/ModuleFE_BaseDB.js';
-export * from './base/types.js';
