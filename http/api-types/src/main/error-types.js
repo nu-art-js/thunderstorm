c8470c37fcbaf7a0eee7b8d7ac0415af239839e9
TacB0sS <adam.vdk@nu-art-software.com>
2026-01-30 23:52:46 +0200

    cleanup

--- http/api-types/src/main/error-types.js
+++
@@ -1,7 +0,0 @@
-/*
- * @nu-art/api-types - Shared API and error types for HTTP client and server
- * Copyright (C) 2026 Adam van der Kruk aka TacB0sS
- * Licensed under the Apache License, Version 2.0
- */
-export {};
-//# sourceMappingURL=error-types.js.map
--- http/client/src/main/types/types.d.ts
+++
@@ -1,7 +0,0 @@
-export type { QueryParams, UrlQueryParams } from '@nu-art/api-types';
-/**
- * Type for route/query parameters with dynamic value support.
- */
-export type RouteParams = {
-    [key: string]: string | number | boolean | undefined | (() => string | number);
-};
