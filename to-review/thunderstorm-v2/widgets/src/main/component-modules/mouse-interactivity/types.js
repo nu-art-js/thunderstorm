f9873f6f8e86ca37be0ce573853337e1f7a5e0b8
TacB0sS <adam.vdk@nu-art-software.com>
2026-02-13 00:48:24 +0200

    cleared dust

--- to-review/thunderstorm-v2/widgets/src/main/component-modules/mouse-interactivity/types.js
+++
@@ -1,3 +0,0 @@
-export const mouseInteractivity_PopUp = 'pop-up';
-export const mouseInteractivity_ToolTip = 'tool-tip';
-//# sourceMappingURL=types.js.map
--- to-review/thunderstorm-v2/widgets/src/main/components/TS_Tree/index.d.ts
+++
@@ -1,3 +0,0 @@
-export * from './SimpleTreeNodeRenderer.js';
-export * from './types.js';
-export * from './TS_Tree.js';
