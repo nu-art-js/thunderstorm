1c8c0a131e98964f49a95006477e31e364bbb3cb
TacB0sS <adam.vdk@nu-art-software.com>
2026-03-18 19:09:19 +0200

    removed store-shared

--- to-review/storm-shared/src/main/collection-actions/index.ts
+++
@@ -1,8 +0,0 @@
-/*
- * @nu-art/storm-shared - Shared types for storm packages
- * Copyright (C) 2026 Adam van der Kruk aka TacB0sS
- * Licensed under the Apache License, Version 2.0
- */
-
-export * from './api-def.js';
-export * from './types.js';
--- to-review/storm-shared/src/main/url-types.ts
+++
@@ -1,7 +0,0 @@
-/*
- * @nu-art/storm-shared - Shared types for storm packages
- * Copyright (C) 2026 Adam van der Kruk aka TacB0sS
- * Licensed under the Apache License, Version 2.0
- */
-
-export type UrlTarget = '_blank' | '_self' | '_parent' | '_top' | string;
